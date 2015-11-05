48bfa4f68edbd7dc004bc9d93c8d4ca35ec2bf46
LeonTG77 <leontg77@gmail.com>
2015-11-05 15:50:00 +0100

    Added a new announcement

--- src/com/leontg77/uhc/Announcer.java
+++ src/com/leontg77/uhc/Announcer.java
@@ -15,10 +15,8 @@ import com.leontg77.uhc.utils.PlayerUtils;
  * @author LeonTG77
  */
 public class Announcer {
-	private static Announcer instance = new Announcer();
-	
 	private ArrayList<String> messages = new ArrayList<String>();
-	private BukkitRunnable task;
+	private static Announcer instance = new Announcer();
 	
 	/**
 	 * Gets the instance of the class.
@@ -33,7 +31,7 @@ public class Announcer {
 	 * Setup the arena class.
 	 */
 	public void setup() {
-		task = new BukkitRunnable() {
+		new BukkitRunnable() {
 			public void run() {
 				if (Game.getInstance().isRecordedRound() || State.isState(State.SCATTER)) {
 					return;
@@ -41,24 +39,30 @@ public class Announcer {
 				
 				PlayerUtils.broadcast("§8§l[§6§l!§8§l]§7 " + randomAnnouncement());
 			}
-		};
-		
-		task.runTaskTimer(Main.plugin, 5000, 5000);
+		}.runTaskTimer(Main.plugin, 10000, 10000);
 		
 		messages.add("Remember to use §a/uhc §7for all game information.");
 		messages.add("You can view the hall of fame with §a/hof§7.");
 		messages.add("If you have an questions, use §a/helpop§7.");
 		messages.add("You can find the match post by doing §a/post§7.");
-		messages.add("This server runs 95% custom plugins made by LeonTG77.");
+		messages.add("The server runs off a custom uhc plugin by §6LeonTG77§7.");
 		messages.add("Wonder if you are lagging? Use §a/ms §7or §a/tps§7.");
 		messages.add("Follow our twitter for games and updates, §a@ArcticUHC§7!");
+		messages.add("You can apply for a rank at: §6http://goo.gl/forms/O6tklNcrEu§7!");
 		
 		Main.plugin.getLogger().info("The announcer has been setup.");
 	}
 
+	/**
+	 * Get a random announcements from the presets.
+	 * 
+	 * @return A random announcement.
+	 */
 	private String randomAnnouncement() {
 		Random rand = new Random();
+		int size = messages.size();
 		
-		return messages.get(rand.nextInt(messages.size()));
+		String message = messages.get(rand.nextInt(size));
+		return message;
 	}
 }
