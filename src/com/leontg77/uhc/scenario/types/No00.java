c0fe5368391610ef8e193840add3da4e8c5bf302
LeonTG77 <leontg77@gmail.com>
2015-11-07 00:13:37 +0100

    Added a no 0,0 scenario

---
+++ src/com/leontg77/uhc/scenario/types/No00.java
@@ -0,0 +1,43 @@
+package com.leontg77.uhc.scenario.types;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+import com.leontg77.uhc.scenario.Scenario;
+
+/**
+ * No fall scenario class
+ * 
+ * @author LeonTG77
+ */
+public class No00 extends Scenario implements Listener {
+	private boolean enabled = false;
+	
+	public No00() {
+		super("No0,0", "There is no 0,0 on the map, meetup coords are announced after 80 minutes.");
+	}
+
+	public void setEnabled(boolean enable) {
+		enabled = enable;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	@EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+		if (!(event.getEntity() instanceof Player)) {
+			return;
+		}
+		
+		if (event.getCause() != DamageCause.FALL) {
+			return;
+		}
+		
+    	event.setCancelled(true);
+    }
+}
