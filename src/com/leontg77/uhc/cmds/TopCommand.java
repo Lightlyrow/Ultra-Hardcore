ee49282781f1e5e7c4065e0b496a97c2c8a893c6
LeonTG77 <leontg77@gmail.com>
2015-11-11 08:22:00 +0100

    WIP: Added /top

---
+++ src/com/leontg77/uhc/cmds/TopCommand.java
@@ -0,0 +1,19 @@
+package com.leontg77.uhc.cmds;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Top command class.
+ * 
+ * @author LeonTG77
+ */
+public class TopCommand implements CommandExecutor {	
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		return true;
+	}
+}
