e509076e46b9d57e481ad2db4270fafd25642bfd
LeonTG77 <leontg77@gmail.com>
2015-11-11 23:08:36 +0100

    Added comments and getMobName(EntityType)

--- src/com/leontg77/uhc/utils/EntityUtils.java
+++ src/com/leontg77/uhc/utils/EntityUtils.java
@@ -1,22 +1,24 @@
 package com.leontg77.uhc.utils;
 
-import java.util.Random;
 import java.util.Set;
 
 import org.bukkit.entity.EntityType;
-import org.bukkit.util.Vector;
 
 import com.google.common.collect.ImmutableSet;
 
 /**
- * Block utilities class.
+ * Entity utils.
  * <p>
- * Contains block related methods.
+ * This class contains a method for getting the name of an 
+ * entity type and checking if an entity type is clearable
  * 
  * @author LeonTG77
  */
 public class EntityUtils {
 	
+	/**
+	 * A HashSet of all entity types that should be removed at the start.
+	 */
 	protected static final Set<EntityType> clearable = ImmutableSet.of(
 		EntityType.ARROW, EntityType.BAT, EntityType.BLAZE, EntityType.BOAT, EntityType.CAVE_SPIDER,
 		EntityType.CREEPER, EntityType.DROPPED_ITEM, EntityType.EGG, EntityType.ENDERMAN, EntityType.ENDERMITE,
@@ -37,21 +39,143 @@ public class EntityUtils {
 	public static boolean isButcherable(EntityType type) {
 		return clearable.contains(type);
 	}
-
+	
 	/**
-	 * Get a random offset for item dropping.
+	 * Get the MC name for the given entity type.
 	 * 
-	 * @return A vector with a random offset.
+	 * @param type The type wanting to check.
+	 * @return The MC name of the type.
 	 */
-	public static Vector randomOffset() {
-		Random rand = new Random();
-		
-		double offsetX = rand.nextDouble() / 20;
-		double offsetZ = rand.nextDouble() / 20;
-
-		offsetX = offsetX - (rand.nextDouble() / 20);
-		offsetZ = offsetZ - (rand.nextDouble() / 20);
-		
-		return new Vector(offsetX, 0.2, offsetZ);
+	public static String getMobName(EntityType type) {
+		switch (type) {
+		case ARMOR_STAND:
+			return "Armor Stand";
+		case ARROW:
+			return "Arrow";
+		case BAT:
+			return "Bat";
+		case BLAZE:
+			return "Blaze";
+		case BOAT:
+			return "Boat";
+		case CAVE_SPIDER:
+			return "Cave Spider";
+		case CHICKEN:
+			return "Chicken";
+		case COW:
+			return "Cow";
+		case CREEPER:
+			return "Creeper";
+		case DROPPED_ITEM:
+			return "Dropped Item";
+		case EGG:
+			return "Egg";
+		case ENDERMAN:
+			return "Enderman";
+		case ENDERMITE:
+			return "Endermite";
+		case ENDER_CRYSTAL:
+			return "Ender Crystal";
+		case ENDER_DRAGON:
+			return "Ender Dragon";
+		case ENDER_PEARL:
+			return "Ender Pearl";
+		case ENDER_SIGNAL:
+			return "Ender Signal";
+		case EXPERIENCE_ORB:
+			return "Exp. Orb";
+		case FALLING_BLOCK:
+			return "Falling Block";
+		case FIREBALL:
+			return "Fireball";
+		case FIREWORK:
+			return "Firework";
+		case FISHING_HOOK:
+			return "Fish. Hook";
+		case GHAST:
+			return "Ghast";
+		case GIANT:
+			return "Giant";
+		case GUARDIAN:
+			return "Guardian";
+		case HORSE:
+			return "Horse";
+		case IRON_GOLEM:
+			return "Iron Golem";
+		case ITEM_FRAME:
+			return "Item Frame";
+		case LEASH_HITCH:
+			return "Leash";
+		case LIGHTNING:
+			return "Lightning";
+		case MAGMA_CUBE:
+			return "Magma Cube";
+		case MINECART:
+			return "Minecart";
+		case MINECART_CHEST:
+			return "Minecart Chest";
+		case MINECART_COMMAND:
+			return "Minecart CommandBlock";
+		case MINECART_FURNACE:
+			return "Minecart Furnace";
+		case MINECART_HOPPER:
+			return "Minecart Hopper";
+		case MINECART_MOB_SPAWNER:
+			return "Minecart Mob Spawner";
+		case MINECART_TNT:
+			return "Minecart";
+		case MUSHROOM_COW:
+			return "Mushroom Cow";
+		case OCELOT:
+			return "Ocelot";
+		case PAINTING:
+			return "Painting";
+		case PIG:
+			return "Pig";
+		case PIG_ZOMBIE:
+			return "Zombie Pigman";
+		case PLAYER:
+			return "Player";
+		case PRIMED_TNT:
+			return "TNT";
+		case RABBIT:
+			return "Rabbit";
+		case SHEEP:
+			return "Sheep";
+		case SILVERFISH:
+			return "Silverfish";
+		case SKELETON:
+			return "Skeleton";
+		case SLIME:
+			return "Slime";
+		case SMALL_FIREBALL:
+			return "Fireball";
+		case SNOWBALL:
+			return "Snowball";
+		case SNOWMAN:
+			return "Snow Golem";
+		case SPIDER:
+			return "Spider";
+		case SPLASH_POTION:
+			return "Potion";
+		case SQUID:
+			return "Squid";
+		case THROWN_EXP_BOTTLE:
+			return "Thrown Exp. Bottle";
+		case VILLAGER:
+			return "Villager";
+		case WITCH:
+			return "Witch";
+		case WITHER:
+			return "Wither";
+		case WITHER_SKULL:
+			return "Wither";
+		case WOLF:
+			return "Wolf";
+		case ZOMBIE:
+			return "Zombie";
+		default:
+			return "???";
+		}
 	}
 }
