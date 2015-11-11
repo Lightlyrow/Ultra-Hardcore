ce42c25252033e525eb07862782575fc7b1153ad
LeonTG77 <leontg77@gmail.com>
2015-11-11 22:43:01 +0100

    Fixed some distance bugs

--- src/com/leontg77/uhc/scenario/types/RewardingLongshotsPlus.java
+++ src/com/leontg77/uhc/scenario/types/RewardingLongshotsPlus.java
@@ -18,19 +18,16 @@ import com.leontg77.uhc.utils.PlayerUtils;
  * @author LeonTG77
  */
 public class RewardingLongshotsPlus extends Scenario implements Listener {
-	private boolean enabled = false;
 	
 	public RewardingLongshotsPlus() {
 		super("RewardingLongshots+", "When shooting and hitting people with a bow from a variable distance, you will be rewarded with various different items.");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-	}
+	@Override
+	public void onDisable() {}
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+	@Override
+	public void onEnable() {}
 	
 	@EventHandler(ignoreCancelled = true)
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
@@ -47,22 +44,27 @@ public class RewardingLongshotsPlus extends Scenario implements Listener {
 		
 		Player killer = (Player) damager.getShooter();
 		double distance = killer.getLocation().distance(player.getLocation());
-
-		PlayerUtils.broadcast("§7[§9Rewarding Longshots+§7] §r" + killer.getName() + "just got a longshot of " + NumberUtils.convertDouble(distance) + " metres!");
 		
-		if (distance >= 30 && distance <= 49) {
-			PlayerUtils.giveItem(killer, new ItemStack(Material.ARROW, 8));
-			PlayerUtils.giveItem(killer, new ItemStack(Material.IRON_INGOT));
+		if (distance < 30) {
+			return;
 		}
 
-		if (distance >= 50 && distance <= 99) {
+		PlayerUtils.broadcast("§7[§9Rewarding Longshots+§7] §r" + killer.getName() + " just got a longshot of " + NumberUtils.convertDouble(distance) + " metres!");
+		
+		if (distance <= 49) {
+			PlayerUtils.giveItem(killer, new ItemStack(Material.ARROW, 8));
+			PlayerUtils.giveItem(killer, new ItemStack(Material.IRON_INGOT));
+			return;
+		}
+
+		if (distance <= 99) {
 			PlayerUtils.giveItem(killer, new ItemStack(Material.ARROW, 16));
 			PlayerUtils.giveItem(killer, new ItemStack(Material.GOLD_INGOT));
 			PlayerUtils.giveItem(killer, new ItemStack(Material.IRON_INGOT));
 			return;
 		} 
 
-		if (distance >= 100 && distance <= 199) {
+		if (distance <= 199) {
 			PlayerUtils.giveItem(killer, new ItemStack(Material.BOW));
 			PlayerUtils.giveItem(killer, new ItemStack(Material.ARROW, 32));
 			PlayerUtils.giveItem(killer, new ItemStack(Material.GOLD_INGOT));
@@ -70,25 +72,11 @@ public class RewardingLongshotsPlus extends Scenario implements Listener {
 			PlayerUtils.giveItem(killer, new ItemStack(Material.DIAMOND));
 			return;
 		}
-
-		if (distance >= 200) {
-			PlayerUtils.giveItem(killer, new ItemStack(Material.BOW, 2));
-			PlayerUtils.giveItem(killer, new ItemStack(Material.ARROW, 64));
-			PlayerUtils.giveItem(killer, new ItemStack(Material.GOLD_INGOT, 3));
-			PlayerUtils.giveItem(killer, new ItemStack(Material.IRON_INGOT, 2));
-			PlayerUtils.giveItem(killer, new ItemStack(Material.DIAMOND, 5));
-		}
-	}
-
-	@Override
-	protected void onEnable() {
-		// TODO Auto-generated method stub
 		
-	}
-
-	@Override
-	protected void onDisable() {
-		// TODO Auto-generated method stub
-		
+		PlayerUtils.giveItem(killer, new ItemStack(Material.BOW, 2));
+		PlayerUtils.giveItem(killer, new ItemStack(Material.ARROW, 64));
+		PlayerUtils.giveItem(killer, new ItemStack(Material.GOLD_INGOT, 3));
+		PlayerUtils.giveItem(killer, new ItemStack(Material.IRON_INGOT, 2));
+		PlayerUtils.giveItem(killer, new ItemStack(Material.DIAMOND, 5));
 	}
 }
