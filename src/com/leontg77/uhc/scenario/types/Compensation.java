2e7f50b9633eb5d8ae1d65b0080d198b99acba49
LeonTG77 <leontg77@gmail.com>
2015-11-11 23:14:33 +0100

    Rewrote compensation

--- src/com/leontg77/uhc/scenario/types/Compensation.java
+++ src/com/leontg77/uhc/scenario/types/Compensation.java
@@ -8,86 +8,98 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.CraftingInventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scoreboard.Team;
 
 import com.leontg77.uhc.scenario.Scenario;
+import com.leontg77.uhc.scoreboard.Teams;
 
 /**
  * Compensation scenario class
  * 
- * @author TheRCPanda
+ * @author TheRCPanda, modified by LeonTG77
  */
 public class Compensation extends Scenario implements Listener {
-	private boolean enabled = false;
 	
 	public Compensation() {
 		super("Compensation", "When a player on a team dies, the player's max health is divided up and added to the max health of the player's teammates. The extra health received will regenerate in 30 seconds.");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-	}
+	@Override
+	public void onDisable() {}
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+	@Override
+	public void onEnable() {}
 	
 	@EventHandler
-    public void onCraft(PrepareItemCraftEvent event) {
-		if (event.getRecipe().getResult().getType() == Material.ARROW) {
-			event.getInventory().getResult().setAmount(event.getInventory().getResult().getAmount() * 4);
+    public void onPrepareItemCraft(PrepareItemCraftEvent event) {
+		Recipe recipe = event.getRecipe();
+		ItemStack result = recipe.getResult();
+		
+		if (result.getType() != Material.ARROW) {
+			return;
 		}
+		
+		CraftingInventory inv = event.getInventory();
+		inv.getResult().setAmount(result.getAmount() * 4);
     }
 	
 	@EventHandler
-    public void onDeath(PlayerDeathEvent event) {
-		Player victim = event.getEntity();
-        double victimMaxHealth = victim.getMaxHealth();
+    public void onPlayerDeath(PlayerDeathEvent event) {
+		Player player = event.getEntity();
+        double maxHealth = player.getMaxHealth();
+        
+        Teams teams = Teams.getInstance();
+        Team team = teams.getTeam(player);
 
-        Team victimTeam = victim.getScoreboard().getEntryTeam(victim.getName());
+        if (team == null) {
+            return;
+        }
+        
+        team.removeEntry(player.getName());
 
-        if (victimTeam != null) {
-            victimTeam.removeEntry(victim.getName());
+        double hp = maxHealth / team.getSize();
+        int hpRounded = (int) hp;
 
-            double healthPerPerson = victimMaxHealth / victimTeam.getSize();
-            int healthPerPersonRounded = (int) healthPerPerson;
+		double excessHealth = hp - hpRounded;
+        int ticksRegen = hpRounded * 50;
 
-			double excessHealth = healthPerPerson - healthPerPersonRounded;
-
-            int ticksRegen = healthPerPersonRounded * 50;
-
-            for (String s : victimTeam.getEntries()) {
-            	Player p = Bukkit.getServer().getPlayer(s);
-            	
-            	if (p == null) {
-            		continue;
-            	}
-            	
-                p.setMaxHealth(p.getMaxHealth() + healthPerPerson);
-                p.removePotionEffect(PotionEffectType.REGENERATION);
-                p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, ticksRegen, 0));
-                p.setHealth(p.getHealth() + excessHealth);
-            }
+        for (String entry : team.getEntries()) {
+        	Player teammate = Bukkit.getPlayer(entry);
+        	
+        	if (teammate == null) {
+        		continue;
+        	}
+        	
+            teammate.setMaxHealth(teammate.getMaxHealth() + hp);
+            teammate.setHealth(teammate.getHealth() + excessHealth);
+            
+            teammate.removePotionEffect(PotionEffectType.REGENERATION);
+            teammate.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, ticksRegen, 0));
         }
     }
 
     @EventHandler
-    public void onEat(PlayerItemConsumeEvent event) {
+    public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
     	Player player = event.getPlayer();
+    	ItemStack item = event.getItem();
 
-        if (event.getItem().getType() == Material.GOLDEN_APPLE) {
-        	player.removePotionEffect(PotionEffectType.REGENERATION);
-
-            double regenTicks = (player.getMaxHealth() / 5) * 25;
-            int regenTicksRounded = (int) regenTicks;
-
-            double excessHealth = regenTicks - regenTicksRounded;
-
-            player.setHealth(player.getHealth() + excessHealth);
-            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, regenTicksRounded, 1));
-
+        if (item.getType() != Material.GOLDEN_APPLE) {
+        	return;
         }
+        
+        player.removePotionEffect(PotionEffectType.REGENERATION);
+
+        double ticks = (player.getMaxHealth() / 5) * 25;
+        int ticksRounded = (int) ticks;
+
+        double excessHealth = ticks - ticksRounded;
+
+        player.setHealth(player.getHealth() + excessHealth);
+        player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, ticksRounded, 1));
     }
 }
