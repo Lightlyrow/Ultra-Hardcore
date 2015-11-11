64f8e28efbf24e5cb8f59713139d27e109a889ff
LeonTG77 <leontg77@gmail.com>
2015-11-11 08:34:45 +0100

    Changed the enable and disable methods

--- src/com/leontg77/uhc/scenario/types/PotentialHearts.java
+++ src/com/leontg77/uhc/scenario/types/PotentialHearts.java
@@ -11,27 +11,22 @@ import com.leontg77.uhc.utils.PlayerUtils;
  * @author LeonTG77
  */
 public class PotentialHearts extends Scenario {
-	private boolean enabled = false;
 
 	public PotentialHearts() {
 		super("PotentialHearts", "Everyone starts off with 10 hearts and 10 unhealed potential hearts you need to heal by yourself.");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-		
-		if (enable) {
-			for (Player online : PlayerUtils.getPlayers()) {
-				online.setMaxHealth(40);
-			}
-		} else {
-			for (Player online : PlayerUtils.getPlayers()) {
-				online.setMaxHealth(20);
-			}
+	@Override
+	public void onDisable() {
+		for (Player online : PlayerUtils.getPlayers()) {
+			online.setMaxHealth(20);
 		}
 	}
 
-	public boolean isEnabled() {
-		return enabled;
+	@Override
+	public void onEnable() {
+		for (Player online : PlayerUtils.getPlayers()) {
+			online.setMaxHealth(40);
+		}
 	}
 }
