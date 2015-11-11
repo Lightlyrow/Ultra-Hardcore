a66d595d6a268ed3a504d80e4081cee81763c94a
LeonTG77 <leontg77@gmail.com>
2015-11-11 08:34:27 +0100

    Changed the disable and enable methods

--- src/com/leontg77/uhc/scenario/types/PotentialMoles.java
+++ src/com/leontg77/uhc/scenario/types/PotentialMoles.java
@@ -8,28 +8,23 @@ import com.leontg77.uhc.scenario.ScenarioManager;
 /**
  * PotentialMoles scenario class
  * 
- * @author Bergasms, modified by LeonTG77
+ * @author LeonTG77
  */
 public class PotentialMoles extends Scenario implements Listener {
-	private boolean enabled = false;
 
 	public PotentialMoles() {
 		super("PotentialMoles", "There is a 50% chance of a team having a mole, moles on each team work together to take out the normal teams.");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-		
+	@Override
+	public void onDisable() {
 		ScenarioManager scen = ScenarioManager.getInstance();
-		
-		if (enable) {
-			scen.getScenario("Moles").enable();
-		} else {
-			scen.getScenario("Moles").disable();
-		}
+		scen.getScenario("Moles").setEnabled(false);
 	}
 
-	public boolean isEnabled() {
-		return enabled;
+	@Override
+	public void onEnable() {
+		ScenarioManager scen = ScenarioManager.getInstance();
+		scen.getScenario("Moles").setEnabled(true);
 	}
 }
