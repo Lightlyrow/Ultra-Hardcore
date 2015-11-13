cbc968c686bd5e46553e4e01d76e5ce976a9e083
LeonTG77 <leontg77@gmail.com>
2015-11-14 00:25:54 +0100

    Rewrote the class

--- src/com/leontg77/uhc/scenario/types/BetaZombies.java
+++ src/com/leontg77/uhc/scenario/types/BetaZombies.java
@@ -1,6 +1,7 @@
 package com.leontg77.uhc.scenario.types;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,28 +16,31 @@ import com.leontg77.uhc.scenario.Scenario;
  * @author LeonTG77
  */
 public class BetaZombies extends Scenario implements Listener {
-	private boolean enabled = false;
 	
 	public BetaZombies() {
 		super("BetaZombies", "Zombies drop feathers.");
 	}
 	
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-	}
-	
-	public boolean isEnabled() {
-		return enabled;
-	}
+	@Override
+	public void onDisable() {}
+
+	@Override
+	public void onEnable() {}
 	
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent event) {
-		if (event.getEntity() instanceof Zombie) {
-			for (ItemStack drops : event.getDrops()) {
-				if (drops.getType() == Material.ROTTEN_FLESH) {
-					drops.setType(Material.FEATHER);
-				}
+		Entity entity = event.getEntity();
+		
+		if (!(entity instanceof Zombie)) {
+			return;
+		}
+		
+		for (ItemStack drops : event.getDrops()) {
+			if (drops.getType() != Material.ROTTEN_FLESH) {
+				continue;
 			}
+			
+			drops.setType(Material.FEATHER);
 		}
 	}
 }
