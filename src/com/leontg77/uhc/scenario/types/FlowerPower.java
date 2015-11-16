9e6cac12c15b8dd8711b613e08567a7cc0799e59
LeonTG77 <leontg77@gmail.com>
2015-11-16 16:50:30 +0100

    AIR, BEDROCK and BARRIER is not longer droppable

--- src/com/leontg77/uhc/scenario/types/FlowerPower.java
+++ src/com/leontg77/uhc/scenario/types/FlowerPower.java
@@ -1,16 +1,18 @@
 package com.leontg77.uhc.scenario.types;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
-import com.leontg77.uhc.Main;
 import com.leontg77.uhc.scenario.Scenario;
+import com.leontg77.uhc.utils.BlockUtils;
 
 /**
  * FlowerPower scenario class
@@ -18,22 +20,20 @@ import com.leontg77.uhc.scenario.Scenario;
  * @author LeonTG77
  */
 public class FlowerPower extends Scenario implements Listener {
-	private boolean enabled = false;
 	
 	public FlowerPower() {
 		super("FlowerPower", "If you break flowers they will drop an random item.");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-	}
+	@Override
+	public void onDisable() {}
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+	@Override
+	public void onEnable() {}
 	
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent event) {
+		Player player = event.getPlayer();
 		Block block = event.getBlock();
 		
 		if (block.getType() != Material.RED_ROSE && block.getType() != Material.YELLOW_FLOWER && block.getType() != Material.DOUBLE_PLANT) {
@@ -41,14 +41,19 @@ public class FlowerPower extends Scenario implements Listener {
 		}
 		
 		if (block.getType() == Material.DOUBLE_PLANT) {
-			short damage = block.getState().getData().toItemStack().getDurability();
+			int damage = BlockUtils.getDurability(block);
 			
 			if (damage == 2 || damage == 3) {
 				return;
 			}
 		}
 		
-		Main.toReplace.put(block.getType(), randomItem());
+		event.setCancelled(true);
+		block.setType(Material.AIR);
+		
+		BlockUtils.blockBreak(player, block);
+		BlockUtils.degradeDurabiliy(player);
+		BlockUtils.dropItem(block.getLocation().add(0.5, 0.7, 0.5), randomItem());
 	}
 
 	/**
@@ -59,8 +64,22 @@ public class FlowerPower extends Scenario implements Listener {
 	private ItemStack randomItem() {
 		Random r = new Random();
 		
-		Material m = Material.values()[r.nextInt(Material.values().length)];
+		ArrayList<Material> list = new ArrayList<Material>();
+		
+		for (Material type : Material.values()) {
+			if (type == Material.AIR || type == Material.BEDROCK || type == Material.BARRIER) {
+				continue;
+			}
+			
+			list.add(type);
+		}
+		
+		Material m = list.get(r.nextInt(list.size()));
 		int a = r.nextInt(m.getMaxStackSize());
+		
+		if (a == 0) {
+			a = 1;
+		}
 		
 		return new ItemStack(m, a);
 	}
