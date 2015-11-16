226a68837cb44af6990ec769fd36e1db3cbda265
LeonTG77 <leontg77@gmail.com>
2015-11-16 17:22:30 +0100

    Changed the prefix

--- src/com/leontg77/uhc/scenario/types/Inventors.java
+++ src/com/leontg77/uhc/scenario/types/Inventors.java
@@ -2,14 +2,13 @@ package com.leontg77.uhc.scenario.types;
 
 import java.util.HashSet;
 
-import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.inventory.ItemStack;
 
-import com.leontg77.uhc.Main;
 import com.leontg77.uhc.scenario.Scenario;
 import com.leontg77.uhc.utils.PlayerUtils;
 
@@ -20,19 +19,16 @@ import com.leontg77.uhc.utils.PlayerUtils;
  */
 public class Inventors extends Scenario implements Listener {
 	private HashSet<String> crafted = new HashSet<String>();
-	private boolean enabled = false;
 
 	public Inventors() {
 		super("Inventors", "The first person to craft any item will be broadcasted in chat.");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-	}
+	@Override
+	public void onDisable() {}
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+	@Override
+	public void onEnable() {}
 	
 	@EventHandler
 	public void onCraftItem(CraftItemEvent event) {
@@ -44,6 +40,10 @@ public class Inventors extends Scenario implements Listener {
 		}
 		
 		crafted.add(item.getType().name() + item.getDurability());
-		PlayerUtils.broadcast(Main.PREFIX.replaceFirst("UHC", "Inventors") + ChatColor.GREEN + player.getName() + " §7crafted §6" + item.getType().name().toLowerCase().replaceAll("_", " ") + (item.getDurability() > 0 ? ":" + item.getDurability() : "") + " §7first.");
+		PlayerUtils.broadcast("§f§l[Inventors] §7§l" + player.getName() + ": §a§l" + item.getType().name().toLowerCase().replaceAll("_", " ") + (item.getDurability() > 0 ? ":" + item.getDurability() : "") + "!");
+		
+		for (Player online : PlayerUtils.getPlayers()) {
+			online.playSound(online.getLocation(), Sound.ITEM_PICKUP, 1, 2);
+		}
 	}
 }
