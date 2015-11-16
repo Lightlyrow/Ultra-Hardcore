ea772c2c2371e2532669288eb54434826313b628
LeonTG77 <leontg77@gmail.com>
2015-11-16 17:15:08 +0100

    Made the code look better

--- src/com/leontg77/uhc/scenario/types/GoToHell.java
+++ src/com/leontg77/uhc/scenario/types/GoToHell.java
@@ -4,6 +4,7 @@ import org.bukkit.World.Environment;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import com.leontg77.uhc.Game;
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.scenario.Scenario;
 import com.leontg77.uhc.utils.PlayerUtils;
@@ -14,34 +15,36 @@ import com.leontg77.uhc.utils.PlayerUtils;
  * @author LeonTG77
  */
 public class GoToHell extends Scenario {
-	private boolean enabled = false;
 	private BukkitRunnable task;
 	
 	public GoToHell() {
 		super("GoToHell", "After 45 minutes you have to be in the nether or else you take 0.5 hearts of damage every 30 seconds");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-		
-		if (enable) {
-			this.task = new BukkitRunnable() {
-				public void run() {
-					for (Player online : PlayerUtils.getPlayers()) {
-						if (online.getWorld().getEnvironment() != Environment.NETHER) {
-							online.damage(1.0);
-						}
-					}
-				}
-			};
-			
-			task.runTaskTimer(Main.plugin, 600, 600);
-		} else {
-			task.cancel();
-		}
+	@Override
+	public void onDisable() {
+		task.cancel();
 	}
 
-	public boolean isEnabled() {
-		return enabled;
+	@Override
+	public void onEnable() {
+		task = new BukkitRunnable() {
+			public void run() {
+				for (Player online : PlayerUtils.getPlayers()) {
+					Environment env = online.getWorld().getEnvironment();
+					
+					if (env == Environment.NETHER) {
+						continue;
+					}
+					
+					online.damage(1.0);
+				}
+			}
+		};
+		
+		task.runTaskTimer(Main.plugin, 600, 600);
+		
+		Game game = Game.getInstance();
+		game.setNether(true);
 	}
 }
