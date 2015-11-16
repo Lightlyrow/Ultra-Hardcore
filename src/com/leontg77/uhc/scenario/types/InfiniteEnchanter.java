ef3e23bc7f54bd1f8a4fe8c68a7adb608a0b843f
LeonTG77 <leontg77@gmail.com>
2015-11-16 17:22:59 +0100

    Changed enabling and disabling

--- src/com/leontg77/uhc/scenario/types/InfiniteEnchanter.java
+++ src/com/leontg77/uhc/scenario/types/InfiniteEnchanter.java
@@ -13,33 +13,28 @@ import com.leontg77.uhc.utils.PlayerUtils;
  * @author LeonTG77
  */
 public class InfiniteEnchanter extends Scenario {
-	private boolean enabled = false;
 	
 	public InfiniteEnchanter() {
 		super("InfiniteEnchanter", "Everyone is given 128 enchantment tables, anvils and bookshelves.");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-		
-		if (enable) {
-			for (Player online : PlayerUtils.getPlayers()) {
-				online.getInventory().addItem(new ItemStack (Material.ENCHANTMENT_TABLE, 128));
-				online.getInventory().addItem(new ItemStack (Material.BOOKSHELF, 128));
-				online.getInventory().addItem(new ItemStack (Material.ANVIL, 128));
-				online.setLevel(23000);
-			}
-		} else {
-			for (Player online : PlayerUtils.getPlayers()) {
-				online.getInventory().remove(Material.ENCHANTMENT_TABLE);
-				online.getInventory().remove(Material.BOOKSHELF);
-				online.getInventory().remove(Material.ANVIL);
-				online.setLevel(0);
-			}
+	@Override
+	public void onDisable() {
+		for (Player online : PlayerUtils.getPlayers()) {
+			online.getInventory().remove(Material.ENCHANTMENT_TABLE);
+			online.getInventory().remove(Material.BOOKSHELF);
+			online.getInventory().remove(Material.ANVIL);
+			online.setLevel(0);
 		}
 	}
 
-	public boolean isEnabled() {
-		return enabled;
+	@Override
+	public void onEnable() {
+		for (Player online : PlayerUtils.getPlayers()) {
+			online.getInventory().addItem(new ItemStack (Material.ENCHANTMENT_TABLE, 128));
+			online.getInventory().addItem(new ItemStack (Material.BOOKSHELF, 128));
+			online.getInventory().addItem(new ItemStack (Material.ANVIL, 128));
+			online.setLevel(23000);
+		}
 	}
 }
