0e44cc52506f927f8c7ed06497722cacedddf268
LeonTG77 <leontg77@gmail.com>
2015-11-17 20:56:47 +0100

    Instead of checking if they're in the lobby check if they're not in a game world

--- src/com/leontg77/uhc/scenario/types/Skyhigh.java
+++ src/com/leontg77/uhc/scenario/types/Skyhigh.java
@@ -5,6 +5,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import com.leontg77.uhc.Main;
 import com.leontg77.uhc.scenario.Scenario;
+import com.leontg77.uhc.utils.GameUtils;
 import com.leontg77.uhc.utils.PlayerUtils;
 
 /**
@@ -13,36 +14,35 @@ import com.leontg77.uhc.utils.PlayerUtils;
  * @author LeonTG77
  */
 public class Skyhigh extends Scenario {
-	private boolean enabled = false;
 	private BukkitRunnable task;
 	
 	public Skyhigh() {
 		super("Skyhigh", "After 45 minutes, any player below y: 101 will begin to take half a heart of damage every 30 seconds.");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-		
-		if (enable) {
-			this.task = new BukkitRunnable() {
-				public void run() {
-					for (Player online : PlayerUtils.getPlayers()) {
-						if (!online.getWorld().getName().equals("lobby")) {
-							if (online.getLocation().getBlockY() < 101) {
-								online.damage(1.0);
-							}
-						}
-					}
-				}
-			};
-			
-			task.runTaskTimer(Main.plugin, 600, 600);
-		} else {
-			task.cancel();
-		}
+	@Override
+	public void onDisable() {
+		task.cancel();
 	}
 
-	public boolean isEnabled() {
-		return enabled;
+	@Override
+	public void onEnable() {
+		task = new BukkitRunnable() {
+			public void run() {
+				for (Player online : PlayerUtils.getPlayers()) {
+					if (!GameUtils.getGameWorlds().contains(online.getWorld())) {
+						continue;
+					}
+					
+					if (online.getLocation().getY() > 101) {
+						continue;
+					}
+					
+					online.damage(1.0);
+				}
+			}
+		};
+		
+		task.runTaskTimer(Main.plugin, 600, 600);
 	}
 }
