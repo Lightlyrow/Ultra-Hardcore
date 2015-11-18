1c802d31d9bf38590aa7a7e9c76dbcee377cc6ef
LeonTG77 <leontg77@gmail.com>
2015-11-18 08:43:12 +0100

    Fixed the enabling and disabling

--- src/com/leontg77/uhc/scenario/types/Permakill.java
+++ src/com/leontg77/uhc/scenario/types/Permakill.java
@@ -13,30 +13,29 @@ import com.leontg77.uhc.scenario.Scenario;
  * @author LeonTG77
  */
 public class Permakill extends Scenario implements Listener {
-	private Game game = Game.getInstance();
-	private boolean enabled = false;
 
 	public Permakill() {
 		super("Permakill", "Everytime a player dies it toggles between perma day and perma night");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-		
-		if (enable) {
-			game.getWorld().setGameRuleValue("doDaylightCycle", "false");
-			game.getWorld().setTime(6000);
-		} else {
-			game.getWorld().setGameRuleValue("doDaylightCycle", "true");
-		}
+	@Override
+	public void onDisable() {
+		Game game = Game.getInstance();
+		game.getWorld().setGameRuleValue("doDaylightCycle", "true");
 	}
 
-	public boolean isEnabled() {
-		return enabled;
+	@Override
+	public void onEnable() {
+		Game game = Game.getInstance();
+		
+		game.getWorld().setGameRuleValue("doDaylightCycle", "false");
+		game.getWorld().setTime(6000);
 	}
 	
 	@EventHandler
-	public void onPlayerDeath(PlayerDeathEvent e) {
+	public void onPlayerDeath(PlayerDeathEvent event) {
+		Game game = Game.getInstance();
+		
 		if (game.getWorld().getTime() == 6000) {
 			game.getWorld().setTime(18000);
 		} else {
