d481ad2def05c3daa18ff27aad106ba8c0b63504
LeonTG77 <leontg77@gmail.com>
2015-11-19 14:56:00 +0100

    Made a recipe removal

--- src/com/leontg77/uhc/scenario/types/Krenzinator.java
+++ src/com/leontg77/uhc/scenario/types/Krenzinator.java
@@ -1,9 +1,12 @@
 package com.leontg77.uhc.scenario.types;
 
+import java.util.Iterator;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Egg;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,9 +17,11 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.vehicle.VehicleEnterEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapelessRecipe;
 
 import com.leontg77.uhc.scenario.Scenario;
+import com.leontg77.uhc.utils.RecipeUtils;
 
 /**
  * Krenzinator scenario class
@@ -24,66 +29,80 @@ import com.leontg77.uhc.scenario.Scenario;
  * @author dans1988
  */
 public class Krenzinator extends Scenario implements Listener {
-    private ShapelessRecipe diamond = new ShapelessRecipe(new ItemStack(Material.DIAMOND)).addIngredient(9, Material.REDSTONE_BLOCK);
-	private boolean enabled = false;
+    private static final ShapelessRecipe diamond = new ShapelessRecipe(new ItemStack(Material.DIAMOND)).addIngredient(9, Material.REDSTONE_BLOCK);
 
 	public Krenzinator() {
 		super("Krenzinator", "Play UHC like Krenzinator does, Reddit post: https://redd.it/2ee99q");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
+	@Override
+	public void onDisable() {
+		Iterator<Recipe> it = Bukkit.recipeIterator();
 		
-		if (enable) {
-			Bukkit.addRecipe(diamond);
+		while (it.hasNext()) {
+			Recipe res = it.next();
+			
+			if (RecipeUtils.areEqual(res, diamond)) {
+				it.remove();
+			}
 		}
 	}
 
-	public boolean isEnabled() {
-		return enabled;
+	@Override
+	public void onEnable() {
+		Bukkit.addRecipe(diamond);
 	}
 
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerDeath(PlayerDeathEvent event) {
 		Player victim = event.getEntity();
 
-		if (victim.getUniqueId().toString().equals("f6eb67da-99f1-4352-b5c5-c0440be575f1") || victim.getUniqueId().toString().equals("42d908a4-c270-4059-b796-53d217f9429f")) {
-			ItemStack diamonds = new ItemStack(Material.DIAMOND, 10);
-
-			event.getDrops().add(diamonds);
+		if (!victim.getUniqueId().toString().equals("f6eb67da-99f1-4352-b5c5-c0440be575f1") && !victim.getUniqueId().toString().equals("42d908a4-c270-4059-b796-53d217f9429f")) {
+			return;
 		}
+		
+		event.getDrops().add(new ItemStack(Material.DIAMOND, 10));
 	}
 	
 	@EventHandler
     public void onVehicleEnter(VehicleEnterEvent event) {
-		if (((event.getVehicle() instanceof Horse)) && ((event.getEntered() instanceof Player))) {
-            
-            Horse horse = (Horse) event.getVehicle();
-            
-            if (horse.getVariant().equals(Horse.Variant.DONKEY)) {
-                return;
-            }
-
-            ((Player)event.getEntered()).sendMessage(ChatColor.RED + "You can't mount horses in this gamemode!");
-            event.setCancelled(true);
+		if (!(event.getVehicle() instanceof Horse) && !(event.getEntered() instanceof Player)) {
+            return;
         }
+		
+		Player player = (Player) event.getEntered();
+		Horse horse = (Horse) event.getVehicle();
+        
+        if (horse.getVariant().equals(Horse.Variant.DONKEY)) {
+            return;
+        }
+
+        player.sendMessage(ChatColor.RED + "You can't mount horses in this gamemode! (Only Donkeys are allowed!)");
+        event.setCancelled(true);
     }
 	
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-		if (event.getDamager().getType().equals(EntityType.EGG)) {
-			event.setDamage(1.0);
+		Entity damager = event.getDamager();
+		
+		if (!(damager instanceof Egg)) {
+			return;
 		}  
+		
+		event.setDamage(1.0);
 	}
 	
 	@EventHandler
-    public void onCraft(CraftItemEvent event)  {
+    public void onCraftItem(CraftItemEvent event)  {
         if (event.isCancelled()) {
             return;
         }
         
-        if (event.getCurrentItem().getType().equals(Material.DIAMOND_SWORD)) {
-            event.getWhoClicked().damage(2.0);
+        ItemStack item = event.getCurrentItem();
+        
+        if (!item.getType().equals(Material.DIAMOND_SWORD)) {
+        	return;
         }
+        event.getWhoClicked().damage(2.0);
     }
 }
