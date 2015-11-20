fe743688e4edf5d92bab92e6102e0a386ae03d17
LeonTG77 <leontg77@gmail.com>
2015-11-20 15:45:45 +0100

    Redid a lot of things

--- src/com/leontg77/uhc/scenario/types/NightmareMode.java
+++ src/com/leontg77/uhc/scenario/types/NightmareMode.java
@@ -10,6 +10,8 @@ import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Blaze;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Enderman;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Silverfish;
 import org.bukkit.entity.Skeleton;
@@ -36,105 +38,136 @@ import com.leontg77.uhc.scenario.Scenario;
  * @author LeonTG77
  */
 public class NightmareMode extends Scenario implements Listener {
-	private boolean enabled = false;
 	
 	public NightmareMode() {
 		super("NightmareMode", "Variety of changes to mobs to make them more difficult.");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-	}
+	@Override
+	public void onDisable() {}
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+	@Override
+	public void onEnable() {}
 	
 	@EventHandler
 	public void onCreatureSpawn(CreatureSpawnEvent event) {
-		if (event.getEntity() instanceof Witch) {
-			event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1726272000, 4));
+		LivingEntity entity = event.getEntity();
+		
+		if (entity instanceof Witch) {
+			entity.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1726272000, 4));
+			return;
 		} 
-		else if (event.getEntity() instanceof Spider) {
-			event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1726272000, 3));
+		
+		if (entity instanceof Spider) {
+			entity.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1726272000, 3));
+			return;
 		} 
-		else if (event.getEntity() instanceof Zombie) {
-			event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 1726272000, 1));
+		
+		if (entity instanceof Zombie) {
+			entity.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 1726272000, 1));
+			return;
 		} 
-		else if (event.getEntity() instanceof Creeper) {
-			event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1726272000, 1));
+		
+		if (entity instanceof Creeper) {
+			entity.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1726272000, 1));
 		} 
 	}
 	
 	@EventHandler
 	public void onEntityDamage(EntityDamageEvent event) {
-		if (event.getEntity() instanceof Zombie) {
-			if (event.getCause() == DamageCause.FIRE || event.getCause() == DamageCause.FIRE_TICK) {
-				if ((new Random().nextInt(99) + 1) >= 50) {
-					event.setCancelled(true);
-				}
+		DamageCause cause = event.getCause();
+		Entity entity = event.getEntity();
+		
+		Random rand = new Random();
+		
+		if (entity instanceof Zombie) {
+			if (cause != DamageCause.FIRE && cause != DamageCause.FIRE_TICK) {
+				return;
 			}
+
+			event.setCancelled(rand.nextBoolean());
 			return;
 		}
 		
-		if (event.getEntity() instanceof Enderman) {
-			if ((new Random().nextInt(99) + 1) <= 10) {
-				event.getEntity().getWorld().spawn(event.getEntity().getLocation(), Blaze.class);
+		if (entity instanceof Enderman) {
+			if (rand.nextDouble() > 0.10) {
+				return;
 			}
+			
+			entity.getWorld().spawn(entity.getLocation(), Blaze.class);
 		}
 	}
 	
 	@EventHandler
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-		if (event.getEntity() instanceof Player) {
-			if (event.getDamager() instanceof Arrow) {
-				if (((Arrow) event.getDamager()) instanceof Skeleton) {
-					((Player) event.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.POISON, 60, 1));
-				}
-				return;
-			}
+		Entity damager = event.getDamager();
+		Entity entity = event.getEntity();
+		
+		if (!(entity instanceof Player)) {
+			return;
+		}
+		
+		Player player = (Player) entity;
+		
+		if (damager instanceof Arrow) {
+			Arrow arrow = (Arrow) damager;
 			
-			if (event.getDamager() instanceof Enderman) {
-				event.getEntity().setFireTicks(60);
+			if (arrow.getShooter() instanceof Skeleton) {
+				player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 60, 1));
 			}
+			return;
+		}
+		
+		if (damager instanceof Enderman) {
+			player.setFireTicks(60);
 		}
 	}
 	
 	@EventHandler
 	public void onEntityExplode(EntityExplodeEvent event) {
-		if (event.getEntity() instanceof Creeper) {
-			event.getEntity().getWorld().spawn(event.getEntity().getLocation(), Silverfish.class);
-			event.getEntity().getWorld().spawn(event.getEntity().getLocation(), Silverfish.class);
-			event.getEntity().getWorld().spawn(event.getEntity().getLocation(), Silverfish.class);
-			event.getEntity().getWorld().spawn(event.getEntity().getLocation(), Silverfish.class);
-			event.getEntity().getWorld().spawn(event.getEntity().getLocation(), Silverfish.class);
+		Entity entity = event.getEntity();
+		
+		if (!(entity instanceof Creeper)) {
+			return;
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			entity.getWorld().spawn(entity.getLocation(), Silverfish.class);
 		}
 	}
 	
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent event) {
-		if (event.getEntity() instanceof Creeper) {
-			if ((new Random().nextInt(99) + 1) >= 50) {
-				event.getEntity().getWorld().spawn(event.getEntity().getLocation(), Silverfish.class);
-			}
-		} 
-		else if (event.getEntity() instanceof Zombie) {
-			Skeleton skelly = event.getEntity().getWorld().spawn(event.getEntity().getLocation(), Skeleton.class);
-			skelly.getEquipment().setItemInHand(new ItemStack (Material.STONE_SWORD));
-			skelly.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1726272000, 0));
-		} 
-		else if (event.getEntity() instanceof Spider) {
-			Location loc = event.getEntity().getLocation();
-			Block b = loc.getBlock();
+		LivingEntity entity = event.getEntity();
+		
+		if (entity instanceof Creeper) {
+			Random rand = new Random();
 			
-			b.getRelative(BlockFace.EAST).setType(Material.WEB);
-			b.getRelative(BlockFace.WEST).setType(Material.WEB);
-			b.getRelative(BlockFace.SOUTH).setType(Material.WEB);
-			b.getRelative(BlockFace.NORTH).setType(Material.WEB);
-			b.getRelative(BlockFace.NORTH_EAST).setType(Material.REDSTONE_WIRE);
-			b.getRelative(BlockFace.NORTH_WEST).setType(Material.REDSTONE_WIRE);
-			b.getRelative(BlockFace.SOUTH_EAST).setType(Material.REDSTONE_WIRE);
-			b.getRelative(BlockFace.SOUTH_WEST).setType(Material.REDSTONE_WIRE);
+			if (rand.nextBoolean()) {
+				entity.getWorld().spawn(entity.getLocation(), Silverfish.class);
+			}
+			return;
+		} 
+		
+		if (entity instanceof Zombie) {
+			Skeleton skelly = entity.getWorld().spawn(entity.getLocation(), Skeleton.class);
+			skelly.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1726272000, 0));
+			skelly.getEquipment().setItemInHand(new ItemStack (Material.STONE_SWORD));
+			return;
+		} 
+		
+		if (entity instanceof Spider) {
+			Location loc = entity.getLocation();
+			Block block = loc.getBlock();
+
+			block.getRelative(BlockFace.NORTH_EAST).setType(Material.REDSTONE_WIRE);
+			block.getRelative(BlockFace.NORTH_WEST).setType(Material.REDSTONE_WIRE);
+			block.getRelative(BlockFace.SOUTH_EAST).setType(Material.REDSTONE_WIRE);
+			block.getRelative(BlockFace.SOUTH_WEST).setType(Material.REDSTONE_WIRE);
+			block.getRelative(BlockFace.SOUTH).setType(Material.WEB);
+			block.getRelative(BlockFace.NORTH).setType(Material.WEB);
+			block.getRelative(BlockFace.EAST).setType(Material.WEB);
+			block.getRelative(BlockFace.WEST).setType(Material.WEB);
 		}
 	}
 }
