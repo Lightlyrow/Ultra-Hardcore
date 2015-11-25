6f3ea0d61992c1083c4a765d98760f6605fd4662
LeonTG77 <leontg77@gmail.com>
2015-11-25 08:51:02 +0100

    Fixed the code to look a lot better

--- src/com/leontg77/uhc/scenario/types/Switcheroo.java
+++ src/com/leontg77/uhc/scenario/types/Switcheroo.java
@@ -1,6 +1,7 @@
 package com.leontg77.uhc.scenario.types;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
@@ -15,38 +16,44 @@ import com.leontg77.uhc.scenario.Scenario;
  * @author LeonTG77
  */
 public class Switcheroo extends Scenario implements Listener {
-	private boolean enabled = false;
 
 	public Switcheroo() {
 		super("Switcheroo", "When you shoot someone, you trade places with them");
 	}
 
-	public void setEnabled(boolean enable) {
-		enabled = enable;
-	}
+	@Override
+	public void onDisable() {}
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+	@Override
+	public void onEnable() {}
 	
 	@EventHandler
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-		if (event.getEntity() instanceof Player) {
-			Player player = (Player) event.getEntity();
-			
-			if (event.getDamager() instanceof Projectile) {
-				Projectile proj = (Projectile) event.getDamager();
-				
-				if (proj.getShooter() instanceof Player) {
-					Player shooter = (Player) proj.getShooter();
-					
-					Location loc1 = player.getLocation();
-					Location loc2 = shooter.getLocation();
-
-					shooter.teleport(loc1);
-					player.teleport(loc2);
-				}
-			}
+		Entity damager = event.getDamager();
+		Entity entity = event.getEntity();
+		
+		if (!(entity instanceof Player)) {
+			return;
 		}
+		
+		Player player = (Player) entity;
+		
+		if (!(damager instanceof Projectile)) {
+			return;
+		}
+		
+		Projectile proj = (Projectile) damager;
+		
+		if (!(proj.getShooter() instanceof Player)) {
+			return;
+		}
+		
+		Player shooter = (Player) proj.getShooter();
+		
+		Location loc1 = player.getLocation();
+		Location loc2 = shooter.getLocation();
+
+		shooter.teleport(loc1);
+		player.teleport(loc2);
 	}
 }
