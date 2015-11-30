102dd80d229d5f7489bfdf87bf3686a065c9a62c
LeonTG77 <leontg77@gmail.com>
2015-11-30 21:17:24 +0100

    Disabled armor stands from being changed in the lobby

--- src/com/leontg77/uhc/listeners/BuildProtectListener.java
+++ src/com/leontg77/uhc/listeners/BuildProtectListener.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.leontg77.uhc.State;
@@ -118,6 +120,27 @@ public class BuildProtectListener implements Listener {
         if (player.hasPermission("uhc.build")) {
 			return;
 		}
+		
+        // the player is in a world that isn't a game world, isn't the arena and he has build perms, cancel the event.
+		event.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onPlayerArmorStandManipulate(PlayerArmorStandManipulateEvent event) {
+		ArmorStand stand = event.getRightClicked();
+		
+		List<World> worlds = GameUtils.getGameWorlds();
+        World world = stand.getWorld();
+        
+        // if the world is a game world we don't want to do anything.
+    	if (worlds.contains(world)) {
+    		return;
+    	}
+    	
+    	// if the world is the arena we don't want to do anything
+    	if (world.getName().equals("arena")) {
+    		return;
+    	}
 		
         // the player is in a world that isn't a game world, isn't the arena and he has build perms, cancel the event.
 		event.setCancelled(true);
