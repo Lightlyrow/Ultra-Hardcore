759392dd5e1ac49b0e5503862bda0d33f9c71f7e
LeonTG77 <leontg77@gmail.com>
2015-11-30 21:12:29 +0100

    You can now get the scenario by putting the class, this returns the class so you can use it's methods

--- src/com/leontg77/uhc/scenario/ScenarioManager.java
+++ src/com/leontg77/uhc/scenario/ScenarioManager.java
@@ -1,84 +1,86 @@
 package com.leontg77.uhc.scenario;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
 import com.leontg77.uhc.Main;
-import com.leontg77.uhc.scenario.types.Assassins;
-import com.leontg77.uhc.scenario.types.AssaultAndBattery;
-import com.leontg77.uhc.scenario.types.Astrophobia;
-import com.leontg77.uhc.scenario.types.Aurophobia;
-import com.leontg77.uhc.scenario.types.Backpacks;
-import com.leontg77.uhc.scenario.types.Barebones;
-import com.leontg77.uhc.scenario.types.BestBTC;
-import com.leontg77.uhc.scenario.types.BestPvE;
-import com.leontg77.uhc.scenario.types.BetaZombies;
-import com.leontg77.uhc.scenario.types.BigCrack;
-import com.leontg77.uhc.scenario.types.BiomeParanoia;
-import com.leontg77.uhc.scenario.types.BlockRush;
-import com.leontg77.uhc.scenario.types.BloodDiamonds;
-import com.leontg77.uhc.scenario.types.BloodEnchants;
-import com.leontg77.uhc.scenario.types.BloodLapis;
-import com.leontg77.uhc.scenario.types.Captains;
-import com.leontg77.uhc.scenario.types.ChunkApocalypse;
-import com.leontg77.uhc.scenario.types.Compensation;
-import com.leontg77.uhc.scenario.types.Cryophobia;
-import com.leontg77.uhc.scenario.types.CutClean;
-import com.leontg77.uhc.scenario.types.DamageCycle;
-import com.leontg77.uhc.scenario.types.Depths;
-import com.leontg77.uhc.scenario.types.Diamondless;
-import com.leontg77.uhc.scenario.types.DragonRush;
-import com.leontg77.uhc.scenario.types.EnchantParanoia;
-import com.leontg77.uhc.scenario.types.EnchantedDeath;
-import com.leontg77.uhc.scenario.types.Fallout;
-import com.leontg77.uhc.scenario.types.FlowerPower;
-import com.leontg77.uhc.scenario.types.Genie;
-import com.leontg77.uhc.scenario.types.GoToHell;
-import com.leontg77.uhc.scenario.types.GoldRush;
-import com.leontg77.uhc.scenario.types.Goldless;
-import com.leontg77.uhc.scenario.types.GoneFishing;
-import com.leontg77.uhc.scenario.types.Halloween;
-import com.leontg77.uhc.scenario.types.HundredHearts;
-import com.leontg77.uhc.scenario.types.InfiniteEnchanter;
-import com.leontg77.uhc.scenario.types.Inventors;
-import com.leontg77.uhc.scenario.types.Kings;
-import com.leontg77.uhc.scenario.types.Krenzinator;
-import com.leontg77.uhc.scenario.types.LAFS;
-import com.leontg77.uhc.scenario.types.Lootcrates;
-import com.leontg77.uhc.scenario.types.MeleeFun;
-import com.leontg77.uhc.scenario.types.Moles;
-import com.leontg77.uhc.scenario.types.MysteryTeams;
-import com.leontg77.uhc.scenario.types.NightmareMode;
-import com.leontg77.uhc.scenario.types.NoFall;
-import com.leontg77.uhc.scenario.types.NoSprint;
-import com.leontg77.uhc.scenario.types.Paranoia;
-import com.leontg77.uhc.scenario.types.PeriodOfResistance;
-import com.leontg77.uhc.scenario.types.Permakill;
-import com.leontg77.uhc.scenario.types.PotentialHearts;
-import com.leontg77.uhc.scenario.types.PotentialMoles;
-import com.leontg77.uhc.scenario.types.PotentialPermanent;
-import com.leontg77.uhc.scenario.types.Pyrophobia;
-import com.leontg77.uhc.scenario.types.RewardingLongshots;
-import com.leontg77.uhc.scenario.types.RewardingLongshotsPlus;
-import com.leontg77.uhc.scenario.types.SharedHealth;
-import com.leontg77.uhc.scenario.types.SkyClean;
-import com.leontg77.uhc.scenario.types.Skyhigh;
-import com.leontg77.uhc.scenario.types.SlaveMarket;
-import com.leontg77.uhc.scenario.types.SlimyCrack;
-import com.leontg77.uhc.scenario.types.Superheroes;
-import com.leontg77.uhc.scenario.types.Switcheroo;
-import com.leontg77.uhc.scenario.types.TeamHealth;
-import com.leontg77.uhc.scenario.types.Timber;
-import com.leontg77.uhc.scenario.types.Timebomb;
-import com.leontg77.uhc.scenario.types.TrainingRabbits;
-import com.leontg77.uhc.scenario.types.TripleOres;
-import com.leontg77.uhc.scenario.types.VengefulSpirits;
-import com.leontg77.uhc.scenario.types.Voidscape;
+import com.leontg77.uhc.scenario.scenarios.AchievementParanoia;
+import com.leontg77.uhc.scenario.scenarios.Assassins;
+import com.leontg77.uhc.scenario.scenarios.AssaultAndBattery;
+import com.leontg77.uhc.scenario.scenarios.Astrophobia;
+import com.leontg77.uhc.scenario.scenarios.Aurophobia;
+import com.leontg77.uhc.scenario.scenarios.Backpacks;
+import com.leontg77.uhc.scenario.scenarios.Barebones;
+import com.leontg77.uhc.scenario.scenarios.BestBTC;
+import com.leontg77.uhc.scenario.scenarios.BestPvE;
+import com.leontg77.uhc.scenario.scenarios.BetaZombies;
+import com.leontg77.uhc.scenario.scenarios.BigCrack;
+import com.leontg77.uhc.scenario.scenarios.BiomeParanoia;
+import com.leontg77.uhc.scenario.scenarios.BlockRush;
+import com.leontg77.uhc.scenario.scenarios.BloodDiamonds;
+import com.leontg77.uhc.scenario.scenarios.BloodEnchants;
+import com.leontg77.uhc.scenario.scenarios.BloodLapis;
+import com.leontg77.uhc.scenario.scenarios.Captains;
+import com.leontg77.uhc.scenario.scenarios.ChunkApocalypse;
+import com.leontg77.uhc.scenario.scenarios.Compensation;
+import com.leontg77.uhc.scenario.scenarios.Cryophobia;
+import com.leontg77.uhc.scenario.scenarios.CutClean;
+import com.leontg77.uhc.scenario.scenarios.DamageCycle;
+import com.leontg77.uhc.scenario.scenarios.Depths;
+import com.leontg77.uhc.scenario.scenarios.Diamondless;
+import com.leontg77.uhc.scenario.scenarios.DragonRush;
+import com.leontg77.uhc.scenario.scenarios.Eggs;
+import com.leontg77.uhc.scenario.scenarios.EnchantParanoia;
+import com.leontg77.uhc.scenario.scenarios.EnchantedDeath;
+import com.leontg77.uhc.scenario.scenarios.Fallout;
+import com.leontg77.uhc.scenario.scenarios.FlowerPower;
+import com.leontg77.uhc.scenario.scenarios.Genie;
+import com.leontg77.uhc.scenario.scenarios.GoToHell;
+import com.leontg77.uhc.scenario.scenarios.GoldRush;
+import com.leontg77.uhc.scenario.scenarios.Goldless;
+import com.leontg77.uhc.scenario.scenarios.GoneFishing;
+import com.leontg77.uhc.scenario.scenarios.Halloween;
+import com.leontg77.uhc.scenario.scenarios.HundredHearts;
+import com.leontg77.uhc.scenario.scenarios.InfiniteEnchanter;
+import com.leontg77.uhc.scenario.scenarios.Inventors;
+import com.leontg77.uhc.scenario.scenarios.Kings;
+import com.leontg77.uhc.scenario.scenarios.Krenzinator;
+import com.leontg77.uhc.scenario.scenarios.LAFS;
+import com.leontg77.uhc.scenario.scenarios.Lootcrates;
+import com.leontg77.uhc.scenario.scenarios.MeleeFun;
+import com.leontg77.uhc.scenario.scenarios.Moles;
+import com.leontg77.uhc.scenario.scenarios.MysteryTeams;
+import com.leontg77.uhc.scenario.scenarios.NightmareMode;
+import com.leontg77.uhc.scenario.scenarios.NoFall;
+import com.leontg77.uhc.scenario.scenarios.NoSprint;
+import com.leontg77.uhc.scenario.scenarios.Paranoia;
+import com.leontg77.uhc.scenario.scenarios.PeriodOfResistance;
+import com.leontg77.uhc.scenario.scenarios.Permakill;
+import com.leontg77.uhc.scenario.scenarios.PotentialHearts;
+import com.leontg77.uhc.scenario.scenarios.PotentialMoles;
+import com.leontg77.uhc.scenario.scenarios.PotentialPermanent;
+import com.leontg77.uhc.scenario.scenarios.Pyrophobia;
+import com.leontg77.uhc.scenario.scenarios.RewardingLongshots;
+import com.leontg77.uhc.scenario.scenarios.RewardingLongshotsPlus;
+import com.leontg77.uhc.scenario.scenarios.SharedHealth;
+import com.leontg77.uhc.scenario.scenarios.SkyClean;
+import com.leontg77.uhc.scenario.scenarios.Skyhigh;
+import com.leontg77.uhc.scenario.scenarios.SlaveMarket;
+import com.leontg77.uhc.scenario.scenarios.SlimyCrack;
+import com.leontg77.uhc.scenario.scenarios.Superheroes;
+import com.leontg77.uhc.scenario.scenarios.Switcheroo;
+import com.leontg77.uhc.scenario.scenarios.TeamHealth;
+import com.leontg77.uhc.scenario.scenarios.Timber;
+import com.leontg77.uhc.scenario.scenarios.Timebomb;
+import com.leontg77.uhc.scenario.scenarios.TrainingRabbits;
+import com.leontg77.uhc.scenario.scenarios.TripleOres;
+import com.leontg77.uhc.scenario.scenarios.VengefulSpirits;
+import com.leontg77.uhc.scenario.scenarios.Voidscape;
 
 /**
  * Scenario management class.
+ * 
  * @author LeonTG77
  */
 public class ScenarioManager {
@@ -87,6 +89,7 @@ public class ScenarioManager {
 	
 	/**
 	 * Get the instance of this class.
+	 * 
 	 * @return The class instance.
 	 */
 	public static ScenarioManager getInstance() {
@@ -97,6 +100,7 @@ public class ScenarioManager {
 	 * Setup all the scenarios.
 	 */
 	public void setup() {
+		scenarios.add(new AchievementParanoia());
 		scenarios.add(new Assassins());
 		scenarios.add(new AssaultAndBattery());
 		scenarios.add(new Astrophobia());
@@ -121,6 +125,7 @@ public class ScenarioManager {
 		scenarios.add(new Depths());
 		scenarios.add(new Diamondless());
 		scenarios.add(new DragonRush());
+		scenarios.add(new Eggs());
 		scenarios.add(new EnchantedDeath());
 		/* TODO: Enchanted Books and Enchant preview */ scenarios.add(new EnchantParanoia());
 		scenarios.add(new Fallout());
@@ -173,13 +178,30 @@ public class ScenarioManager {
 	
 	/**
 	 * Get a scenario by a name.
+	 * 
 	 * @param name the name.
 	 * @return The scenario, null if not found.
 	 */
 	public Scenario getScenario(String name) {
 		for (Scenario s : scenarios) {
-			if (name.equalsIgnoreCase(s.getName())) {
+			if (s.getName().equals(name)) {
 				return s;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Get a scenario by the class.
+	 * 
+	 * @param scenarioClass The class.
+	 * @return The scenario, null if not found.
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> T getScenario(Class<T> scenarioClass) {
+		for (Scenario s : scenarios) {
+			if (s.getClass().equals(scenarioClass)) {
+				return (T) s;
 			}
 		}
 		return null;
@@ -187,21 +209,20 @@ public class ScenarioManager {
 
 	/**
 	 * Get a list of all scenarios.
+	 * 
 	 * @return the list of scenarios.
 	 */
-	public ArrayList<Scenario> getScenarios() {
-		ArrayList<Scenario> list = new ArrayList<Scenario>(scenarios);
-		Collections.shuffle(list);
-		
-		return list;
+	public List<Scenario> getScenarios() {
+		return new ArrayList<Scenario>(scenarios);
 	}
 
 	/**
 	 * Get a list of all enabled scenarios.
+	 * 
 	 * @return the list of enabled scenarios.
 	 */
 	public List<Scenario> getEnabledScenarios() {
-		ArrayList<Scenario> list = new ArrayList<Scenario>();
+		List<Scenario> list = new ArrayList<Scenario>();
 		
 		for (Scenario s : scenarios) {
 			if (s.isEnabled()) {
@@ -209,25 +230,22 @@ public class ScenarioManager {
 			}
 		}
 		
-		Collections.shuffle(list);
-		
 		return list;
 	}
 
 	/**
 	 * Get a list of all enabled scenarios.
+	 * 
 	 * @return the list of enabled scenarios.
 	 */
 	public List<Scenario> getDisabledScenarios() {
-		ArrayList<Scenario> list = new ArrayList<Scenario>();
+		List<Scenario> list = new ArrayList<Scenario>();
 		
 		for (Scenario s : scenarios) {
 			if (!s.isEnabled()) {
 				list.add(s);
 			}
 		}
-		
-		Collections.shuffle(list);
 		
 		return list;
 	}
