4713a83b2eda5345296698464edb2d8893b31979
LeonTG77 <leontg77@gmail.com>
2015-11-30 20:49:53 +0100

    Added a achievement paranoia scenario

---
+++ src/com/leontg77/uhc/scenario/scenarios/AchievementParanoia.java
@@ -0,0 +1,138 @@
+package com.leontg77.uhc.scenario.scenarios;
+
+import org.bukkit.Achievement;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAchievementAwardedEvent;
+
+import com.leontg77.uhc.scenario.Scenario;
+
+/**
+ * Achievement Paranoia scenario class.
+ * 
+ * @author LeonTG77
+ */
+public class AchievementParanoia extends Scenario implements Listener {
+	public static final String PREFIX = "§7[§cAchParanoia§7] §f";
+
+	public AchievementParanoia() {
+		super("AchievementParanoia", "Achievements show up in chat like in vanilla minecraft but at the end off each achievement it shows the coordinates of the player who earned that achievement");
+	}
+
+	@Override
+	public void onDisable() {}
+
+	@Override
+	public void onEnable() {
+		// remove all achievements for all players.
+		for (Player online : Bukkit.getOnlinePlayers()) {
+			for (Achievement ach : Achievement.values()) {
+				if (online.hasAchievement(ach)) {
+					online.removeAchievement(ach);
+				}
+			}
+			
+			// add only open inventory, to make sure they don't cheat the system.
+			online.awardAchievement(Achievement.OPEN_INVENTORY);
+		}
+	}
+
+	@EventHandler
+	public void onPlayerItemConsume(PlayerAchievementAwardedEvent event) {
+		Achievement ach = event.getAchievement();
+		Player player = event.getPlayer();
+
+		// loop n' broadcast.
+		for (Player online : Bukkit.getOnlinePlayers()) {
+			online.sendMessage(PREFIX + "§a" + player.getName() + "§f has earned §e" + achievementName(ach) + "§f at " + locToString(player.getLocation()));
+		}
+	}
+
+	/**
+	 * Convert the given location to a string form.
+	 * 
+	 * @param loc The location converting.
+	 * @return The new string version.
+	 */
+	private String locToString(Location loc) {
+		return loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ".";
+	}
+	
+	private String achievementName(Achievement ach) {
+		switch (ach) {
+		case ACQUIRE_IRON:
+			return "Acquire Hardware";
+		case BAKE_CAKE:
+			return "The Lie";
+		case BOOKCASE:
+			return "Librarian";
+		case BREED_COW:
+			return "Repopulation";
+		case BREW_POTION:
+			return "Local Brewery";
+		case BUILD_BETTER_PICKAXE:
+			return "Getting an Upgrade";
+		case BUILD_FURNACE:
+			return "Hot Topic";
+		case BUILD_HOE:
+			return "Time to Farm";
+		case BUILD_PICKAXE:
+			return "Time to Mine";
+		case BUILD_SWORD:
+			return "Time to Strike";
+		case BUILD_WORKBENCH:
+			return "Benchmarking";
+		case COOK_FISH:
+			return "Delicious Fish";
+		case DIAMONDS_TO_YOU:
+			return "Diamonds to you";
+		case ENCHANTMENTS:
+			return "Enchanter";
+		case END_PORTAL:
+			return "The End?";
+		case EXPLORE_ALL_BIOMES:
+			return "Adventuring time";
+		case FLY_PIG:
+			return "When Pigs Fly";
+		case FULL_BEACON:
+			return "Beaconator";
+		case GET_BLAZE_ROD:
+			return "Into Fire";
+		case GET_DIAMONDS:
+			return "DIAMONDS";
+		case GHAST_RETURN:
+			return "Return to Sender";
+		case KILL_COW:
+			return "Cow Tipper";
+		case KILL_ENEMY:
+			return "Monster Hunter";
+		case KILL_WITHER:
+			return "The Beginning";
+		case MAKE_BREAD:
+			return "Bake Bread";
+		case MINE_WOOD:
+			return "Getting Wood";
+		case NETHER_PORTAL:
+			return "We Need to Go Deeper";
+		case ON_A_RAIL:
+			return "On A Rail";
+		case OPEN_INVENTORY:
+			return "Taking Inventory";
+		case OVERKILL:
+			return "Overkill";
+		case OVERPOWERED:
+			return "Overpowered";
+		case SNIPE_SKELETON:
+			return "Sniper Duel";
+		case SPAWN_WITHER:
+			return "The Beginning?";
+		case THE_END:
+			return "The End";
+		default:
+			return ach.name().toLowerCase().replaceAll("_", " ");
+		}
+	}
+}
