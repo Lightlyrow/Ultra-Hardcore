aafb5f123fdc8dd4c0fdc34152051b738e9c4697
LeonTG77 <leontg77@gmail.com>
2015-12-01 14:49:40 +0100

    Disabled ice/snow melting in the lobby for christmas

--- src/com/leontg77/uhc/listeners/BlockListener.java
+++ src/com/leontg77/uhc/listeners/BlockListener.java
@@ -11,6 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.LeavesDecayEvent;
 import org.bukkit.inventory.ItemStack;
@@ -199,6 +200,23 @@ public class BlockListener implements Listener {
 			BlockUtils.dropItem(loc.clone().add(0.5, 0.7, 0.5), new ItemStack(Material.APPLE, 1));
 		}
     }
+	
+	@EventHandler
+	public void on(BlockFadeEvent event) {
+		Block block = event.getBlock();
+		
+		if (!block.getWorld().getName().equals("lobby")) {
+    		return;
+    	}
+		
+		if (block.getType() == Material.ICE) {
+			event.setCancelled(true);
+		}
+		
+		if (block.getType() == Material.SNOW) {
+			event.setCancelled(true);
+		}
+	}
 	
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent event) {
