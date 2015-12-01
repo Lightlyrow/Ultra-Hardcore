6a7aec16b7ea6b8b5263c42b31d4a8f77642604d
LeonTG77 <leontg77@gmail.com>
2015-12-01 23:05:57 +0100

    Disabled milk drinking

--- src/com/leontg77/uhc/scenario/scenarios/Kings.java
+++ src/com/leontg77/uhc/scenario/scenarios/Kings.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,6 +16,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scoreboard.Team;
@@ -64,6 +67,17 @@ public class Kings extends Scenario implements Listener, CommandExecutor {
 	
 	public Set<String> getKings() {
 		return kings;
+	}
+	
+	@EventHandler
+	public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
+		if (event.getItem().getType() != Material.MILK_BUCKET) {
+			return;
+		}
+		
+		event.getPlayer().sendMessage(Main.PREFIX + "You cannot drink milk in kings.");
+		event.setCancelled(true);
+		event.setItem(new ItemStack (Material.AIR));
 	}
 	
 	@EventHandler
