800e1bde9fe04d9b56451ef1735d39f52aa3c640
LeonTG77 <leontg77@gmail.com>
2015-12-02 08:24:53 +0100

    Made a list of FileConfiguration to store all User files with an update method called when enabled and when creating a new file

--- src/com/leontg77/uhc/utils/FileUtils.java
+++ src/com/leontg77/uhc/utils/FileUtils.java
@@ -1,20 +1,43 @@
 package com.leontg77.uhc.utils;
 
+import static com.leontg77.uhc.Main.plugin;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 /**
  * File utilities class.
  * <p>
  * Contains file related methods.
  * 
- * @author D4mnX
+ * @author LeonTG77
  */
 public class FileUtils {
-
+	public static List<FileConfiguration> files = new ArrayList<FileConfiguration>();
+	
+	/**
+	 * Update the files list if any new ones came.
+	 */
+	public static void updateFiles() {
+		File folder = new File(plugin.getDataFolder() + File.separator + "users" + File.separator);
+		
+		files.clear();
+		
+		for (File file : folder.listFiles()) {
+			files.add(YamlConfiguration.loadConfiguration(file));
+		}
+	}
+	
 	/**
 	 * Deletes the given file and it's subfiles.
 	 * 
 	 * @return True if sucess, false otherwise.
+	 * 
+	 * @author D4mnX
 	 */
 	public static boolean deleteFile(File path) {
 		if (path.exists()) {
