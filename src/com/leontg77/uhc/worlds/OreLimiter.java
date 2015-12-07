81d9d4f59c30bb939ac022b5b99e9a425bdcee2c
LeonTG77 <leontg77@gmail.com>
2015-12-07 09:01:00 +0100

    Finished the OreLimiter code

--- src/com/leontg77/uhc/worlds/OreLimiter.java
+++ src/com/leontg77/uhc/worlds/OreLimiter.java
@@ -1,89 +1,204 @@
 package com.leontg77.uhc.worlds;
 
-import java.util.Map;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.WorldInitEvent;
+import org.bukkit.generator.BlockPopulator;
 
-import com.google.common.collect.Maps;
+import com.google.common.collect.Lists;
 import com.leontg77.uhc.Main;
-import com.leontg77.uhc.worlds.orelimiter.OreListener;
 
-public class OreLimiter {
+public class OreLimiter extends BlockPopulator implements Listener {
+	private static OreLimiter instance = new OreLimiter();
+	
+    private static final int CHUNK_HEIGHT_LIMIT = 128;
+    private static final int BLOCKS_PER_CHUNK = 16;
+    private static final int RANDOM_BOUNDS = 100;
 
-    public static final Material[] SUPPORTED_ORES = {
-        Material.GOLD_ORE,
-        Material.DIAMOND_ORE,
-        Material.IRON_ORE,
-        Material.LAPIS_ORE,
-        Material.COAL_ORE,
-        Material.REDSTONE_ORE,
-        Material.QUARTZ_ORE
-    };
-
-    private final Map<Material, Material> replacements;
-    private final Map<Material, Integer> oreRates;
+    /**
+     * Get the instance of the class.
+     * 
+     * @return The instance.
+     */
+	public static OreLimiter getInstance() {
+		return instance;
+	}
     
-    public OreLimiter() {
-        this.oreRates = Maps.newHashMap();
-        this.replacements = Maps.newHashMap();
-    }
-
+	/**
+	 * Setup the ore limiter class.
+	 */
     public void setup() {
-        Bukkit.getPluginManager().registerEvents(new OreListener(), Main.plugin);
-
-        oreRates.put(Material.GOLD_ORE, configuration.getInt("goldOre.rate"));
-        oreRates.put(Material.DIAMOND_ORE, configuration.getInt("diamondOre.rate"));
-        oreRates.put(Material.IRON_ORE, configuration.getInt("ironOre.rate"));
-        oreRates.put(Material.LAPIS_ORE, configuration.getInt("lapisOre.rate"));
-        oreRates.put(Material.COAL_ORE, configuration.getInt("coalOre.rate"));
-        oreRates.put(Material.REDSTONE_ORE, configuration.getInt("redstoneOre.rate"));
-        oreRates.put(Material.QUARTZ_ORE, configuration.getInt("quartzOre.rate"));
-
-        for (Material material : oreRates.keySet()) {
-            if (oreRates.get(material) < 0) {
-                oreRates.put(material, 0);
-            } else if (oreRates.get(material) > 100) {
-                oreRates.put(material, 100);
-            }
-        }
-
-        replacements.put(Material.GOLD_ORE, Material.matchMaterial(configuration.getString("quartzOre.replacement")));
-        replacements.put(Material.DIAMOND_ORE, Material.matchMaterial(configuration.getString("quartzOre.replacement")));
-        replacements.put(Material.IRON_ORE, Material.matchMaterial(configuration.getString("quartzOre.replacement")));
-        replacements.put(Material.LAPIS_ORE, Material.matchMaterial(configuration.getString("quartzOre.replacement")));
-        replacements.put(Material.COAL_ORE, Material.matchMaterial(configuration.getString("quartzOre.replacement")));
-        replacements.put(Material.REDSTONE_ORE, Material.matchMaterial(configuration.getString("quartzOre.replacement")));
-        replacements.put(Material.QUARTZ_ORE, Material.matchMaterial(configuration.getString("quartzOre.replacement")));
-
-        for (Material material : replacements.keySet()) {
-            if (replacements.get(material) == null) {
-                replacements.put(material, Material.STONE);
-            }
-        }
-    }
-
-    public Map<Material, Integer> getOreRates() {
-        return oreRates;
-    }
-
-    public Map<Material, Material> getReplacements() {
-        return replacements;
-    }
-
-    public Boolean getRunning() {
-        return running;
-    }
-
-    public void setRunning(Boolean running) {
-        this.running = running;
-    }
-
-    public void setRate(Material material, Integer rate) {
-        oreRates.put(material, rate);
+        Bukkit.getPluginManager().registerEvents(this, Main.plugin);
     }
     
-    public void setReplacement(Material material, Material replacement) {
-        replacements.put(material, replacement);
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onWorldInit(final WorldInitEvent event) {
+        Bukkit.getLogger().log(Level.INFO, "World init detected! OreLimiter");
+        event.getWorld().getPopulators().add(this);
+    }
+
+    @Override
+    public void populate(World world, Random rand, Chunk chunk) {
+        for (int x = 0; x < BLOCKS_PER_CHUNK; x++) {
+            for (int z = 0; z < BLOCKS_PER_CHUNK; z++) {
+                for (int y = CHUNK_HEIGHT_LIMIT - 1; y > 0; y--) {
+                    Block block = world.getBlockAt(chunk.getX() * 16 + x, y, chunk.getZ() * 16 + z);
+                    
+                    OreLocation oreLocation = new OreLocation(block.getX(), block.getY(), block.getZ());
+                    
+                    if (block.getType() == Material.DIAMOND_ORE || block.getType() == Material.GOLD_ORE) {
+                        boolean replace;
+                        
+                        if (block.getType() == Material.GOLD_ORE) {
+                        	replace = rand.nextInt(RANDOM_BOUNDS) >= 65;
+                        } else if (block.getType() == Material.DIAMOND_ORE) {
+                        	replace = rand.nextInt(RANDOM_BOUNDS) >= 75;
+                        } else {
+                        	// return so it doesn't think it's not being set.
+                        	return;
+                        }
+                        
+                        OreTraverser oreTraverser = new OreTraverser();
+                        oreTraverser.traverseVein(oreLocation, block.getType(), world, replace, Material.STONE);
+                    }
+                }
+            }
+        }
+    }
+    
+    /**
+     * Ore Traverser class.
+     * 
+     * @author dans1988
+     */
+    public class OreTraverser {
+    	private final List<OreLocation> vein;
+
+    	public OreTraverser() {
+    		this.vein = Lists.newArrayList();
+    	}
+
+    	/**
+    	 * Traverser the vein.
+    	 * 
+    	 * @param ore The ore location of the vein.
+    	 * @param material The vein material.
+    	 * @param world The world of the vein.
+    	 * @param replace Wether to replace the vein or not.
+    	 * @param replacement If replacing, this is what we're replacing with.
+    	 */
+    	public void traverseVein(OreLocation ore, Material material, World world, boolean replace, Material replacement) {
+    		Block block = world.getBlockAt(ore.getX(), ore.getY(), ore.getZ());
+
+    		if (material.equals(block.getType()) && !vein.contains(ore)) {
+    			vein.add(ore);
+    			
+    			if (replace) {
+    				block.setType(replacement);
+    			}
+
+    			for (int x = -1; x <= 1; x++) {
+    				for (int y = -1; y <= 1; y++) {
+    					for (int z = -1; z <= 1; z++) {
+    						OreLocation newLocation = new OreLocation(ore.getX() + x, ore.getY() + y, ore.getZ() + z);
+
+    						traverseVein(newLocation, material, world, replace, replacement);
+    					}
+    				}
+    			}
+    		}
+    	}
+
+    	/**
+    	 * Get the list of all the ore locations in the vein.
+    	 * 
+    	 * @return A list of ore locs.
+    	 */
+    	public List<OreLocation> getVein() {
+    		return vein;
+    	}
+    }
+    
+    /**
+     * Ore location class.
+     * 
+     * @author dans1988, modified by LeonTG77.
+     */
+    public class OreLocation {
+    	private final int x;
+    	private final int y;
+    	private final int z;
+
+    	private final int hashCode;
+
+    	public OreLocation(int x, int y, int z) {
+    		this.x = x;
+    		this.y = y;
+    		this.z = z;
+
+    		hashCode = Objects.hash(x, y, z);
+    	}
+
+    	/**
+    	 * Get the X coord of the OreLocation.
+    	 * 
+    	 * @return The X coord.
+    	 */
+    	public int getX() {
+    		return x;
+    	}
+
+    	/**
+    	 * Get the Y coord of the OreLocation.
+    	 * 
+    	 * @return The Y coord.
+    	 */
+    	public int getY() {
+    		return y;
+    	}
+
+    	/**
+    	 * Get the Z coord of the OreLocation.
+    	 * 
+    	 * @return The Z coord.
+    	 */
+    	public int getZ() {
+    		return z;
+    	}
+
+    	@Override
+    	public int hashCode() {
+    		return hashCode;
+    	}
+
+    	@Override
+    	public boolean equals(Object obj) {
+    		if (obj == this) {
+    			return true;
+    		}
+
+    		if (obj == null) {
+    			return false;
+    		}
+
+    		if (!(obj instanceof OreLocation)) {
+    			return false;
+    		}
+
+    		OreLocation other = (OreLocation) obj;
+
+    		return Objects.equals(this.x, other.x) && Objects.equals(this.y, other.y) && Objects.equals(this.z, other.z);
+    	}
     }
 }
