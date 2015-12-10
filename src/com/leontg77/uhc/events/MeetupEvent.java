2ad3fe846accff5047d7bfac872a2754afc26b88
LeonTG77 <leontg77@gmail.com>
2015-12-10 23:36:53 +0100

    Added custom events, so scenarios can enable when they should enable :D

---
+++ src/com/leontg77/uhc/events/MeetupEvent.java
@@ -0,0 +1,14 @@
+package com.leontg77.uhc.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class MeetupEvent extends Event {
+
+	@Override
+	public HandlerList getHandlers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
