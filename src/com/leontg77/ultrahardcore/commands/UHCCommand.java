fd03f0a3bc1a4594c1b7c21fc140bf7fdf800c03
LeonTG77 <leontg77@gmail.com>
2015-12-13 01:24:34 +0100

    Added parsers

--- src/com/leontg77/ultrahardcore/commands/UHCCommand.java
+++ src/com/leontg77/ultrahardcore/commands/UHCCommand.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.bukkit.command.CommandSender;
 
+import com.leontg77.ultrahardcore.utils.NameUtils;
+
 /**
  * Super class for commands.
  * 
@@ -38,4 +40,52 @@ public abstract class UHCCommand {
 	public abstract boolean execute(CommandSender sender, String[] args) throws CommandException;
 	
 	public abstract List<String> tabComplete(CommandSender sender, String[] args);
+	
+	public int parseInt(String parse) throws CommandException {
+		return parseInt(parse, "number");
+	}
+	
+	public double parseDouble(String parse) throws CommandException {
+		return parseDouble(parse, "number");
+	}
+	
+	public long parseLong(String parse) throws CommandException {
+		return parseLong(parse, "number");
+	}
+	
+	public int parseInt(String parse, String criteria) throws CommandException {
+		try {
+			return Integer.parseInt(parse);
+		} catch (Exception e) {
+			throw new CommandException("'" + parse + "' is not a vaild " + criteria + ".");
+		}
+	}
+	
+	public double parseDouble(String parse, String criteria) throws CommandException {
+		try {
+			return Double.parseDouble(parse);
+		} catch (Exception e) {
+			throw new CommandException("'" + parse + "' is not a vaild " + criteria + ".");
+		}
+	}
+	
+	public long parseLong(String parse, String criteria) throws CommandException {
+		try {
+			return Long.parseLong(parse);
+		} catch (Exception e) {
+			throw new CommandException("'" + parse + "' is not a vaild " + criteria + ".");
+		}
+	}
+	
+	public boolean parseBoolean(String parse, String criteria) throws CommandException {
+		if (parse.equalsIgnoreCase("true") || parse.equalsIgnoreCase("on")) {
+			return true;
+		} 
+
+		if (parse.equalsIgnoreCase("false") || parse.equalsIgnoreCase("off")) {
+			return false;
+		}
+
+		throw new CommandException(NameUtils.fixString(criteria, false) + " can only be 'true' or 'false', not '" + parse + "'.");
+	}
 }
