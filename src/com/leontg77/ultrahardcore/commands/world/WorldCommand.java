e63e3423a9805d34f4c1560262ee46e9c24f26b1
LeonTG77 <leontg77@gmail.com>
2015-12-13 01:19:02 +0100

    Redid the entire class and made it use the new UHCCommand superclass

--- src/com/leontg77/ultrahardcore/commands/world/WorldCommand.java
+++ src/com/leontg77/ultrahardcore/commands/world/WorldCommand.java
@@ -1,5 +1,6 @@
 package com.leontg77.ultrahardcore.commands.world;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,7 +13,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.leontg77.ultrahardcore.Main;
+import com.leontg77.ultrahardcore.commands.CommandException;
 import com.leontg77.ultrahardcore.commands.UHCCommand;
+import com.leontg77.ultrahardcore.utils.GameUtils;
 import com.leontg77.ultrahardcore.worlds.WorldManager;
 
 /**
@@ -27,89 +30,47 @@ public class WorldCommand extends UHCCommand {
 	}
 
 	@Override
-	public boolean execute(CommandSender sender, String[] args) {
+	public boolean execute(CommandSender sender, String[] args) throws CommandException {
+		WorldManager manager = WorldManager.getInstance();
+		
 		if (args.length > 0) {
-			WorldManager manager = WorldManager.getInstance();
-			
 			if (args[0].equalsIgnoreCase("create")) {
 				if (args.length < 5) {
-					sender.sendMessage(Main.PREFIX + "Usage: /world create <host> <mapradius> <nether> <end> [worldtype] [seed]");
+					sender.sendMessage(Main.PREFIX + "Usage: /world create <host> <mapradius> <nether> <end> [seed]");
 					return true;
 				}
 				
-				String worldname;
-				int radius;
-				long seed = 0;
-				WorldType worldtype = WorldType.NORMAL;
-				boolean nether;
-				boolean end;
+				String worldname = args[1];
+				int radius = parseInt(args[2], "radius");
 				
-				worldname = args[1];
+				boolean nether = parseBoolean(args[3], "nether");
+				boolean end = parseBoolean(args[4], "The End");
 				
-				try {
-					radius = Integer.parseInt(args[2]);
-				} catch (Exception e) {
-					sender.sendMessage(ChatColor.RED + args[2] + " is not a vaild radius.");
-					return true;
-				}
-				
-				if (args[3].equalsIgnoreCase("true")) {
-					nether = true;
-				} else if (args[3].equalsIgnoreCase("false")) {
-					nether = false;
-				} else {
-					sender.sendMessage(ChatColor.RED + "Nether can only be true or false.");
-					return true;
-				}
-				
-				if (args[4].equalsIgnoreCase("true")) {
-					end = true;
-				} else if (args[4].equalsIgnoreCase("false")) {
-					end = false;
-				} else {
-					sender.sendMessage(ChatColor.RED + "End can only be true or false.");
-					return true;
-				}
+				long seed = new Random().nextLong();
 				
 				if (args.length > 5) {
 					try {
-						worldtype = WorldType.valueOf(args[5].toUpperCase());
-					} catch (Exception e) {
-						sender.sendMessage(ChatColor.RED + args[5] + " is not an vaild worldtype.");
-						return true;
-					}
-				}
-				
-				if (args.length > 6) {
-					try {
-						seed = Long.parseLong(args[6]);
-						
-						if (seed == 0) {
-							throw new Exception();
-						}
-					} catch (Exception e) {
-						sender.sendMessage(ChatColor.RED + args[6] + " is not a vaild seed.");
-						return true;
-					}
+						seed = parseLong(args[5]);
+		            } catch (Exception e) {
+		                seed = (long) args[5].hashCode();
+		            }
 				}
 				
 				sender.sendMessage(Main.PREFIX + "Starting creation of world '§a" + worldname + "§7'...");
 				sender.sendMessage("§8» §7Radius: §6" + radius);
-				sender.sendMessage("§8» §7Seed: §6" + (seed == 0 ? "Random" : seed));
-				sender.sendMessage("§8» §7Type: §6" + worldtype);
+				sender.sendMessage("§8» §7Seed: §6" + seed);
+				sender.sendMessage("§8» §7Type: §6" + WorldType.NORMAL);
 				sender.sendMessage("§8» §7Nether: §6" + nether);
 				sender.sendMessage("§8» §7End: §6" + end);
 				
-				long finalSeed = seed == 0 ? new Random().nextLong() : seed;
-				
-				manager.createWorld(worldname, radius, finalSeed, Environment.NORMAL, worldtype);
+				manager.createWorld(worldname, radius, seed, Environment.NORMAL, WorldType.NORMAL);
 				
 				if (nether) {
-					manager.createWorld(worldname + "_nether", radius, finalSeed, Environment.NETHER, worldtype);
+					manager.createWorld(worldname + "_nether", radius, seed, Environment.NETHER, WorldType.NORMAL);
 				}
 				
 				if (end) {
-					manager.createWorld(worldname + "_end", radius, finalSeed, Environment.THE_END, worldtype);
+					manager.createWorld(worldname + "_end", radius, seed, Environment.THE_END, WorldType.NORMAL);
 				}
 
 				sender.sendMessage(Main.PREFIX + "World creation finished.");
@@ -122,30 +83,27 @@ public class WorldCommand extends UHCCommand {
 					return true;
 				}
 				
-				World world = Bukkit.getServer().getWorld(args[1]);
+				World world = Bukkit.getWorld(args[1]);
 				
 				if (world == null) {
-					sender.sendMessage(ChatColor.RED + args[1] + " does not exist.");
-					return true;
+					throw new CommandException("The world '" + args[1] + "'  does not exist.");
 				}
 				
 				if (world.getName().equals("lobby")) {
-					sender.sendMessage(ChatColor.RED + "You cannot delete the lobby world.");
-					return true;
+					throw new CommandException("You cannot delete the lobby world.");
 				}
 
-				if (manager.deleteWorld(world)) {
-					sender.sendMessage(Main.PREFIX + "World '§a" + world.getName() + "§7' has been deleted.");
-				} else {
-					sender.sendMessage(Main.PREFIX + "Couldn't delete '§a" + world.getName() + "§7'.");
+				if (!manager.deleteWorld(world)) {
+					throw new CommandException("Could not delete world '" + world.getName() + "'.");
 				}
+				
+				sender.sendMessage(Main.PREFIX + "World '§a" + world.getName() + "§7' has been deleted.");
 				return true;
 			}
 			
 			if (args[0].equalsIgnoreCase("tp")) {
 				if (!(sender instanceof Player)) {
-					sender.sendMessage(ChatColor.RED + "Only players can teleport to worlds.");
-					return true;
+					throw new CommandException("Only players can teleport to worlds.");
 				}
 				
 				Player player = (Player) sender;
@@ -155,11 +113,10 @@ public class WorldCommand extends UHCCommand {
 					return true;
 				}
 				
-				World world = Bukkit.getServer().getWorld(args[1]);
+				World world = Bukkit.getWorld(args[1]);
 				
 				if (world == null) {
-					player.sendMessage(ChatColor.RED + args[1] + " does not exist.");
-					return true;
+					throw new CommandException("The world '" + args[1] + "'  does not exist.");
 				}
 				
 				player.sendMessage(Main.PREFIX + "Teleported to world '§a" + world.getName() + "§7'.");
@@ -168,7 +125,7 @@ public class WorldCommand extends UHCCommand {
 			}
 			
 			if (args[0].equalsIgnoreCase("list")) {
-				sender.sendMessage(Main.PREFIX + "List of all worlds:");
+				sender.sendMessage(Main.PREFIX + "List of all worlds: §8(§6" + Bukkit.getWorlds().size() + "§8)");
 				
 				for (World world : Bukkit.getWorlds()) {
 					ChatColor color;
@@ -198,19 +155,14 @@ public class WorldCommand extends UHCCommand {
 					return true;
 				}
 				
-				World world = Bukkit.getServer().getWorld(args[1]);
+				World world = Bukkit.getWorld(args[1]);
 				
 				if (world != null) {
-					sender.sendMessage(ChatColor.RED + args[1] + " is already loaded.");
-					return true;
+					throw new CommandException("The world '" + args[1] + "'  is already loaded.");
 				}
 				
-				try {
-					manager.loadWorld(args[1]);
-					sender.sendMessage(Main.PREFIX + "World '§a" + args[1] + "§7' has been loaded.");
-				} catch (Exception e) {
-					sender.sendMessage(ChatColor.RED + "This world doesn't exist.");
-				}
+				manager.loadWorld(args[1]);
+				sender.sendMessage(Main.PREFIX + "World '§a" + args[1] + "§7' has been loaded.");
 				return true;
 			}
 			
@@ -220,16 +172,14 @@ public class WorldCommand extends UHCCommand {
 					return true;
 				}
 				
-				World world = Bukkit.getServer().getWorld(args[1]);
+				World world = Bukkit.getWorld(args[1]);
 				
 				if (world == null) {
-					sender.sendMessage(ChatColor.RED + args[1] + " does not exist.");
-					return true;
+					throw new CommandException("The world '" + args[1] + "'  does not exist.");
 				}
 				
 				if (world == Bukkit.getWorlds().get(0)) {
-					sender.sendMessage(ChatColor.RED + "You cannot unload the main world.");
-					return true;
+					throw new CommandException("You cannot unload the lobby world.");
 				}
 				
 				sender.sendMessage(Main.PREFIX + "World '§a" + world.getName() + "§7' has been unloaded.");
@@ -239,17 +189,73 @@ public class WorldCommand extends UHCCommand {
 		}
 		
 		sender.sendMessage(Main.PREFIX + "World management help:");
-		sender.sendMessage("§8» §a/world create §8- §f§oCreate a world.");
-		sender.sendMessage("§8» §a/world delete §8- §f§oDelete a world.");
-		sender.sendMessage("§8» §a/world load §8- §f§oLoad a world.");
-		sender.sendMessage("§8» §a/world unload §8- §f§oUnload a world.");
-		sender.sendMessage("§8» §a/world list §8- §f§oList all worlds.");
-		sender.sendMessage("§8» §a/world tp §8- §f§oTeleport to a world.");
+		sender.sendMessage("§8» §a/world create §8- §7§oCreate a world.");
+		sender.sendMessage("§8» §a/world delete §8- §7§oDelete a world.");
+		sender.sendMessage("§8» §a/world load §8- §7§oLoad a world.");
+		sender.sendMessage("§8» §a/world unload §8- §7§oUnload a world.");
+		sender.sendMessage("§8» §a/world list §8- §7§oList all worlds.");
+		sender.sendMessage("§8» §a/world tp §8- §7§oTeleport to a world.");
 		return true;
 	}
 
 	@Override
 	public List<String> tabComplete(CommandSender sender, String[] args) {
-		return null;
+		List<String> toReturn = new ArrayList<String>();
+		
+		if (args.length == 1) {
+			toReturn.add("create");
+			toReturn.add("delete");
+			toReturn.add("list");
+			toReturn.add("tp");
+			toReturn.add("load");
+			toReturn.add("unload");
+		}
+		
+		if (args.length == 2) {
+			switch (args[0].toLowerCase()) {
+			case "create":
+				for (String name : GameUtils.HOSTS_BY_NAME.keySet()) {
+					toReturn.add(name.toLowerCase());
+				}
+				break;
+			case "load":
+			case "unload":
+			case "tp":
+			case "delete":
+				for (World world : Bukkit.getWorlds()) {
+					toReturn.add(world.getName());
+				}
+				break;
+			}
+		}
+		
+		if (args.length == 3) {
+			if (args[0].equalsIgnoreCase("create")) {
+				toReturn.add("1500");
+				toReturn.add("1000");
+			}
+		}
+		
+		if (args.length == 4) {
+			if (args[0].equalsIgnoreCase("create")) {
+				toReturn.add("true");
+				toReturn.add("false");
+			}
+		}
+		
+		if (args.length == 5) {
+			if (args[0].equalsIgnoreCase("create")) {
+				toReturn.add("true");
+				toReturn.add("false");
+			}
+		}
+		
+		if (args.length == 6) {
+			if (args[0].equalsIgnoreCase("create")) {
+				toReturn.add("" + new Random().nextLong());
+			}
+		}
+		
+		return toReturn;
 	}
 }
