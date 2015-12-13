b491e8d8c9e654749cb856932f61da6820e40cab
LeonTG77 <leontg77@gmail.com>
2015-12-13 21:37:29 +0100

    Added JavaDocs

--- src/com/leontg77/ultrahardcore/commands/UHCCommand.java
+++ src/com/leontg77/ultrahardcore/commands/UHCCommand.java
@@ -25,34 +25,102 @@ public abstract class UHCCommand {
 		this.name = name;
 	}
 	
+	/**
+	 * Get the name of the command used after the /
+	 * 
+	 * @return The command name.
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Get the usage of the command
+	 * <p>
+	 * Usage can be /nameofcommand [argurments...]
+	 * 
+	 * @return The command usage.
+	 */
 	public String getUsage() {
 		return "/" + name + " " + usage;
 	}
 	
+	/**
+	 * Return the permission of the command
+	 * <p>
+	 * The permission will be uhc.[nameofcommand]
+	 * 
+	 * @return The command permission.
+	 */
 	public String getPermission() {
 		return "uhc." + name;
 	}
 	
+	/**
+	 * Execute the command.
+	 * 
+	 * @param sender The sender of the command.
+	 * @param args The argurments typed after the command.
+	 * @return True if successful, false otherwise. Returning false will send usage to the sender.
+	 * 
+	 * @throws CommandException If anything was wrongly typed this is thrown sending the sender a warning.
+	 */
 	public abstract boolean execute(CommandSender sender, String[] args) throws CommandException;
 	
+	/**
+	 * Tab complete the command.
+	 * 
+	 * @param sender The sender of the command.
+	 * @param args The argurments typed after the command
+	 * @return A list of tab completable argurments.
+	 */
 	public abstract List<String> tabComplete(CommandSender sender, String[] args);
 	
+	/**
+	 * Parse an int from the given string.
+	 * 
+	 * @param parse The string to parse.
+	 * @return The int parsed.
+	 * 
+	 * @throws CommandException If the string typed isn't an int, this will send the sender a message.
+	 */
 	public int parseInt(String parse) throws CommandException {
 		return parseInt(parse, "number");
 	}
 	
+	/**
+	 * Parse an double from the given string.
+	 * 
+	 * @param parse The string to parse.
+	 * @return The double parsed.
+	 * 
+	 * @throws CommandException If the string typed isn't an double, this will send the sender a message.
+	 */
 	public double parseDouble(String parse) throws CommandException {
 		return parseDouble(parse, "number");
 	}
 	
+	/**
+	 * Parse an long from the given string.
+	 * 
+	 * @param parse The string to parse.
+	 * @return The long parsed.
+	 * 
+	 * @throws CommandException If the string typed isn't an long, this will send the sender a message.
+	 */
 	public long parseLong(String parse) throws CommandException {
 		return parseLong(parse, "number");
 	}
 	
+	/**
+	 * Parse an int from the given string.
+	 * 
+	 * @param parse The string to parse.
+	 * @param criteria What the int is used for.
+	 * @return The int parsed.
+	 * 
+	 * @throws CommandException If the string typed isn't an int, this will send the sender a message.
+	 */
 	public int parseInt(String parse, String criteria) throws CommandException {
 		try {
 			return Integer.parseInt(parse);
@@ -61,6 +129,15 @@ public abstract class UHCCommand {
 		}
 	}
 	
+	/**
+	 * Parse an double from the given string.
+	 * 
+	 * @param parse The string to parse.
+	 * @param criteria What the double is used for.
+	 * @return The double parsed.
+	 * 
+	 * @throws CommandException If the string typed isn't an double, this will send the sender a message.
+	 */
 	public double parseDouble(String parse, String criteria) throws CommandException {
 		try {
 			return Double.parseDouble(parse);
@@ -69,6 +146,15 @@ public abstract class UHCCommand {
 		}
 	}
 	
+	/**
+	 * Parse an long from the given string.
+	 * 
+	 * @param parse The string to parse.
+	 * @param criteria What the long is used for.
+	 * @return The long parsed.
+	 * 
+	 * @throws CommandException If the string typed isn't an long, this will send the sender a message.
+	 */
 	public long parseLong(String parse, String criteria) throws CommandException {
 		try {
 			return Long.parseLong(parse);
@@ -77,6 +163,15 @@ public abstract class UHCCommand {
 		}
 	}
 	
+	/**
+	 * Parse an boolean from the given string.
+	 * 
+	 * @param parse The string to parse.
+	 * @param criteria What the boolean is used for.
+	 * @return The boolean parsed.
+	 * 
+	 * @throws CommandException If the string typed isn't an boolean, this will send the sender a message.
+	 */
 	public boolean parseBoolean(String parse, String criteria) throws CommandException {
 		if (parse.equalsIgnoreCase("true") || parse.equalsIgnoreCase("on")) {
 			return true;
@@ -86,6 +181,16 @@ public abstract class UHCCommand {
 			return false;
 		}
 
-		throw new CommandException(NameUtils.fixString(criteria, false) + " can only be 'true' or 'false', not '" + parse + "'.");
+		throw new CommandException(NameUtils.capitalizeString(criteria, false) + " can only be 'true' or 'false', not '" + parse + "'.");
+	}
+	
+	/**
+	 * Turn a the given boolean into "Enabled" or "Disabled".
+	 * 
+	 * @param converting The boolean converting.
+	 * @return The converted boolean.
+	 */
+	public String booleanToString(boolean converting) {
+		return converting ? "enabled" : "disabled";
 	}
 }
