bb771f3b3110bb4fdd12546635b34fca51a8b0dd
LeonTG77 <leontg77@gmail.com>
2015-12-13 21:38:10 +0100

    Update to work with the new UHCCommand super class

--- src/com/leontg77/ultrahardcore/commands/world/BorderCommand.java
+++ src/com/leontg77/ultrahardcore/commands/world/BorderCommand.java
@@ -1,16 +1,17 @@
 package com.leontg77.ultrahardcore.commands.world;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.World;
-import org.bukkit.World.Environment;
 import org.bukkit.WorldBorder;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.leontg77.ultrahardcore.Main;
+import com.leontg77.ultrahardcore.commands.CommandException;
+import com.leontg77.ultrahardcore.commands.UHCCommand;
 import com.leontg77.ultrahardcore.utils.PlayerUtils;
 
 /**
@@ -18,69 +19,68 @@ import com.leontg77.ultrahardcore.utils.PlayerUtils;
  * 
  * @author LeonTG77
  */
-public class BorderCommand implements CommandExecutor {
+public class BorderCommand extends UHCCommand {
+
+	public BorderCommand() {
+		super("border", "<world> <size>");
+	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean execute(CommandSender sender, String[] args) throws CommandException {
 		if (args.length == 0) {
-			if (sender instanceof Player) {
-				Player player = (Player) sender;
-				World world = player.getWorld();
-				
-				WorldBorder border = world.getWorldBorder();
-				int size = (int) border.getSize();
-				
-				sender.sendMessage(Main.PREFIX + "The border is currently: §a" + size + "x" + size);
-				return true;
+			if (!(sender instanceof Player)) {
+				return false;
 			}
+
+			Player player = (Player) sender;
+			World world = player.getWorld();
 			
-			sender.sendMessage(Main.PREFIX + "Usage: /border <world> [size]");
+			WorldBorder border = world.getWorldBorder();
+			int size = (int) border.getSize();
+			
+			sender.sendMessage(Main.PREFIX + "The border is currently: §a" + size + "x" + size);
 			return true;
 		}
 		
-		if (!sender.hasPermission("uhc.border")) {
+		if (!sender.hasPermission("uhc.border.set")) {
 			sender.sendMessage(Main.NO_PERM_MSG);
 			return true;
+		}
+		
+		if (args.length == 1) {
+			return false;
 		}
 		
 		World world = Bukkit.getWorld(args[0]);
 		
 		if (world == null) {
-			sender.sendMessage(ChatColor.RED + "There are no worlds with the name " + args[0]);
-			return true;
+			throw new CommandException("The world '" + args[0] + "'  does not exist.");
 		}
 
 		WorldBorder border = world.getWorldBorder();
 		
-		if (args.length == 1) {
-			int size = (int) border.getSize();
-			
-			sender.sendMessage(Main.PREFIX + "The border in '§6" + world.getName() + "§7' is currently: §a" + size + "x" + size);
-			return true;
-		}
-		
-		int radius;
-		
-		try {
-			radius = Integer.parseInt(args[1]);
-		} catch (Exception e) {
-			sender.sendMessage(ChatColor.RED + args[1] + " is not an vaild radius.");
-			return true;
-		}
-
+		int radius = parseInt(args[1], "radius");
 		border.setSize(radius);
-		border.setWarningDistance(0);
-		border.setWarningTime(60);
-		border.setDamageAmount(0.1);
-		border.setDamageBuffer(0);
-		
-		if (world.getEnvironment() == Environment.NETHER) {
-			border.setCenter(0.5, 0.5);
-		} else {
-			border.setCenter(0.0, 0.0);
-		}
 		
 		PlayerUtils.broadcast(Main.PREFIX + "Borders in world '§6" + world.getName() + "§7' has been setup with radius §a" + radius + "x" + radius + "§7.");
 		return true;
+	}
+
+	@Override
+	public List<String> tabComplete(CommandSender sender, String[] args) {
+		List<String> toReturn = new ArrayList<String>();
+    	
+		if (args.length == 1) {
+        	for (World world : Bukkit.getWorlds()) {
+        		toReturn.add(world.getName());
+        	}
+        }
+		
+		if (args.length == 2) {
+        	toReturn.add("3000");
+        	toReturn.add("2000");
+        }
+		
+		return toReturn;
 	}
 }
