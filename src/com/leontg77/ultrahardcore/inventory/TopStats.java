075c5b835bd30e64270f23f2a17939a7335a84b8
LeonTG77 <leontg77@gmail.com>
2015-12-15 09:56:24 +0100

    Added listeners and made the command 1 loop faster

--- src/com/leontg77/ultrahardcore/inventory/TopStats.java
+++ src/com/leontg77/ultrahardcore/inventory/TopStats.java
@@ -3,11 +3,16 @@ package com.leontg77.ultrahardcore.inventory;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
@@ -16,28 +21,69 @@ import com.leontg77.ultrahardcore.User.Stat;
 import com.leontg77.ultrahardcore.utils.FileUtils;
 import com.leontg77.ultrahardcore.utils.NumberUtils;
 
-public class TopStats extends InvGUI {
+/**
+ * Top stats inventory class.
+ * 
+ * @author LeonTG77
+ */
+public class TopStats extends InvGUI implements Listener {
 	private Inventory inv = Bukkit.createInventory(null, 45, "Top 10 Stats");
+	
+	@EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {	
+        if (event.getCurrentItem() == null) {
+        	return;
+        }
+        
+		Inventory inv = event.getInventory();
+		
+		if (!inv.getTitle().equals(this.inv.getTitle())) {
+			return;
+		}
+		
+		event.setCancelled(true);
+	}
 
+	/**
+	 * Get the top stats inventory.
+	 * 
+	 * @return The inventory.
+	 */
 	public Inventory get() {
+		// This isn't that laggy so updating it is fine.
 		update();
 		
 		return inv;
 	}
 
+	/**
+	 * Update the top stats inventory.
+	 */
 	public void update() {
 		List<String> data = new ArrayList<String>();
-		
 		int slot = 0;
+
+		Map<String, Integer> deaths = new HashMap<String, Integer>();
+		Map<String, Integer> kills = new HashMap<String, Integer>();
+		
+		List<FileConfiguration> files = FileUtils.getUserFiles();
 		
 		for (Stat stat : Stat.values()) {
 			data.clear();
 			
-			for (FileConfiguration config : FileUtils.getUserFiles()) {
+			for (FileConfiguration config : files) {
 				String name = config.getString("username");
 				int number = config.getInt("stats." + stat.name().toLowerCase());
 				
 				data.add(number + " " + name);
+
+				if (stat == Stat.KILLS) {
+					kills.put(name, number);
+				}
+				
+				if (stat == Stat.DEATHS) {
+					deaths.put(name, number);
+				}
 			}
 
 			Collections.sort(data, new Comparator<String>() {
@@ -58,19 +104,17 @@ public class TopStats extends InvGUI {
 		
 		data.clear();
 		
-		for (FileConfiguration config : FileUtils.getUserFiles()) {
-			String name = config.getString("username");
-			
-			int deaths = config.getInt("stats.deaths");
-			int kills = config.getInt("stats.kills");
+		for (String name : kills.keySet()) {
+			int d = deaths.get(name);
+			int k = kills.get(name);
 			
 			double kdr;
 			
-			if (deaths < 2) {
+			if (d < 2) {
 				continue;
-			} else {
-				kdr = ((double) kills) / ((double) deaths);
-			}
+			} 
+			
+			kdr = ((double) k) / ((double) d);
 			
 			data.add(NumberUtils.convertDouble(kdr) + " " + name);
 		}
