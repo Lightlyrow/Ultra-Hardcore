8f0e2e97d8c84b145e38dc07b804b10efdb91c4f
LeonTG77 <leontg77@gmail.com>
2015-12-23 17:36:37 +0100

    Added the death and kill stats listening

--- src/com/leontg77/ultrahardcore/listeners/StatsListener.java
+++ src/com/leontg77/ultrahardcore/listeners/StatsListener.java
@@ -1,7 +1,66 @@
 package com.leontg77.ultrahardcore.listeners;
 
+import java.util.List;
+
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.scoreboard.Team;
+
+import com.leontg77.ultrahardcore.Arena;
+import com.leontg77.ultrahardcore.User;
+import com.leontg77.ultrahardcore.User.Stat;
+import com.leontg77.ultrahardcore.managers.BoardManager;
+import com.leontg77.ultrahardcore.managers.TeamManager;
+import com.leontg77.ultrahardcore.utils.GameUtils;
 
 public class StatsListener implements Listener {
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerDeath(final PlayerDeathEvent event) {
+		final Player player = event.getEntity();
+		final Arena arena = Arena.getInstance();
+		
+		// the arena has it's own way of doing deaths.
+		if (arena.isEnabled() && arena.hasPlayer(player)) {
+			return;
+		} 
+
+		final List<World> worlds = GameUtils.getGameWorlds();
+		
+	    // I don't care about the rest they're not in a game world.
+	    if (!worlds.contains(player.getWorld())) {
+	    	return;
+	    }
+
+		final BoardManager board = BoardManager.getInstance();
+		final User user = User.get(player);
+		
+		final Player killer = player.getKiller();
+
+		if (killer == null) {
+			user.increaseStat(Stat.DEATHS);
+			return;
+		}
+		
+		final Team pteam = TeamManager.getInstance().getTeam(player);
+		final Team team = TeamManager.getInstance().getTeam(killer);
+		
+		if (pteam != null && pteam.equals(team)) {
+			return;
+		}
+		
+		user.increaseStat(Stat.DEATHS);
+		
+		final User killUser = User.get(killer);
+		killUser.increaseStat(Stat.KILLS);
+		
+		if (killUser.getStat(Stat.KILLSTREAK) < board.getScore(killer.getName())) {
+			killUser.setStat(Stat.KILLSTREAK, board.getScore(killer.getName()));
+		}
+	}
 
 }
