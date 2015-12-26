13513cb8ee28a3604b09c04e114d0bcb8d061775
LeonTG77 <leontg77@gmail.com>
2015-12-27 00:40:31 +0100

    Made the command use the new super class

--- src/com/leontg77/ultrahardcore/commands/spectate/SpecChatCommand.java
+++ src/com/leontg77/ultrahardcore/commands/spectate/SpecChatCommand.java
@@ -1,13 +1,19 @@
 package com.leontg77.ultrahardcore.commands.spectate;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.google.common.base.Joiner;
 import com.leontg77.ultrahardcore.Main;
 import com.leontg77.ultrahardcore.Spectator;
+import com.leontg77.ultrahardcore.commands.CommandException;
+import com.leontg77.ultrahardcore.commands.UHCCommand;
 import com.leontg77.ultrahardcore.utils.PlayerUtils;
 
 /**
@@ -15,43 +21,43 @@ import com.leontg77.ultrahardcore.utils.PlayerUtils;
  * 
  * @author LeonTG77
  */
-public class SpecChatCommand implements CommandExecutor {
+public class SpecChatCommand extends UHCCommand {
+
+	public SpecChatCommand() {
+		super("specchat", "<message>");
+	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean execute(CommandSender sender, String[] args) throws CommandException {
 		if (!(sender instanceof Player)) {
-			sender.sendMessage(ChatColor.RED + "Only players can talk in spec-chat.");
-			return true;
+			throw new CommandException("Only players can talk in the spectator chat.");
 		}
 		
         Spectator spec = Spectator.getInstance();
         Player player = (Player) sender;
         
 		if (!spec.isSpectating(player)) {
-			sender.sendMessage(Main.PREFIX + "You are not a spectator.");
-			return true;
+			throw new CommandException("You can only do this while spectating.");
 		}
 		
 		if (args.length == 0) {
-    		sender.sendMessage(Main.PREFIX + "Usage: /sc <message>");
-        	return true;
+        	return false;
         } 
         
-    	StringBuilder message = new StringBuilder("");
-		
-		for (int i = 0; i < args.length; i++) {
-			message.append(args[i]).append(" ");
-		}
-
-        String msg = "§8[§5SpecChat§8] §7" + sender.getName() + "§8 » §f" + message.toString().trim();
+        String msg = Joiner.on(' ').join(Arrays.copyOfRange(args, 0, args.length));
         
         for (Player online : PlayerUtils.getPlayers()) {
         	if (!spec.isSpectating(online)) {
         		continue;
         	}
         	
-        	online.sendMessage(msg);
+        	online.sendMessage("§8[§5SpecChat§8] §7" + sender.getName() + "§8 » §f" + msg);
         }
 		return true;
+	}
+
+	@Override
+	public List<String> tabComplete(CommandSender sender, String[] args) {
+		return null;
 	}
 }
