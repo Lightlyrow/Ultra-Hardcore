62a08ef4fe64a11f9213c33dda282dfd51015507
LeonTG77 <leontg77@gmail.com>
2015-12-28 01:23:22 +0100

    Moved class and converted to using the new UHCCommand super class

---
+++ src/com/leontg77/ultrahardcore/commands/spectate/TpCommand.java
@@ -0,0 +1,79 @@
+package com.leontg77.ultrahardcore.commands.spectate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.leontg77.ultrahardcore.Main;
+import com.leontg77.ultrahardcore.Spectator;
+import com.leontg77.ultrahardcore.commands.CommandException;
+import com.leontg77.ultrahardcore.commands.UHCCommand;
+
+/**
+ * Tp command class
+ * 
+ * @author LeonTG77
+ */
+public class TpCommand extends UHCCommand {
+
+	public TpCommand() {
+		super("tp", "<player> [player]");
+	}
+
+	@Override
+	public boolean execute(CommandSender sender, String[] args) throws CommandException {
+		Spectator spec = Spectator.getInstance();
+		
+		if (!sender.hasPermission("uhc.tp.bypass") && !spec.isSpectating(sender.getName())) {
+			throw new CommandException("You can only do this while spectating.");
+		}
+		
+		if (args.length == 0) {
+			return false;
+		}
+		
+		Player targetOne = Bukkit.getPlayer(args[0]);
+		
+		if (targetOne == null) {
+			throw new CommandException("'" + args[0] + "' is not online.");
+		}
+		
+		if (args.length == 1) {
+			if (!(sender instanceof Player)) {
+				throw new CommandException("Only players can teleport to players.");
+			}
+
+			Player player = (Player) sender;
+			
+			player.sendMessage(Main.PREFIX + "You teleported to §a" + targetOne.getName() + "§7.");
+			player.teleport(targetOne);
+			return true;
+		}
+		
+		if (!sender.hasPermission("uhc.tp.other")) {
+			throw new CommandException("You cannot teleport players to another player.");
+		}
+		
+		Player targetTwo = Bukkit.getPlayer(args[1]);
+		
+		if (targetTwo == null) {
+			throw new CommandException("'" + args[1] + "' is not online.");
+		}
+		
+		sender.sendMessage(Main.PREFIX + "You teleported §a" + targetOne.getName() + "§7 to §a" + targetTwo.getName() + "§7.");
+		targetOne.teleport(targetTwo);
+		return true;
+	}
+
+	@Override
+	public List<String> tabComplete(CommandSender sender, String[] args) {
+		if (args.length < 3) {
+			return null;
+		}
+		
+		return new ArrayList<String>();
+	}
+}
