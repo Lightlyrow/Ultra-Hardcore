0f8d6ebd5edec281e9e22684cc150cd11ea7e3c5
LeonTG77 <leontg77@gmail.com>
2016-01-04 20:02:41 +0100

    Changed to using the new super class and general code improvement

--- src/com/leontg77/ultrahardcore/commands/game/HelpopCommand.java
+++ src/com/leontg77/ultrahardcore/commands/game/HelpopCommand.java
@@ -1,19 +1,20 @@
 package com.leontg77.ultrahardcore.commands.game;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Sound;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import com.google.common.base.Joiner;
 import com.leontg77.ultrahardcore.Main;
 import com.leontg77.ultrahardcore.Spectator;
+import com.leontg77.ultrahardcore.commands.CommandException;
+import com.leontg77.ultrahardcore.commands.UHCCommand;
 import com.leontg77.ultrahardcore.utils.PlayerUtils;
 
 /**
@@ -21,45 +22,46 @@ import com.leontg77.ultrahardcore.utils.PlayerUtils;
  * 
  * @author LeonTG77
  */
-public class HelpopCommand implements CommandExecutor {
-	public static List<CommandSender> cooldown = new ArrayList<CommandSender>();
+public class HelpopCommand extends UHCCommand {
+	private static List<CommandSender> cooldown = new ArrayList<CommandSender>();
 	private static final String PREFIX = "§4§lHelp§8§l-§4§lOp §8» §7";
+	
+	public HelpopCommand() {
+		super("helpop", "<message>");
+	}
 
 	@Override
-	public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean execute(final CommandSender sender, String[] args) throws CommandException {
 		if (args.length == 0) {
-           	sender.sendMessage(ChatColor.RED + "Usage: /helpop <message>");
-           	return true;
+           	return false;
 		}
 		
 		if (cooldown.contains(sender)) {
-			sender.sendMessage(PREFIX + "Do not spam helpops.");
-			return true;
+			throw new CommandException("Do not spam helpops.");
 		}
 		
+		String msg = Joiner.on(' ').join(Arrays.copyOfRange(args, 0, args.length));
 		Spectator spec = Spectator.getInstance();
-		StringBuilder sb = new StringBuilder("");
-		
-		for (int i = 0; i < args.length; i++) {
-			sb.append(args[i]).append(" ");
-		}
-		
-		String msg = sb.toString().trim();
 
 		for (Player online : PlayerUtils.getPlayers()) {
 			if (!online.hasPermission("uhc.staff") && !spec.isSpectating(online)) {
 				continue;
 			}
 			
-			online.sendMessage("§4§lHelp§8§l-§4§lOp §8» §7" + sender.getName() + "§7: §6" + msg);
+			if (online == sender) {
+				continue;
+			}
+			
+			online.sendMessage(PREFIX + sender.getName() + "§7: §6" + msg);
 			online.playSound(online.getLocation(), Sound.NOTE_PLING, 1, 1);
 		}
 		
-		Bukkit.getLogger().info("§4Help§8-§4Op §8» §7" + sender.getName() + "§7: §6" + msg);
+		Bukkit.getLogger().info(PREFIX.replaceAll("§l", "") + sender.getName() + "§7: §6" + msg);
 		cooldown.add(sender);
-		
-		sender.sendMessage("§4§lHelp§8§l-§4§lOp §8» §7Helpop sent, please don't spam this.");
-		sender.sendMessage("§4§lHelp§8§l-§4§lOp §8» §7Your message: §6" + msg);
+
+		sender.sendMessage(PREFIX + sender.getName() + "§7: §6" + msg);
+		sender.sendMessage(PREFIX + "§7Helpop has been sent, please don't spam this :)");
+		sender.sendMessage(PREFIX + "§7Remember to check §a/uhc §7for other game info.");
 		
 		new BukkitRunnable() {
 			public void run() {
@@ -67,5 +69,10 @@ public class HelpopCommand implements CommandExecutor {
 			}
 		}.runTaskLater(Main.plugin, 100);
 		return true;
+	}
+
+	@Override
+	public List<String> tabComplete(CommandSender sender, String[] args) {
+		return null;
 	}
 }
