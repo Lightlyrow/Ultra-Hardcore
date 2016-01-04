007be8fe5763d3d06f16b609b1f5d377d3dfcd4a
LeonTG77 <leontg77@gmail.com>
2016-01-04 20:02:18 +0100

    Recoded the entire class

--- src/com/leontg77/ultrahardcore/scenario/scenarios/MysteryTeams.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/MysteryTeams.java
@@ -1,15 +1,21 @@
 package com.leontg77.ultrahardcore.scenario.scenarios;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.DyeColor;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,11 +24,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BannerMeta;
 
-import com.leontg77.ultrahardcore.Main;
-import com.leontg77.ultrahardcore.Spectator;
+import com.google.common.collect.ImmutableSet;
 import com.leontg77.ultrahardcore.events.PvPEnableEvent;
 import com.leontg77.ultrahardcore.scenario.Scenario;
 import com.leontg77.ultrahardcore.utils.GameUtils;
@@ -36,8 +42,8 @@ import com.leontg77.ultrahardcore.utils.PlayerUtils;
 public class MysteryTeams extends Scenario implements Listener, CommandExecutor {
 	private static final String PREFIX = "§6[§cMysteryTeams§6] §f";
 
-	private HashMap<MysteryTeam, ArrayList<String>> orgTeams = new HashMap<MysteryTeam, ArrayList<String>>();
-	private HashMap<MysteryTeam, ArrayList<String>> teams = new HashMap<MysteryTeam, ArrayList<String>>();
+	protected static List<MysteryTeam> teams = new ArrayList<MysteryTeam>(Arrays.asList(MysteryTeam.values()));
+	protected static Map<MysteryTeam, List<UUID>> orgTeams = new HashMap<MysteryTeam, List<UUID>>();
 
 	public MysteryTeams() {
 		super("MysteryTeams", "Teams are unknown until meeting and comparing banners.");
@@ -48,60 +54,40 @@ public class MysteryTeams extends Scenario implements Listener, CommandExecutor 
 	@Override
 	public void onDisable() {
 		orgTeams.clear();
-		teams.clear();
 	}
 
 	@Override
 	public void onEnable() {
-		orgTeams.clear();
-		teams.clear();
+		// this does not disable the scenario.
+		onDisable();
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (!isEnabled()) {
-			sender.sendMessage(PREFIX + "\"MysteryTeams\" is not enabled.");
+			sender.sendMessage(PREFIX + "MysteryTeams is not enabled.");
 			return true;
 		}
 		
 		if (args.length == 0) {
-			printHelp(sender);
-			return true;
+			return helpMenu(sender);
 		}
 		
 		if (args[0].equalsIgnoreCase("teamsize")) {
-			if (!(sender instanceof Player)) {
-				sender.sendMessage(ChatColor.RED + "Only players are on a team.");
-				return true;
+			sender.sendMessage(PREFIX + "Existing teamsizes:");
+			
+			for (MysteryTeam team : teams) {
+				sender.sendMessage(PREFIX + team.getChatColor() + team.getName() + "'s teamsize: §f" + team.getSize());
 			}
-			
-			Player player = (Player) sender;
-			MysteryTeam team = null;
-			
-			for (MysteryTeam t : teams.keySet()) {
-				if (teams.get(t).contains(player.getUniqueId().toString())) {
-					team = t;
-					break;
-				}
-			}
-			
-			if (team == null) {
-				sender.sendMessage(PREFIX + "You are not on a team.");
-				return true;
-			}
-			
-			sender.sendMessage(PREFIX + "Original Teamsize: §a" + orgTeams.get(team).size());
-			sender.sendMessage(PREFIX + "Current Teamsize: §a" + teams.get(team).size());
 		} 
 		
-		if (!sender.hasPermission("mysteryteams.admin")) {
-			sender.sendMessage(Main.NO_PERM_MSG);
-			return true;
+		if (!sender.hasPermission("uhc.mysteryteams")) {
+			return helpMenu(sender);
 		}
 		
 		if (args[0].equalsIgnoreCase("randomize")) {
 			if (args.length == 1) {
-				sender.sendMessage(PREFIX + "§7Usage: /mt randomize <teamsize>");
+				sender.sendMessage(PREFIX + "Usage: /mt randomize <teamsize> <amount of teams>");
 				return true;
 			}
 			
@@ -110,69 +96,66 @@ public class MysteryTeams extends Scenario implements Listener, CommandExecutor 
 			try {
 				teamSize = Integer.parseInt(args[1]);
 			} catch (Exception e) {
-				sender.sendMessage(ChatColor.RED + "Invaild team size.");
+				sender.sendMessage(ChatColor.RED + "'" + args[2] + "' is not a vaild teamsize.");
 				return true;
 			}
 			
-			ArrayList<Player> players = new ArrayList<Player>();
-			
-			for (Player online : PlayerUtils.getPlayers()) {
-				if (!isOnTeam(online) && !Spectator.getInstance().isSpectating(online) && GameUtils.getGameWorlds().contains(online.getWorld())) {
-					players.add(online);
-				}
-			}
-
-			Collections.shuffle(players);
-    		MysteryTeam t = null;
-			
-			for (MysteryTeam mt : MysteryTeam.values()) {
-    			if (!teams.containsKey(mt)) {
-    				teams.put(mt, new ArrayList<String>());
-    				orgTeams.put(mt, new ArrayList<String>());
-    				t = mt;
-    				break;
-    			}
-    			if (this.teams.get(mt).size() < 1)  {
-    				t = mt;
-    				break;
-    			}
-			}
+			int amount;
 			
 			try {
-				for (int i = 0; i < teamSize; i++) {
-					if (players.size() < i) {
-						sender.sendMessage(ChatColor.RED + "Could not add a player to team " + t.getName() + ".");
-						continue;
+				amount = Integer.parseInt(args[2]);
+			} catch (Exception e) {
+				sender.sendMessage(ChatColor.RED + "'" + args[2] + "' is not a vaild amount.");
+				return true;
+			}
+			
+			for (int i = 0; i < amount; i++) {
+				List<Player> players = new ArrayList<Player>();
+				
+				for (Player online : PlayerUtils.getPlayers()) {
+					if (getTeam(online) == null && GameUtils.getGamePlayers().contains(online)) {
+						players.add(online);
+					}
+				}
+
+				Collections.shuffle(players);
+	    		MysteryTeam team = findAvailableTeam();
+				
+	    		if (team == null) {
+					sender.sendMessage(ChatColor.RED + "There are no more available teams.");
+					break;
+	    		}
+	    		
+				for (int j = 0; j < teamSize; j++) {
+					if (players.isEmpty()) {
+						sender.sendMessage(ChatColor.RED + "No more players to add to the team.");
+						break;
 					}
 					
-					Player p = players.get(i);
+					Player player = players.remove(0);
 					
-		    		orgTeams.get(t).add(p.getUniqueId().toString());
-		    		this.teams.get(t).add(p.getUniqueId().toString());
+		    		orgTeams.get(team).add(player.getUniqueId());
+		    		team.addPlayer(player);
 				}
-			} catch (Exception e) {
-				sender.sendMessage(ChatColor.RED + "Not enough players for this team.");
 			}
-
-			if (this.teams.get(t).size() > 0) {
-				sender.sendMessage(PREFIX + "Created a §amTo" + teamSize + " §fusing team " + t.getChatColor() + t.getName() + "§f.");
-			}
+			
+			sender.sendMessage(PREFIX + "Randomized " + amount + " teams of " + teamSize + ".");
 			return true;
 		}
 		
 		if (args[0].equalsIgnoreCase("list")) {
-			if (orgTeams.size() <= 0) {
+			if (orgTeams.isEmpty()) {
 				sender.sendMessage(PREFIX + "There are no teams set.");
 				return true;
 			}
 
 			sender.sendMessage(PREFIX + "All teams: §o(Names in red means they are dead)");
 			
-			for (Entry<MysteryTeam, ArrayList<String>> team : orgTeams.entrySet()) { 
+			for (Entry<MysteryTeam, List<UUID>> team : orgTeams.entrySet()) { 
 				StringBuilder members = new StringBuilder();
 				int i = 1;
 				
-				for (String member : team.getValue()) {
+				for (UUID member : team.getValue()) {
 					if (members.length() > 0) {
 						if (i == team.getValue().size()) {
 							members.append(" §8and §f");
@@ -181,10 +164,12 @@ public class MysteryTeams extends Scenario implements Listener, CommandExecutor 
 						}
 					}
 					
-					if (teams.containsKey(team.getKey())) {
-						members.append(teams.get(team.getKey()).contains(member) ? ChatColor.GREEN + Bukkit.getOfflinePlayer(UUID.fromString(member)).getName() : ChatColor.RED + Bukkit.getOfflinePlayer(UUID.fromString(member)).getName());
+					OfflinePlayer offline = Bukkit.getOfflinePlayer(member);
+					
+					if (teams.contains(team.getKey())) {
+						members.append(team.getKey().hasPlayer(offline) ? ChatColor.GREEN + offline.getName() : ChatColor.RED + offline.getName());
 					} else {
-						members.append(ChatColor.RED + Bukkit.getOfflinePlayer(UUID.fromString(member)).getName());
+						members.append(ChatColor.RED + offline.getName());
 					}
 					i++;
 				}
@@ -195,80 +180,113 @@ public class MysteryTeams extends Scenario implements Listener, CommandExecutor 
 		}
 		
 		if (args[0].equalsIgnoreCase("give")) {
-			if (args.length > 1) {
-				Player target = Bukkit.getServer().getPlayer(args[1]);
+			if (args.length == 0) {
+				sender.sendMessage(PREFIX + "Gave item to everyone.");
 				
-				if (target == null) {
-					sender.sendMessage(ChatColor.RED + "That player is not online.");
-					return true;
-				}
-				
-				MysteryTeam team = null;
-				
-				for (MysteryTeam t : teams.keySet()) {
-					if (teams.get(t).contains(target.getUniqueId().toString())) {
-						team = t;
-						break;
+				for (Player online : PlayerUtils.getPlayers()) {
+					MysteryTeam team = getTeam(online);
+					
+					if (team == null) {
+						continue;
 					}
+					
+					ItemStack item = new ItemStack(Material.BANNER);
+		    		BannerMeta meta = (BannerMeta) item.getItemMeta();
+		    		meta.setBaseColor(team.getDyeColor());
+		    		item.setItemMeta(meta);
+		    		PlayerUtils.giveItem(online, item);
 				}
-				
-				if (team == null) {
-					sender.sendMessage(PREFIX + "That player is not on a team.");
-					return true;
-				}
-				
-				sender.sendMessage(PREFIX + "Gave item to player.");
-				
-				ItemStack item = new ItemStack(Material.BANNER);
-	    		BannerMeta meta = (BannerMeta) item.getItemMeta();
-	    		meta.setBaseColor(team.getDyeColor());
-	    		item.setItemMeta(meta);
-	    		PlayerUtils.giveItem(target, item);
-	    		return true;
+			}
+
+			Player target = Bukkit.getPlayer(args[1]);
+			
+			if (target == null) {
+				sender.sendMessage(ChatColor.RED + "'" + args[1] + "' is not online.");
+				return true;
 			}
 			
-			sender.sendMessage(PREFIX + "Gave item to everyone.");
+			MysteryTeam team = getTeam(target);
 			
-			for (Player online : PlayerUtils.getPlayers()) {
-				MysteryTeam team = null;
-				
-				for (MysteryTeam t : teams.keySet()) {
-					if (teams.get(t).contains(online.getUniqueId().toString())) {
-						team = t;
-						break;
-					}
-				}
-				
-				if (team == null) {
-					continue;
-				}
-				
-				ItemStack item = new ItemStack(Material.BANNER);
-	    		BannerMeta meta = (BannerMeta) item.getItemMeta();
-	    		meta.setBaseColor(team.getDyeColor());
-	    		item.setItemMeta(meta);
-	    		PlayerUtils.giveItem(online, item);
+			if (team == null) {
+				sender.sendMessage(ChatColor.RED + "'" + args[1] + "' is not on a team.");
+				return true;
 			}
-			return true;
+			
+			sender.sendMessage(PREFIX + "Gave item to player.");
+			
+			ItemStack item = new ItemStack(Material.BANNER);
+    		BannerMeta meta = (BannerMeta) item.getItemMeta();
+    		meta.setBaseColor(team.getDyeColor());
+    		item.setItemMeta(meta);
+    		PlayerUtils.giveItem(target, item);
+    		return true;
 		}
 		
-		printHelp(sender);
-		return true;
+		return helpMenu(sender);
+	}
+	
+	/**
+	 * Register a new fake team.
+	 * 
+	 * @return The fake team created.
+	 */
+	public static MysteryTeam findAvailableTeam() {
+		for (MysteryTeam team : teams) {
+			if (team.getSize() == 0) {
+				return team;
+			}
+		}
+		
+		return null;
+	}
+	
+	private MysteryTeam getTeam(Player player) {
+		for (MysteryTeam team : teams) {
+			if (team.hasPlayer(player)) {
+				return team;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Get a Set of all the fake teams.
+	 * 
+	 * @return A set of fake teams.
+	 */
+	public static Set<MysteryTeam> getTeams() {
+		return ImmutableSet.copyOf(teams);
+	}
+
+	/**
+	 * Print the help menu to the given sender.
+	 * 
+	 * @param sender The sender printing too
+	 * @return 
+	 * @return Always true;
+	 */
+	public boolean helpMenu(CommandSender sender) {
+		sender.sendMessage(PREFIX + "MysteryTeams Command");
+	    sender.sendMessage(" - " + ChatColor.BLUE + "/mt teamsize");
+	    
+	    if (!sender.hasPermission("mysteryteams.admin")) {
+	    	return true;
+	    }
+	    
+		sender.sendMessage(" - " + ChatColor.BLUE + "/mt randomize <teamsize>");
+	    sender.sendMessage(" - " + ChatColor.BLUE + "/mt give [player]");
+	    sender.sendMessage(" - " + ChatColor.BLUE + "/mt clear");
+	    sender.sendMessage(" - " + ChatColor.BLUE + "/mt list");
+    	return true;
 	}
 	
 	@EventHandler
 	public void on(PvPEnableEvent event) {
-		PlayerUtils.broadcast("Banners have been given.");
+		PlayerUtils.broadcast(PREFIX + "Banners have been given.");
 		
 		for (Player online : PlayerUtils.getPlayers()) {
-			MysteryTeam team = null;
-			
-			for (MysteryTeam t : teams.keySet()) {
-				if (teams.get(t).contains(online.getUniqueId().toString())) {
-					team = t;
-					break;
-				}
-			}
+			MysteryTeam team = getTeam(online);
 			
 			if (team == null) {
 				continue;
@@ -282,77 +300,53 @@ public class MysteryTeams extends Scenario implements Listener, CommandExecutor 
 		}
 	}
 	
-	private boolean isOnTeam(Player player) {
-		for (MysteryTeam t : teams.keySet()) {
-			if (teams.get(t).contains(player.getUniqueId().toString())) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public void printHelp(CommandSender sender) {
-		sender.sendMessage(PREFIX + "MysteryTeams Command");
-	    sender.sendMessage(" - " + ChatColor.BLUE + "/mt teamsize");
-	    
-	    if (!sender.hasPermission("mysteryteams.admin")) {
-	    	return;
-	    }
-	    
-		sender.sendMessage(" - " + ChatColor.BLUE + "/mt randomize <teamsize>");
-	    sender.sendMessage(" - " + ChatColor.BLUE + "/mt give [player]");
-	    sender.sendMessage(" - " + ChatColor.BLUE + "/mt clear");
-	    sender.sendMessage(" - " + ChatColor.BLUE + "/mt list");
-	}
-	
 	@EventHandler
 	public void onPrepareCraft(PrepareItemCraftEvent event) {
-		if (event.getInventory().getResult() != null && event.getInventory().getResult().getType() == Material.BANNER) {
-			event.getInventory().setResult(new ItemStack(Material.AIR));
+		CraftingInventory inv = event.getInventory();
+		ItemStack result = inv.getResult();
+		
+		if (result == null) {
+			return;
+		}
+		
+		if (result.getType() == Material.BANNER) {
+			inv.setResult(new ItemStack(Material.AIR));
 		}
 	}
 
 	@EventHandler
 	public void onPlayerDeath(PlayerDeathEvent event) {
 		Player player = event.getEntity();
-		
-		MysteryTeam team = null;
-		
-		for (MysteryTeam t : teams.keySet()) {
-			if (teams.get(t).contains(player.getUniqueId().toString())) {
-				team = t;
-				break;
-			}
-		}
+		MysteryTeam team = getTeam(player);
 
 		if (team == null) {
 			return;
 		}
 		
-		teams.get(team).remove(player.getUniqueId().toString());
+		team.removePlayer(player);
 
 		PlayerUtils.broadcast(PREFIX + team.getChatColor() + "Player from team " + team.getName() + " died.");
 		
-		if (teams.get(team).size() > 0) {
-			PlayerUtils.broadcast(PREFIX + team.getChatColor() + "Team " + team.getName() + " has " + ChatColor.RESET + teams.get(team).size() + team.getChatColor() + (teams.get(team).size() > 1 ? " players" : " player") + " left");
+		if (team.getSize() > 0) {
+			PlayerUtils.broadcast(PREFIX + team.getChatColor() + "Team " + team.getName() + " has §f" + team.getSize() + team.getChatColor() + (team.getSize() > 1 ? " players" : " player") + " left");
 			return;
 		}
 		
 		teams.remove(team);
 		
 		if (teams.size() > 1) {
-			PlayerUtils.broadcast(PREFIX + team.getChatColor() + "Team " + team.getName() + " eliminated. There are " + ChatColor.RESET + teams.size() + team.getChatColor() + " teams left.");
+			PlayerUtils.broadcast(PREFIX + team.getChatColor() + "Team " + team.getName() + " eliminated. There are §f" + teams.size() + team.getChatColor() + " teams left.");
 			return;
 		}
 		
-		MysteryTeam m = null;
+		MysteryTeam killerTeam = null;
 		
-		for (MysteryTeam mt : teams.keySet()) {
-			m = mt;
+		for (MysteryTeam mTeam : teams) {
+			killerTeam = mTeam;
 			break;
 		}
 		
-		PlayerUtils.broadcast(PREFIX + team.getChatColor() + "Team " + team.getName() + " eliminated. " + m.getChatColor() + "The " + m.getName() + " team won!");
+		PlayerUtils.broadcast(PREFIX + team.getChatColor() + "Team " + team.getName() + " eliminated. " + killerTeam.getChatColor() + "The " + killerTeam.getName() + " team won!");
 	}
 
 	public enum MysteryTeam {
@@ -374,23 +368,106 @@ public class MysteryTeams extends Scenario implements Listener, CommandExecutor 
 		private ChatColor cColor;
 		private DyeColor dColor;
 		private String name;
+		
+		private Set<UUID> players = new HashSet<UUID>();
 
+		/**
+		 * Constructor for mystery teams.
+		 * 
+		 * @param cColor The chat color.
+		 * @param dColor The dye color.
+		 * @param name The name.
+		 */
 		private MysteryTeam(ChatColor cColor, DyeColor dColor, String name) {
 			this.cColor = cColor;
 			this.dColor = dColor;
 			this.name = name;
 		}
 
+		/**
+		 * Get the name of the team.
+		 * 
+		 * @return The name.
+		 */
 		public String getName() {
 			return name;
 		}
 
+		/**
+		 * Get the chat color of the team.
+		 * 
+		 * @return The chat color.
+		 */
 		public ChatColor getChatColor() {
 			return cColor;
 		}
 
+		/**
+		 * Get the dye color of the team.
+		 * 
+		 * @return The dye color.
+		 */
 		public DyeColor getDyeColor() {
 			return dColor;
+		}
+
+		/**
+		 * Get a set of all UUID's on this team.
+		 * 
+		 * @return A set of UUID's.
+		 */
+		public Set<OfflinePlayer> getPlayers() {
+			Set<OfflinePlayer> list = new HashSet<OfflinePlayer>();
+			
+			for (UUID uuid : players) {
+				list.add(Bukkit.getOfflinePlayer(uuid));
+			}
+			
+			return ImmutableSet.copyOf(list);
+		}
+
+		/**
+		 * Get the size of the team.
+		 * 
+		 * @return The size.
+		 */
+		public int getSize() {
+			return players.size();
+		}
+
+		/**
+		 * Add the given player to the team.
+		 * 
+		 * @param player The player adding.
+		 */
+		public void addPlayer(OfflinePlayer player) {
+			for (MysteryTeam team : teams) {
+				if (team.hasPlayer(player)) {
+					team.removePlayer(player);
+				}
+			}
+			
+			players.add(player.getUniqueId());
+		}
+		
+		/**
+		 * Remove the given player to the team.
+		 * 
+		 * @param player The player removing.
+		 * @return True if successful, false otherwise.
+		 */
+		public boolean removePlayer(OfflinePlayer player) {
+			return players.remove(player.getUniqueId());
+		}
+
+		/**
+		 * Check if the given player is on this team.
+		 * 
+		 * @param player The player checking.
+		 * @return True if he is, false otherwise.
+		 */
+		public boolean hasPlayer(OfflinePlayer player) {
+			return players.contains(player.getUniqueId());
 		}
 	}
 }
