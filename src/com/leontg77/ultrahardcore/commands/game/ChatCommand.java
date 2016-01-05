9168cf93b13bc851773585d62a9d03fb97b0ac32
LeonTG77 <leontg77@gmail.com>
2016-01-05 23:00:40 +0100

    Converted to using the new super class

--- src/com/leontg77/ultrahardcore/commands/game/ChatCommand.java
+++ src/com/leontg77/ultrahardcore/commands/game/ChatCommand.java
@@ -3,14 +3,12 @@ package com.leontg77.ultrahardcore.commands.game;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
 import com.leontg77.ultrahardcore.Game;
 import com.leontg77.ultrahardcore.Main;
+import com.leontg77.ultrahardcore.commands.UHCCommand;
 import com.leontg77.ultrahardcore.utils.PlayerUtils;
 
 /**
@@ -18,69 +16,53 @@ import com.leontg77.ultrahardcore.utils.PlayerUtils;
  * 
  * @author LeonTG77
  */
-public class ChatCommand implements CommandExecutor, TabCompleter {	
+public class ChatCommand extends UHCCommand {	
+
+	public ChatCommand() {
+		super("chat", "<clear|mute>");
+	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (!sender.hasPermission("uhc.chat")) {
-			sender.sendMessage(Main.NO_PERM_MSG);
-			return true;
-		}
-		
+	public boolean execute(CommandSender sender, String[] args) {
 		if (args.length == 0) {
-			sender.sendMessage(Main.PREFIX + "Usage: /chat <clear|mute>");
-			return true;
+			return false;
 		}
 		
 		if (args[0].equalsIgnoreCase("mute")) {
 			Game game = Game.getInstance();
 			
 			if (game.isMuted()) {
-				game.setMuted(false);
 				PlayerUtils.broadcast(Main.PREFIX + "The chat has been enabled.");
+				game.setMuted(false);
 				return true;
 			} 
 			
 			PlayerUtils.broadcast(Main.PREFIX + "The chat has been disabled.");
 			game.setMuted(true);
-		} else if (args[0].equalsIgnoreCase("clear")) {
-			for (int i = 0; i < 150; i++) {
-				for (Player online : PlayerUtils.getPlayers()) {
+			return true;
+		}
+		
+		if (args[0].equalsIgnoreCase("clear")) {
+			for (Player online : PlayerUtils.getPlayers()) {
+				for (int i = 0; i < 150; i++) {
 					online.sendMessage("§0");
 				}
 			}
 			
 			PlayerUtils.broadcast(Main.PREFIX + "The chat has been cleared.");
-		} else {
-			sender.sendMessage(Main.PREFIX + "Usage: /chat <clear|mute>");
+			return true;
 		}
-		return true;
+		
+		return false;
 	}
 
 	@Override
-	public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
-		if (!sender.hasPermission("uhc.chat")) {
-			return null;
-		}
-
-    	ArrayList<String> toReturn = new ArrayList<String>();
+	public List<String> tabComplete(CommandSender sender, String[] args) {
+		List<String> toReturn = new ArrayList<String>();
 		
 		if (args.length == 1) {
-	    	ArrayList<String> types = new ArrayList<String>();
-	    	types.add("clear");
-	    	types.add("mute");
-	    	
-        	if (args[0].equals("")) {
-        		for (String type : types) {
-        			toReturn.add(type);
-        		}
-        	} else {
-        		for (String type : types) {
-        			if (type.startsWith(args[0].toLowerCase())) {
-        				toReturn.add(type);
-        			}
-        		}
-        	}
+	    	toReturn.add("clear");
+	    	toReturn.add("mute");
         }
     	
     	return toReturn;
