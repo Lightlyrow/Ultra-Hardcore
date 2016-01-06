be32be1ef250ff78e5ac0156f9f46819baf25e9b
LeonTG77 <leontg77@gmail.com>
2016-01-06 08:45:29 +0100

    Updated to using the new super class

--- src/com/leontg77/ultrahardcore/commands/give/GiveallCommand.java
+++ src/com/leontg77/ultrahardcore/commands/give/GiveallCommand.java
@@ -1,15 +1,15 @@
 package com.leontg77.ultrahardcore.commands.give;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import com.leontg77.ultrahardcore.Main;
+import com.leontg77.ultrahardcore.commands.CommandException;
 import com.leontg77.ultrahardcore.commands.UHCCommand;
 import com.leontg77.ultrahardcore.utils.PlayerUtils;
 
@@ -26,15 +26,9 @@ public class GiveallCommand extends UHCCommand {
 	}
 
 	@Override
-	public boolean execute(CommandSender sender, String[] args) {
-		if (!sender.hasPermission("uhc.giveall")) {
-			sender.sendMessage(Main.NO_PERM_MSG);
-			return true;
-		}
-		
+	public boolean execute(CommandSender sender, String[] args) throws CommandException {
 		if (args.length == 0) {
-			sender.sendMessage(Main.PREFIX + "Usage: /giveall ");
-			return true;
+			return false;
 		}
 		
 		Material material = null;
@@ -42,9 +36,8 @@ public class GiveallCommand extends UHCCommand {
 		short durability = 0;
 		
 		try {
-			material = Material.getMaterial(Integer.parseInt(args[0]));
-		} 
-		catch (Exception e) {
+			material = Material.getMaterial(parseInt(args[0]));
+		} catch (Exception e) {
 			for (Material types : Material.values()) {
 				if (types.name().startsWith(args[0].toUpperCase())) {
 					material = types;
@@ -54,40 +47,38 @@ public class GiveallCommand extends UHCCommand {
 		}
 		
 		if (material == null) {
-			sender.sendMessage(ChatColor.RED + args[0] + " is not a vaild type.");
-			return true;
+			throw new CommandException("'" + args[0] + "' is not a vaild item.");
 		}
 		
 		if (args.length > 1) {
-			try {
-				amount = Integer.parseInt(args[1]);
-			} catch (Exception e) {
-				sender.sendMessage(ChatColor.RED + args[1] + " is not a vaild number.");
-				return true;
-			}
+			amount = parseInt(args[1], "amount");
 		}
 		
 		if (args.length > 2) {
-			try {
-				durability = Short.parseShort(args[2]);
-			} catch (Exception e) {
-				sender.sendMessage(ChatColor.RED + args[2] + " is not a vaild number.");
-				return true;
-			}
+			durability = (short) parseInt(args[2], "durability");
 		}
 
 		ItemStack item = new ItemStack(material, amount, durability);
-		PlayerUtils.broadcast(Main.PREFIX + "All players recieved §a" + amount + " " + item.getType().name().toLowerCase().replaceAll("_", " ") + (amount > 1 ? "s" : "") + "§7.");
+		String itemName = item.getType().name().toLowerCase().replaceAll("_", " ") + (amount > 1 && !item.getType().name().endsWith("s") ? "s" : "");
+
+		PlayerUtils.broadcast(Main.PREFIX + "All players recieved §a" + amount + " " + itemName + "§7.");
 		
-		for (Player online : Bukkit.getServer().getOnlinePlayers()) {
-			PlayerUtils.giveItem(online, item);
+		for (Player online : PlayerUtils.getPlayers()) {
+			PlayerUtils.giveItem(online, item.clone());
 		}
 		return true;
 	}
 
 	@Override
 	public List<String> tabComplete(CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> toReturn = new ArrayList<String>();
+		
+		if (args.length == 1) {
+			for (Material type : Material.values()) {
+				toReturn.add(type.name().toLowerCase());
+			}
+		}
+		
+		return toReturn;
 	}
 }
