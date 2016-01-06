5741640e94db3bfb4d011db20a70f505671f12a6
LeonTG77 <leontg77@gmail.com>
2016-01-06 08:49:14 +0100

    Updated to using the new super class

--- src/com/leontg77/ultrahardcore/commands/game/MatchpostCommand.java
+++ src/com/leontg77/ultrahardcore/commands/game/MatchpostCommand.java
@@ -1,11 +1,14 @@
 package com.leontg77.ultrahardcore.commands.game;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.command.CommandSender;
 
 import com.leontg77.ultrahardcore.Game;
 import com.leontg77.ultrahardcore.Main;
+import com.leontg77.ultrahardcore.commands.CommandException;
+import com.leontg77.ultrahardcore.commands.UHCCommand;
 import com.leontg77.ultrahardcore.utils.GameUtils;
 
 /**
@@ -13,19 +16,27 @@ import com.leontg77.ultrahardcore.utils.GameUtils;
  * 
  * @author LeonTG77
  */
-public class MatchpostCommand implements CommandExecutor {	
+public class MatchpostCommand extends UHCCommand {	
+
+	public MatchpostCommand() {
+		super("matchpost", "");
+	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean execute(CommandSender sender, String[] args) throws CommandException {
 		String teamSize = GameUtils.getTeamSize(false, false);
 		Game game = Game.getInstance();
 		
 		if (teamSize.startsWith("No") || teamSize.startsWith("Open")) {
-			sender.sendMessage(Main.PREFIX + "There are no matches running.");
-			return true;
+			throw new CommandException("There are no matches running.");
 		}
 		
 		sender.sendMessage(Main.PREFIX + "Match post: §a" + game.getMatchPost());
 		return true;
+	}
+
+	@Override
+	public List<String> tabComplete(CommandSender sender, String[] args) {
+		return new ArrayList<String>();
 	}
 }
