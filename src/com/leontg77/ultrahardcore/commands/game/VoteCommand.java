bdaec7ab0cd76b6ec9f05541c27634395369b0bd
LeonTG77 <leontg77@gmail.com>
2016-01-18 22:39:12 +0100

    Bugfixes and code improvement

--- src/com/leontg77/ultrahardcore/commands/game/VoteCommand.java
+++ src/com/leontg77/ultrahardcore/commands/game/VoteCommand.java
@@ -1,16 +1,19 @@
 package com.leontg77.ultrahardcore.commands.game;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import com.google.common.base.Joiner;
 import com.leontg77.ultrahardcore.Main;
+import com.leontg77.ultrahardcore.commands.CommandException;
+import com.leontg77.ultrahardcore.commands.UHCCommand;
 import com.leontg77.ultrahardcore.utils.PlayerUtils;
 
 /**
@@ -18,21 +21,84 @@ import com.leontg77.ultrahardcore.utils.PlayerUtils;
  * 
  * @author LeonTG77
  */
-public class VoteCommand implements CommandExecutor {
-	private static List<UUID> voted = new ArrayList<UUID>();
+public class VoteCommand extends UHCCommand {
+	private static final List<UUID> voted = new ArrayList<UUID>();
+	private static BukkitRunnable run = null;
 	
-	private static boolean running = false;
 	private static int yes = 0;
 	private static int no = 0;
 	
-	public static boolean isRunning() {
-		return running;
+	public VoteCommand() {
+		super("vote", "<message>");
 	}
 	
+	@Override
+	public boolean execute(final CommandSender sender, final String[] args) throws CommandException {
+		if (args.length == 0) {
+			return false;
+		}
+		
+		if (isRunning()) {
+			throw new CommandException("A vote is already running");
+		}
+        
+        String message = Joiner.on(' ').join(Arrays.copyOfRange(args, 0, args.length));
+        voted.clear();
+        
+		yes = 0;
+		no = 0;
+        
+        PlayerUtils.broadcast(Main.PREFIX + "A vote has started for: §6" + message + "§7.");
+        PlayerUtils.broadcast("§8» §7Say §a'y'§7 or §c'n'§7 in chat to vote.");
+        
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "timer 30 &7The vote ends in &8»&a");
+        
+        run = new BukkitRunnable() {
+			public void run() {
+				PlayerUtils.broadcast(Main.PREFIX + "The vote has ended, the results are: §a" + yes + " yes §7and §c" + no + " no§7.");
+				
+				run = null;
+				voted.clear();
+				
+				yes = 0;
+				no = 0;
+			}
+		};
+		
+		run.runTaskLater(Main.plugin, 600);
+		return true;
+	}
+
+	@Override
+	public List<String> tabComplete(CommandSender sender, String[] args) {
+		return new ArrayList<String>();
+	}
+	
+	/**
+	 * Check if a vote is currently running.
+	 * 
+	 * @return True if it is, false otherwise.
+	 */
+	public static boolean isRunning() {
+		return run != null;
+	}
+	
+	/**
+	 * Check if the given player has already voted.
+	 * 
+	 * @param player The player checking.
+	 * @return True if he has, false otherwise.
+	 */
 	public static boolean hasVoted(Player player) {
 		return voted.contains(player.getUniqueId());
 	}
 	
+	/**
+	 * Add a vote for the given player.
+	 * 
+	 * @param player The player voting.
+	 * @param votedYes True if they voted yes, false if they voted no.
+	 */
 	public static void addVote(Player player, boolean votedYes) {
 		voted.add(player.getUniqueId());
 		
@@ -41,55 +107,5 @@ public class VoteCommand implements CommandExecutor {
 		} else {
 			no++;
 		}
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (!sender.hasPermission("uhc.vote")) {
-			sender.sendMessage(Main.NO_PERM_MSG);
-			return true;
-		}
-		
-		if (args.length == 0) {
-			sender.sendMessage(Main.PREFIX + "Usage: /vote <message|end>");
-			return true;
-		}
-		
-		if (args[0].equalsIgnoreCase("end")) {
-			if (!running) {
-				sender.sendMessage(ChatColor.RED + "No votes are running.");
-				return true;
-			}
-			
-			PlayerUtils.broadcast(Main.PREFIX + "The vote has ended, the results are: §a" + yes + " yes §7and §c" + no + " no§7.");
-			
-			running = false;
-			voted.clear();
-			yes = 0;
-			no = 0;
-			return true;
-		}
-		
-		if (running) {
-			sender.sendMessage(ChatColor.RED + "Theres already a vote running.");
-			return true;
-		}
-		
-		StringBuilder message = new StringBuilder();
-           
-        for (int i = 0; i < args.length; i++) {
-        	message.append(args[i]).append(" ");
-        }
-        
-        String msg = message.toString().trim();
-        
-        running = true;
-        voted.clear();
-		yes = 0;
-		no = 0;
-        
-        PlayerUtils.broadcast(Main.PREFIX + "A vote has started for: §6" + msg + "§7.");
-        PlayerUtils.broadcast("§8» §7Say §a'y'§7 or §c'n'§7 in chat to vote.");
-		return true;
 	}
 }
