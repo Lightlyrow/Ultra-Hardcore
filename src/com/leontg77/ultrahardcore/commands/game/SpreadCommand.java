d14cb959b180d07cc3b1014567e06030bb7bb255
LeonTG77 <leontg77@gmail.com>
2016-01-18 22:36:06 +0100

    Completly recoded the scatter

--- src/com/leontg77/ultrahardcore/commands/game/SpreadCommand.java
+++ src/com/leontg77/ultrahardcore/commands/game/SpreadCommand.java
@@ -4,20 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Difficulty;
-import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.Team;
 
+import com.google.common.collect.ImmutableList;
 import com.leontg77.ultrahardcore.Arena;
 import com.leontg77.ultrahardcore.Game;
 import com.leontg77.ultrahardcore.Main;
@@ -26,7 +22,6 @@ import com.leontg77.ultrahardcore.Settings;
 import com.leontg77.ultrahardcore.State;
 import com.leontg77.ultrahardcore.commands.CommandException;
 import com.leontg77.ultrahardcore.commands.UHCCommand;
-import com.leontg77.ultrahardcore.managers.BoardManager;
 import com.leontg77.ultrahardcore.managers.ScatterManager;
 import com.leontg77.ultrahardcore.managers.TeamManager;
 import com.leontg77.ultrahardcore.utils.EntityUtils;
@@ -46,13 +41,7 @@ public class SpreadCommand extends UHCCommand {
 
 	@Override
 	public boolean execute(final CommandSender sender, final String[] args) throws CommandException {
-		if (!sender.hasPermission("uhc.spread")) {
-			sender.sendMessage(Main.NO_PERM_MSG);
-			return true;
-		}
-		
 		if (args.length < 2) {
-			sender.sendMessage(Main.PREFIX + "Usage: /spread ");
 			return false;
 		}
 
@@ -67,8 +56,9 @@ public class SpreadCommand extends UHCCommand {
 		}
 		
 		final boolean teamSpread = parseBoolean(args[0], "Team Spread");
+		final TeamManager teamMan = TeamManager.getInstance();
 		
-		if (args[2].equalsIgnoreCase("*")) {
+		if (args[1].equalsIgnoreCase("*")) {
 			switch (State.getState()) {
 			case NOT_RUNNING:
 				throw new CommandException("You can't scatter when no games are running.");
@@ -100,10 +90,10 @@ public class SpreadCommand extends UHCCommand {
 			int teams = 0;
 			int solo = 0;
 			
-			final BoardManager boardMan = BoardManager.getInstance();
-			final TeamManager teamMan = TeamManager.getInstance();
+			List<String> toScatter = new ArrayList<String>();
+			game.setPreWhitelists(true);
 			
-			if (game.getTeamSize().toLowerCase().startsWith("cto")) {
+			if (game.getTeamSize().toLowerCase().startsWith("cto") && teamSpread) {
 				for (OfflinePlayer whitelisted : Bukkit.getWhitelistedPlayers()) {
 					if (teamMan.getTeam(whitelisted) != null) {
 						continue;
@@ -121,6 +111,8 @@ public class SpreadCommand extends UHCCommand {
 				if (loopTeam.getSize() == 0) {
 					continue;
 				}
+
+				toScatter.add(loopTeam.getName());
 				
 				if (loopTeam.getSize() > 1) {
 					teams++;
@@ -129,10 +121,32 @@ public class SpreadCommand extends UHCCommand {
 				}
 			}
 			
+			if (!teamSpread) {
+				for (OfflinePlayer whitelisted : Bukkit.getWhitelistedPlayers()) {
+					toScatter.add(whitelisted.getName());
+					solo++;
+				}
+			}
+			
+			manager.setTeamScatter(teamSpread);
+			manager.setWorld(world);
+			
+			manager.scatter(ImmutableList.copyOf(toScatter));
+			
+			if (teamSpread) {
+				PlayerUtils.broadcast(Main.PREFIX + "Scattering §a" + teams + " §7teams and §a" + solo + " §7solos...");
+			} else {
+				PlayerUtils.broadcast(Main.PREFIX + "Scattering §a" + solo + " §7players...");
+			}
+			
+			for (Player online : PlayerUtils.getPlayers()) {
+				online.playSound(online.getLocation(), Sound.FIREWORK_LAUNCH, 1, 1);
+			}
+			
 			for (World worlds : GameUtils.getGameWorlds()) {
-				worlds.setTime(0);
 				worlds.setDifficulty(Difficulty.HARD);
 				worlds.setPVP(false);
+				worlds.setTime(0);
 				
 				worlds.setGameRuleValue("doDaylightCycle", "true");
 				worlds.setSpawnFlags(false, true);
@@ -145,210 +159,56 @@ public class SpreadCommand extends UHCCommand {
 					}
 				}
 			}
-			
-			if (teamSpread) {
-				PlayerUtils.broadcast(Main.PREFIX + "Scattering §a" + teams + " §7teams and §a" + solo + " §7solos...");
-			} else {
-				PlayerUtils.broadcast(Main.PREFIX + "Scattering §a" + Bukkit.getServer().getWhitelistedPlayers().size() + " §7players...");
-			}
-			
-			for (Player online : PlayerUtils.getPlayers()) {
-				online.playSound(online.getLocation(), Sound.FIREWORK_LAUNCH, 1, 1);
-			}
-			
-			new BukkitRunnable() {
-				public void run() {
-					PlayerUtils.broadcast(Main.PREFIX + "Finding scatter locations...");
-
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_BASS, 1, 1);
-					}
-					
-					if (teams) {
-						List<Location> loc = ScatterManager.findScatterLocations(Bukkit.getWorld(world), radius, te + so);
-						
-						int index = 0;
-						
-						for (Team tem : TeamManager.getInstance().getTeamsWithPlayers()) {
-							for (String player : tem.getEntries()) {
-								scatterLocs.put(player, loc.get(index));
-								
-								PlayerUtils.getOfflinePlayer(player).setWhitelisted(true);
-							}
-							index++;
-						}
-						
-						for (OfflinePlayer online : Bukkit.getServer().getWhitelistedPlayers()) {
-							if (BoardManager.getInstance().board.getEntryTeam(online.getName()) == null) {
-								scatterLocs.put(online.getName(), loc.get(index));
-								index++;
-							}
-						}
-					} else {
-						List<Location> loc = ScatterManager.findScatterLocations(Bukkit.getWorld(world), radius, Bukkit.getServer().getWhitelistedPlayers().size());
-					
-						int index = 0;
-						
-						for (OfflinePlayer wld : Bukkit.getServer().getWhitelistedPlayers()) {
-							scatterLocs.put(wld.getName(), loc.get(index));
-							index++;
-						}
-					}
-				}
-			}.runTaskLater(Main.plugin, 30);
-
-			new BukkitRunnable() {
-				public void run() {
-					PlayerUtils.broadcast(Main.PREFIX + "Locations found, loading chunks...");
-
-					for (Player online : PlayerUtils.getPlayers()) {
-						online.playSound(online.getLocation(), Sound.NOTE_BASS, 1, 1);
-					}
-					
-					final ArrayList<Location> locs = new ArrayList<Location>(scatterLocs.values());
-					final ArrayList<String> names = new ArrayList<String>(scatterLocs.keySet());
-					
-					new BukkitRunnable() {
-						int i = 0;
-						
-						public void run() {
-							if (i < locs.size()) {
-								if (sender instanceof Player) {
-									Player player = (Player) sender;
-									player.teleport(locs.get(i));
-								} else {
-									locs.get(i).getChunk().load(true);
-								}
-								i++;
-							} else {
-								cancel();
-								locs.clear();
-								PlayerUtils.broadcast(Main.PREFIX + "All chunks loaded, starting scatter...");
-
-								for (Player online : PlayerUtils.getPlayers()) {
-									online.playSound(online.getLocation(), Sound.NOTE_BASS, 1, 1);
-								}
-								
-								new BukkitRunnable() {
-									int i = 0;
-									
-									public void run() {
-										if (i < names.size()) {
-											if (names.get(i) == null) {
-												PlayerUtils.broadcast(Main.PREFIX + "- §4An error occured while scattering a player.");
-												i++;
-												return;
-											}
-											
-											Player scatter = Bukkit.getServer().getPlayer(names.get(i));
-											
-											if (scatter == null) {
-												PlayerUtils.broadcast(Main.PREFIX + "- §c" + names.get(i) + " §7offline, scheduled.");
-												
-												for (Player online : PlayerUtils.getPlayers()) {
-													online.playSound(online.getLocation(), "random.pop", 1, 0);
-												}
-											} else {
-												scatter.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 1726272000, 128));
-												scatter.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 1726272000, 6));
-												scatter.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 1726272000, 6));
-												scatter.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 1726272000, 10));
-												scatter.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 1726272000, 6));
-												scatter.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 1726272000, 2));
-												scatter.teleport(scatterLocs.get(names.get(i)));
-												PlayerUtils.broadcast(Main.PREFIX + "- §a" + names.get(i) + " §7has been scattered.");
-												scatterLocs.remove(names.get(i));
-												
-												for (Player online : PlayerUtils.getPlayers()) {
-													online.playSound(online.getLocation(), "random.pop", 1, 0);
-												}
-											}
-											i++;
-										} else {
-											PlayerUtils.broadcast(Main.PREFIX + "The scatter has finished.");
-											isReady = true;
-											names.clear();
-											cancel();
-											
-											for (Player online : PlayerUtils.getPlayers()) {
-												online.playSound(online.getLocation(), Sound.FIREWORK_TWINKLE, 1, 1);
-											}
-										}
-									}
-								}.runTaskTimer(Main.plugin, 40, 3);
-							}
-						}
-					}.runTaskTimer(Main.plugin, 5, 5);
-				}
-			}.runTaskLater(Main.plugin, 60);
 		} else {
-			final Player target = Bukkit.getPlayer(args[2]);
+			final Player target = Bukkit.getPlayer(args[1]);
 			
 			if (target == null) {
-				sender.sendMessage(ChatColor.RED + "That player is not online.");
-				return true;
+				throw new CommandException("'" + args[1] + "' is not online.");
 			}
 
 			PlayerUtils.broadcast(Main.PREFIX + "Scattering §a" + target.getName() + "§7...");
 
-			new BukkitRunnable() {
-				public void run() {
-					PlayerUtils.broadcast(Main.PREFIX + "Finding scatter location...");
-					
-					if (teamSpread) {
-						if (target.getScoreboard().getEntryTeam(target.getName()) == null) {
-							List<Location> loc = ScatterManager.findScatterLocations(Bukkit.getWorld(world), radius, 1);
-							scatterLocs.put(target.getName(), loc.get(0));
-							return;
+			if (teamSpread) {
+				Team team = teamMan.getTeam(target);
+				
+				if (team != null) {
+					for (OfflinePlayer teammate : teamMan.getPlayers(team)) {
+						if (teammate.getPlayer() == null) {
+							continue;
 						}
 						
-						Team tem = target.getScoreboard().getEntryTeam(target.getName());
-						
-						for (String tm : tem.getEntries()) {
-							Player temmate = Bukkit.getServer().getPlayer(tm);
-							
-							if (temmate != null) {
-								scatterLocs.put(target.getName(), temmate.getLocation());
-								break;
-							}
-						}
-					} else {
-						List<Location> loc = ScatterManager.findScatterLocations(Bukkit.getWorld(world), radius, 1);
-						scatterLocs.put(target.getName(), loc.get(0));
+						PlayerUtils.broadcast(Main.PREFIX + "Scattered §a" + target.getName() + "§7.");
+						target.teleport(teammate.getPlayer());
+						return true;
 					}
 				}
-			}.runTaskLater(Main.plugin, 30);
-
-			new BukkitRunnable() {
-				public void run() {
-					PlayerUtils.broadcast(Main.PREFIX + "Location found, scattering...");
-					
-					if (!target.isOnline()) {
-						PlayerUtils.broadcast(Main.PREFIX + "- §c" + target.getName() + " §7offline, scheduled.");
-					} else {
-						if (State.isState(State.SCATTER)) {
-							target.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 1726272000, 128));
-							target.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 1726272000, 6));
-							target.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 1726272000, 6));
-							target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 1726272000, 10));
-							target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 1726272000, 6));
-							target.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 1726272000, 2));
-						}
-						
-						target.setWhitelisted(true);
-						target.teleport(scatterLocs.get(target.getName()));
-						PlayerUtils.broadcast(Main.PREFIX + "- §a" + target.getName() + " §7has been scattered.");
-						scatterLocs.remove(target.getName());
-					}
-				}
-			}.runTaskLater(Main.plugin, 60);
+			}
+			
+			manager.setTeamScatter(false);
+			manager.setWorld(world);
+			
+			manager.scatter(ImmutableList.of(target.getName()));
 		}
 		return true;
 	}
 
 	@Override
 	public List<String> tabComplete(CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> toReturn = new ArrayList<String>();
+		
+		if (args.length == 1) {
+			toReturn.add("true");
+			toReturn.add("false");
+		}
+		
+		if (args.length == 2) {
+			toReturn.add("*");
+			
+			for (Player online : PlayerUtils.getPlayers()) {
+				toReturn.add(online.getName());
+			}
+		}
+		
+		return toReturn;
 	}
 }
--- src/com/leontg77/ultrahardcore/managers/ScatterManager.java
+++ src/com/leontg77/ultrahardcore/managers/ScatterManager.java
@@ -1,18 +1,33 @@
 package com.leontg77.ultrahardcore.managers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Team;
 
 import com.google.common.collect.ImmutableSet;
+import com.leontg77.ultrahardcore.Game;
+import com.leontg77.ultrahardcore.Main;
+import com.leontg77.ultrahardcore.State;
+import com.leontg77.ultrahardcore.commands.CommandException;
 import com.leontg77.ultrahardcore.utils.LocationUtils;
+import com.leontg77.ultrahardcore.utils.NumberUtils;
+import com.leontg77.ultrahardcore.utils.PacketUtils;
 import com.leontg77.ultrahardcore.utils.PlayerUtils;
 
 /**
@@ -21,29 +36,282 @@ import com.leontg77.ultrahardcore.utils.PlayerUtils;
  * @author LeonTG77
  */
 public class ScatterManager {
-	private static final Set<Material> INVAILD_SPAWN_BLOCKS = ImmutableSet.of(Material.STATIONARY_WATER, Material.WATER, Material.STATIONARY_LAVA, Material.LAVA, Material.CACTUS);
 	private static final ScatterManager INSTANCE = new ScatterManager();
+	private static final int EFFECT_TICKS = NumberUtils.get999DaysInTicks();
 	
+	/**
+	 * List of all freeze effects.
+	 */
+	private static final Set<PotionEffect> FREEZE_EFFECTS = ImmutableSet.of(
+			new PotionEffect(PotionEffectType.JUMP, EFFECT_TICKS, 128), 
+			new PotionEffect(PotionEffectType.BLINDNESS, EFFECT_TICKS, 6),
+			new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, EFFECT_TICKS, 6),
+			new PotionEffect(PotionEffectType.SLOW_DIGGING, EFFECT_TICKS, 10),
+			new PotionEffect(PotionEffectType.SLOW, EFFECT_TICKS, 6),
+			new PotionEffect(PotionEffectType.INVISIBILITY, EFFECT_TICKS, 2)
+	);
+	
+	/**
+	 * List of blocks not to spawn on.
+	 */
+	private static final Set<Material> INVAILD_SPAWN_BLOCKS = ImmutableSet.of(
+			Material.STATIONARY_WATER, 
+			Material.WATER, 
+			Material.STATIONARY_LAVA,
+			Material.LAVA, 
+			Material.CACTUS
+	);
+
+	private final Map<String, Location> lateScatters = new HashMap<String, Location>();
+	
+	private boolean scattering = true;
 	private boolean scatterTeams;
+	
 	private World world;
 	private int radius;
 	
+	/**
+	 * Get the instance of the class.
+	 * 
+	 * @return The instance.
+	 */
 	public static ScatterManager getInstance() {
 		return INSTANCE;
 	}
+
+	/**
+	 * Check if the scatter is currently running.
+	 * 
+	 * @return True if it is, false otherwise.
+	 */
+	public boolean isScattering() {
+		return scattering;
+	}
 	
-	public void setTeamScatter(boolean enable) {
+	/**
+	 * Enable or disable team spead.
+	 * <p>
+	 * Team spead means wether to spread teams together or have everyone at seperate places.
+	 * 
+	 * @param enable True to enable, false to disable.
+	 */
+	public void setTeamScatter(final boolean enable) {
 		this.scatterTeams = enable;
 	}
 	
-	public void setWorld(World world) {
+	/**
+	 * Set the world to use for the scatter.
+	 * <p>
+	 * Once the world is set it will calculate the radius.
+	 * 
+	 * @param world The world to use.
+	 */
+	public void setWorld(final World world) {
 		this.world = world;
 		
 		this.radius = ((((int) world.getWorldBorder().getSize()) / 2) - 1);
 	}
+
+	/**
+	 * Check if the given player needs a late scatter.
+	 * 
+	 * @param player The player to check.
+	 * @return True if he needs it, false otherwise.
+	 */
+	public boolean needsLateScatter(Player player) {
+		return lateScatters.containsKey(player.getName());
+	}
+
+	/**
+	 * Handle a late scatter.
+	 * 
+	 * @param toScatter The player to handle.
+	 */
+	public void handleLateScatter(Player toScatter) {
+		if (State.isState(State.INGAME)) {
+			for (PotionEffect effect : FREEZE_EFFECTS) {
+				if (toScatter.hasPotionEffect(effect.getType())) {
+					toScatter.removePotionEffect(effect.getType());
+				}
+				
+				toScatter.addPotionEffect(effect);
+			}
+		}
+			
+		toScatter.teleport(lateScatters.get(toScatter.getName()));
+		lateScatters.remove(toScatter.getName());
+	}
 	
-	public void scatter(List<Player> toScatter) {
+	/**
+	 * Scatter the given list of strings.
+	 * <p>
+	 * If team spread is on the strings will be team names, otherwise it will be player names.
+	 * 
+	 * @param toScatter The players to scatter.
+	 * @throws CommandException If theres no players to scatter.
+	 */
+	public void scatter(final List<String> toScatter) throws CommandException {
+		if (toScatter.isEmpty()) {
+			throw new CommandException("There are no players to scatter.");
+		}
 		
+		final Map<String, Location> scatterLocs = new HashMap<String, Location>();
+		final TeamManager manager = TeamManager.getInstance();
+		
+		scattering = true;
+		
+		new BukkitRunnable() {
+			public void run() {
+				if (toScatter.size() > 1) {
+					PlayerUtils.broadcast(Main.PREFIX + "Finding scatter locations...");
+					
+					for (Player online : PlayerUtils.getPlayers()) {
+						online.playSound(online.getLocation(), Sound.NOTE_BASS, 1, 1);
+					}
+				}
+				
+				List<Location> loc = ScatterManager.findScatterLocations(world, radius, toScatter.size());
+				int index = 0;
+
+				for (String teamOrPlayer : toScatter) {
+					scatterLocs.put(teamOrPlayer, loc.get(index));
+					index++;
+				}
+			}
+		}.runTaskLater(Main.plugin, 30);
+
+		new BukkitRunnable() {
+			public void run() {
+				if (toScatter.size() > 1) {
+					PlayerUtils.broadcast(Main.PREFIX + "Locations found, loading chunks...");
+
+					for (Player online : PlayerUtils.getPlayers()) {
+						online.playSound(online.getLocation(), Sound.NOTE_BASS, 1, 1);
+					}
+				}
+				
+				final List<Location> locs = new ArrayList<Location>(scatterLocs.values());
+				final List<String> names = new ArrayList<String>(scatterLocs.keySet());
+				
+				new BukkitRunnable() {
+					int i = 0;
+					
+					public void run() {
+						if (i < locs.size()) {
+							Player host = Bukkit.getPlayer(Game.getInstance().getHost());
+							
+							if (State.isState(State.INGAME) || host == null) {
+								locs.get(i).getChunk().load(true);
+							} else {
+								host.teleport(locs.get(i));
+							}
+							
+							i++;
+
+							if (toScatter.size() > 1) {
+								for (Player online : PlayerUtils.getPlayers()) {
+									PacketUtils.sendAction(online, "§7Loading scatter locations... §8[§a" + i + "§7/§a" + locs.size() + "§8]");
+								}
+							}
+						} else {
+							if (toScatter.size() > 1) {
+								PlayerUtils.broadcast(Main.PREFIX + "All chunks loaded, starting scatter...");
+
+								for (Player online : PlayerUtils.getPlayers()) {
+									online.playSound(online.getLocation(), Sound.NOTE_BASS, 1, 1);
+								}
+							}
+							
+							locs.clear();
+							cancel();
+							
+							new BukkitRunnable() {
+								int i = 0;
+								
+								public void run() {
+									if (i < names.size()) {
+										String scatter = names.get(i);
+										
+										if (scatter == null) {
+											PlayerUtils.broadcast("§cAn error occured while scattering a player.", "uhc.staff");
+											i++;
+											return;
+										}
+										
+										if (scatterTeams) {
+											Team team = manager.getTeam(scatter);
+											
+											for (OfflinePlayer teammate : manager.getPlayers(team)) {
+												Player toScatter = teammate.getPlayer();
+												
+												if (toScatter == null) {
+													lateScatters.put(teammate.getName(), scatterLocs.get(scatter));
+												} else {
+													if (State.isState(State.SCATTER)) {
+														for (PotionEffect effect : FREEZE_EFFECTS) {
+															if (toScatter.hasPotionEffect(effect.getType())) {
+																toScatter.removePotionEffect(effect.getType());
+															}
+															
+															toScatter.addPotionEffect(effect);
+														}
+													}
+													
+													toScatter.teleport(scatterLocs.get(scatter));
+												}
+											}
+											
+											scatterLocs.remove(scatter);
+										} else {
+											Player toScatter = Bukkit.getPlayer(scatter);
+											
+											if (toScatter == null) {
+												lateScatters.put(scatter, scatterLocs.get(scatter));
+												scatterLocs.remove(scatter);
+											} else {
+												if (State.isState(State.SCATTER)) {
+													for (PotionEffect effect : FREEZE_EFFECTS) {
+														if (toScatter.hasPotionEffect(effect.getType())) {
+															toScatter.removePotionEffect(effect.getType());
+														}
+														
+														toScatter.addPotionEffect(effect);
+													}
+												}
+												
+												toScatter.teleport(scatterLocs.get(scatter));
+												scatterLocs.remove(scatter);
+											}
+										}
+										
+										i++;
+
+										if (toScatter.size() > 1) {
+											for (Player online : PlayerUtils.getPlayers()) {
+												PacketUtils.sendAction(online, "§7Scattered " + scatter + " §8[§a" + i + "§7/§a" + names.size() + "§8]");
+											}
+										}
+									} else {
+										if (toScatter.size() > 1) {
+											PlayerUtils.broadcast(Main.PREFIX + "The scatter has finished.");
+											
+											for (Player online : PlayerUtils.getPlayers()) {
+												online.playSound(online.getLocation(), Sound.FIREWORK_TWINKLE, 1, 1);
+											}
+										}
+										
+										scattering = false;
+										
+										names.clear();
+										cancel();
+									}
+								}
+							}.runTaskTimer(Main.plugin, 40, 3);
+						}
+					}
+				}.runTaskTimer(Main.plugin, 5, 5);
+			}
+		}.runTaskLater(Main.plugin, 60);
 	}
 	
 	/**
@@ -56,7 +324,7 @@ public class ScatterManager {
 	 * @return A list of vaild scatter locations.
 	 */
 	public static List<Location> findScatterLocations(World world, int radius, int count) {
-		ArrayList<Location> locs = new ArrayList<Location>();
+		final List<Location> locs = new ArrayList<Location>();
 		
 		for (int i = 0; i < count; i++) {
 			double min = 150;
@@ -67,7 +335,8 @@ public class ScatterManager {
 					break;
 				}
 				
-				Random rand = new Random();
+				final Random rand = new Random();
+				
 				int x = rand.nextInt(radius * 2) - radius;
 				int z = rand.nextInt(radius * 2) - radius;
 
