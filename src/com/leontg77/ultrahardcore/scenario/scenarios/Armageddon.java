4b905cfcba125d67081b20915c8e8f87d3b050a4
LeonTG77 <leontg77@gmail.com>
2016-01-23 14:17:40 +0100

    Code improvement

--- src/com/leontg77/ultrahardcore/scenario/scenarios/Armageddon.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/Armageddon.java
@@ -14,9 +14,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionType;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-import com.leontg77.ultrahardcore.Game;
 import com.leontg77.ultrahardcore.Main;
 import com.leontg77.ultrahardcore.events.GameStartEvent;
 import com.leontg77.ultrahardcore.scenario.Scenario;
@@ -28,136 +28,167 @@ import com.leontg77.ultrahardcore.utils.BlockUtils;
  * @author Bergasms, modified by LeonTG77.
  */
 public class Armageddon extends Scenario implements Listener {
-	private World world = Game.getInstance().getWorld();
+	private BukkitRunnable task = null;
+	private World world;
 	
-	private long tickInterval = 50L;
-	private int frequency = 5;
-	private int task = -1;
+	private static final long TICKS_TO_START = 12000L;
+	private static final long TICK_INTERVAL = 50L;
+	private static final int FREQUENCY = 5;
 	
 	public Armageddon() {
-		super("Armageddon", "Lava, gravel, potions, and nuggets fall from the sky.");
+		super("Armageddon", "Lava, gravel, sand, potions, and some items fall from the sky.");
 	}
 
 	@Override
 	public void onDisable() {
-		if (task != -1) {
-			Bukkit.getScheduler().cancelTask(task);
+		if (task != null && Bukkit.getScheduler().isCurrentlyRunning(task.getTaskId())) {
+			task.cancel();
 		}
 		
-		this.task = -1;
+		task = null;
 	}
 
 	@Override
 	public void onEnable() {
-		world = Game.getInstance().getWorld();
+		world = game.getWorld();
 	}
 	
 	@EventHandler
 	public void on(GameStartEvent event) {
-		task = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.plugin, new Runnable() {
+		onEnable();
+		
+		task = new BukkitRunnable() {
 			public void run() {
-				for (int i = 0; i < frequency; i++) {
+				for (int i = 0; i < FREQUENCY; i++) {
 					dropArmageddonItem();
 				}
 			}
-		}, 12000, tickInterval);
+		};
+		
+		task.runTaskTimer(Main.plugin, TICKS_TO_START, TICK_INTERVAL);
 	}
 
+	/**
+	 * Drop an armageddon item in the game world.
+	 */
 	@SuppressWarnings("deprecation")
 	private void dropArmageddonItem() {
-		Chunk[] chunkscandidates = world.getLoadedChunks();
-		Random rand = new Random();
+		final Chunk[] chunks = world.getLoadedChunks();
+		final Random rand = new Random();
 		
-		if (chunkscandidates.length == 0) {
+		if (chunks.length == 0) {
 			return;
 		}
 		
-		Chunk dropChunk = chunkscandidates[rand.nextInt(chunkscandidates.length)];
-		Location dropFrom = new Location(dropChunk.getWorld(), dropChunk.getX() * 16 + rand.nextInt(16), 255.0D, dropChunk.getZ() * 16 + rand.nextInt(15));
+		final Chunk chunk = chunks[rand.nextInt(chunks.length)];
+		final Location loc = new Location(chunk.getWorld(), chunk.getX() * 16 + rand.nextInt(16), 255.0D, chunk.getZ() * 16 + rand.nextInt(15));
 		
-		handlePotionDrop(dropChunk, dropFrom);
-		int randomInt = rand.nextInt(100);
+		handlePotionDrop(loc);
+		final int randomInt = rand.nextInt(100);
 		
 		if (randomInt > 90) {
 			int itemChoice = rand.nextInt(10);
 			
 			switch (itemChoice) {
 			case 0:
-				BlockUtils.dropItem(dropFrom, new ItemStack(Material.GOLD_INGOT, 1));
+				BlockUtils.dropItem(loc, new ItemStack(Material.GOLD_INGOT, 1));
 				break;
 			case 1:
-				BlockUtils.dropItem(dropFrom, new ItemStack(Material.DIAMOND, 1));
+				BlockUtils.dropItem(loc, new ItemStack(Material.DIAMOND, 1));
 				break;
 			case 2:
-				BlockUtils.dropItem(dropFrom, new ItemStack(Material.NETHER_STALK, 1));
+				BlockUtils.dropItem(loc, new ItemStack(Material.NETHER_STALK, 1));
 				break;
 			case 3:
-				BlockUtils.dropItem(dropFrom, new ItemStack(Material.BLAZE_ROD, 1));
+				BlockUtils.dropItem(loc, new ItemStack(Material.BLAZE_ROD, 1));
 				break;
 			case 4:
-				BlockUtils.dropItem(dropFrom, new ItemStack(Material.STRING, 3));
+				BlockUtils.dropItem(loc, new ItemStack(Material.STRING, 3));
 				break;
 			case 5:
-				dropChunk.getWorld().spawnFallingBlock(dropFrom, Material.MELON_BLOCK, (byte) 0);
+				world.spawnFallingBlock(loc, Material.MELON_BLOCK, (byte) 0);
 				break;
 			case 6:
-				dropChunk.getWorld().spawnFallingBlock(dropFrom, Material.IRON_BLOCK, (byte) 0);
+				world.spawnFallingBlock(loc, Material.IRON_BLOCK, (byte) 0);
 				break;
 			case 7:
-				dropChunk.getWorld().spawnFallingBlock(dropFrom, Material.MOB_SPAWNER, (byte) 0);
+				world.spawnFallingBlock(loc, Material.MOB_SPAWNER, (byte) 0);
 				break;
 			case 8:
-				BlockUtils.dropItem(dropFrom, new ItemStack(Material.BLAZE_POWDER, 1));
+				BlockUtils.dropItem(loc, new ItemStack(Material.BLAZE_POWDER, 1));
+				break;
 			case 9:
-				BlockUtils.dropItem(dropFrom, new ItemStack(Material.BOOK, 1));
+				BlockUtils.dropItem(loc, new ItemStack(Material.BOOK, 1));
+				break;
 			}
-		} else if (randomInt > 70) {
-			handlePotionDrop(dropChunk, dropFrom);
-		} else if (randomInt > 40) {
-			dropChunk.getWorld().spawnFallingBlock(dropFrom, rand.nextBoolean() ? Material.SAND : Material.GRAVEL, (byte) 0);
-		} else {
-			dropChunk.getWorld().spawnFallingBlock(dropFrom, Material.LAVA, (byte) 0);
+		} 
+		else if (randomInt > 70) {
+			handlePotionDrop(loc);
+		} 
+		else if (randomInt > 40) {
+			world.spawnFallingBlock(loc, rand.nextBoolean() ? Material.SAND : Material.GRAVEL, (byte) 0);
+		} 
+		else {
+			world.spawnFallingBlock(loc, Material.LAVA, (byte) 0);
 		}
 	}
 
-	public void handlePotionDrop(Chunk dropChunk, Location dropFrom) {
+	/**
+	 * Handle a potion drop.
+	 * 
+	 * @param loc The location to use.
+	 */
+	private void handlePotionDrop(Location loc) {
 		Random rand = new Random();
 		int itemChoice = rand.nextInt(30);
 		
 		if (itemChoice > 20) {
-			dropPotion(dropChunk, dropFrom, PotionType.INSTANT_DAMAGE, rand.nextInt(1));
-		} else if (itemChoice > 15) {
-			dropPotion(dropChunk, dropFrom, PotionType.POISON, rand.nextInt(1));
-		} else if (itemChoice > 10) {
-			dropPotion(dropChunk, dropFrom, PotionType.WEAKNESS, rand.nextInt(1));
-		} else if (itemChoice > 5) {
-			dropPotion(dropChunk, dropFrom, PotionType.SLOWNESS, rand.nextInt(1));
-		} else {
+			dropPotion(loc, PotionType.INSTANT_DAMAGE, rand.nextInt(1));
+		} 
+		else if (itemChoice > 15) {
+			dropPotion(loc, PotionType.POISON, rand.nextInt(1));
+		} 
+		else if (itemChoice > 10) {
+			dropPotion(loc, PotionType.WEAKNESS, rand.nextInt(1));
+		} 
+		else if (itemChoice > 5) {
+			dropPotion(loc, PotionType.SLOWNESS, rand.nextInt(1));
+		} 
+		else {
 			PotionType[] types = PotionType.values();
-			dropPotion(dropChunk, dropFrom, types[rand.nextInt(types.length)], rand.nextInt(1));
+			dropPotion(loc, types[rand.nextInt(types.length)], rand.nextInt(1));
 		}
 	}
 
-	private void dropPotion(Chunk dropChunk, Location dropFrom, PotionType type, int modifier) {
+	/**
+	 * Drop a potion.
+	 * 
+	 * @param loc The location to drop it at.
+	 * @param type The potion type to use.
+	 * @param modifier The tier of the potion.
+	 */
+	private void dropPotion(Location loc, PotionType type, int modifier) {
 		if (type == PotionType.WATER) {
 			return;
 		}
 		
-		Potion potion = new Potion(type, 1 + modifier);
+		final Potion potion = new Potion(type, 1 + modifier);
 		potion.setSplash(true);
 		
+		// idk why, but berg said this works and it does so I won't question him
 		if (potion.getType() != PotionType.INSTANT_DAMAGE && potion.getType() != PotionType.INSTANT_HEAL) {
 			potion.extend();
 		}
 		
-		ItemStack itemStack = potion.toItemStack(1);
+		final ItemStack itemStack = potion.toItemStack(1);
 		
+		// I don't want air to be dropped, it spams the console.
 		if (itemStack.getType() == Material.AIR) {
 			return;
 		}
 
-		Chicken entity = dropChunk.getWorld().spawn(dropFrom, Chicken.class);
-		ThrownPotion pot = entity.launchProjectile(ThrownPotion.class, new Vector(0, -1, 0));
+		final Chicken entity = world.spawn(loc, Chicken.class);
+		final ThrownPotion pot = entity.launchProjectile(ThrownPotion.class, new Vector(0, -1, 0));
 		
 		pot.setVelocity(new Vector(0, -1, 0));
 		pot.setItem(itemStack);
