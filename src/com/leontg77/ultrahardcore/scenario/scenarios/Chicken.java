c012f49135bcec3d54edd4b2ef2ef1c9fd557492
LeonTG77 <leontg77@gmail.com>
2016-01-24 11:04:41 +0100

    Added the chicken scenario

---
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/Chicken.java
@@ -0,0 +1,44 @@
+package com.leontg77.ultrahardcore.scenario.scenarios;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+import com.leontg77.ultrahardcore.State;
+import com.leontg77.ultrahardcore.events.GameStartEvent;
+import com.leontg77.ultrahardcore.scenario.Scenario;
+import com.leontg77.ultrahardcore.utils.PlayerUtils;
+
+/**
+ * Chicken scenario class
+ * 
+ * @author LeonTG77
+ */
+public class Chicken extends Scenario implements Listener {
+
+	public Chicken() {
+		super("Chicken", "Everyone starts on 1/2 heart with a notch apple.");
+	}
+
+	@Override
+	public void onDisable() {}
+
+	@Override
+	public void onEnable() {
+		if (!State.isState(State.INGAME)) {
+			return;
+		}
+		
+		on(new GameStartEvent());
+	}
+	
+	@EventHandler
+	public void on(final GameStartEvent event) {
+		for (Player online : PlayerUtils.getPlayers()) {
+        	PlayerUtils.giveItem(online, new ItemStack(Material.GOLDEN_APPLE, 1, (short) 1));
+        	online.setHealth(1);
+        }
+	}
+}
