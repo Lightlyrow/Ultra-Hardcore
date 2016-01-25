c9349835fa5e6a2b3fcdc5684448ab5237753e73
LeonTG77 <leontg77@gmail.com>
2016-01-26 00:06:33 +0100

    Completly rewrote the class

--- src/com/leontg77/ultrahardcore/scenario/scenarios/Astrophobia.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/Astrophobia.java
@@ -1,19 +1,21 @@
 package com.leontg77.ultrahardcore.scenario.scenarios;
 
+import java.util.List;
 import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Builder;
+import org.bukkit.FireworkEffect.Type;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Firework;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.entity.TNTPrimed;
@@ -35,31 +37,31 @@ import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-import com.leontg77.ultrahardcore.Game;
 import com.leontg77.ultrahardcore.Main;
 import com.leontg77.ultrahardcore.State;
 import com.leontg77.ultrahardcore.Timers;
 import com.leontg77.ultrahardcore.events.GameStartEvent;
 import com.leontg77.ultrahardcore.scenario.Scenario;
 import com.leontg77.ultrahardcore.utils.BlockUtils;
-import com.leontg77.ultrahardcore.utils.PlayerUtils;
+import com.leontg77.ultrahardcore.utils.GameUtils;
 
 /**
  * Astrophobia scenario class
  * 
- * @author Bergasms
+ * @author Bergasms, modified by LeonTG77
  */
 public class Astrophobia extends Scenario implements Listener {
-	private Game game = Game.getInstance();
+	private BukkitRunnable task = null;
+
+	private static final long TICKS_TO_START = 6000;
+	private static final long TICK_INTERVAL = 100L;
 	
-	private long tickInterval = 100L;
-	private int astroTask = -1;
-	private int frequency = 5;
-	private int fuse = 150;
+	private static final int FREQUENCY = 5;
+	private static final int FUSE = 150;
 	
-	private float chancePerDiamond = 10.0F;
-	private float chancePerGold = 30.0F;
-	private float chancePerIron = 60.0F;
+	private static final double CHANCE_PER_DIAMOND = 0.1;
+	private static final double CHANCE_PER_GOLD = 0.3;
+	private static final double CHANCE_PER_IRON = 0.6;
 
 	public Astrophobia() {
 		super("Astrophobia", "The sun is gone, and the world is in eternal night. Deadly meteors impact the surface at random, leaving craters and flames, and sometimes ores. Aliens arrive from the sky wearing protective armor and shooting powerful weapons. Their tracking bombs (charged creepers) are fired onto the world to target any players that come near them. After defeating one, players can spawn a charged creeper of their own.");
@@ -67,62 +69,75 @@ public class Astrophobia extends Scenario implements Listener {
 
 	@Override
 	public void onDisable() {
-		Game game = Game.getInstance();
 		game.getWorld().setGameRuleValue("doDaylightCycle", "true");
 
-		if (astroTask != -1) {
-			Bukkit.getScheduler().cancelTask(astroTask);
+		if (task != null && Bukkit.getScheduler().isCurrentlyRunning(task.getTaskId())) {
+			task.cancel();
 		}
 		
-		astroTask = -1;
+		task = null;
 	}
 	
 	@Override
-	public void onEnable() {}
+	public void onEnable() {
+		if (!State.isState(State.INGAME)) {
+			return;
+		}
+		
+		on(new GameStartEvent());
+	}
 	
 	@EventHandler
 	public void on(GameStartEvent event) {
-		Game game = Game.getInstance();
-		
 		game.getWorld().setGameRuleValue("doDaylightCycle", "false");
 		game.getWorld().setTime(18000);
 		
-		astroTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.plugin, new Runnable() {
+		task = new BukkitRunnable() {
 			public void run() {
-				for (int i = 0; i < frequency; i++) {
+				for (int i = 0; i < FREQUENCY; i++) {
 					dropAstroItem();
 				}
 			}
-		}, 6000, tickInterval);
+		};
+		
+		task.runTaskTimer(Main.plugin, TICKS_TO_START - (Timers.timeSeconds * 20), TICK_INTERVAL);
 	}
 
 	@EventHandler
-	public void onBlockIgnite(BlockIgniteEvent event) {
+	public void on(BlockIgniteEvent event) {
 		if (!State.isState(State.INGAME)) {
 			return;
 		}
 		
-		IgniteCause cause = event.getCause();
-
-		if (cause == IgniteCause.SPREAD) {
-			event.setCancelled(true);
+		final Block block = event.getBlock();
+		
+		if (game.getWorld() != null && block.getWorld() != game.getWorld()) {
+			return;
 		}
+		
+		final IgniteCause cause = event.getCause();
+
+		if (cause != IgniteCause.SPREAD) {
+			return;
+		}
+		
+		event.setCancelled(true);
 	}
 
 	@EventHandler
-	public void onEntityShootBowEvent(EntityShootBowEvent event) {
+	public void on(EntityShootBowEvent event) {
 		if (!State.isState(State.INGAME)) {
 			return;
 		}
 		
-		Entity proj = event.getProjectile();
-		Entity entity = event.getEntity();
+		final Entity proj = event.getProjectile();
+		final Entity entity = event.getEntity();
 		
 		if (event.isCancelled()) {
 			return;
 		}
 		
-		if (game.getWorld() == null) {
+		if (!GameUtils.getGameWorlds().contains(entity.getWorld())) {
 			return;
 		}
 		
@@ -134,23 +149,18 @@ public class Astrophobia extends Scenario implements Listener {
 			return;
 		}
 		
-		Skeleton skelly = (Skeleton) entity;
+		final Skeleton skelly = (Skeleton) entity;
 		
 		if (skelly.getEquipment().getHelmet().getType() != Material.GLASS) {
 			return;
 		}
 		
-		Firework f = (Firework) game.getWorld().spawnEntity(event.getEntity().getEyeLocation(), EntityType.FIREWORK);
-		FireworkMeta fwm = f.getFireworkMeta();
-		FireworkEffect effect = FireworkEffect.builder().withTrail().with(FireworkEffect.Type.STAR).withColor(new Color[] { Color.GREEN, Color.GREEN }).with(FireworkEffect.Type.BURST).build();
-		
-		fwm.addEffect(effect);
-		f.setFireworkMeta(fwm);
-		proj.setPassenger(f);
+		final Firework firework = game.getWorld().spawn(skelly.getEyeLocation(), Firework.class);
+		proj.setPassenger(firework);
 	}
 
 	@EventHandler
-	public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
+	public void on(EntityDamageByEntityEvent event) {
 		if (!State.isState(State.INGAME)) {
 			return;
 		}
@@ -165,93 +175,91 @@ public class Astrophobia extends Scenario implements Listener {
 			return;
 		}
 		
-		Firework fw = (Firework) damager.getPassenger();
-		FireworkMeta fwMeta = fw.getFireworkMeta();
-		FireworkEffect effect = FireworkEffect.builder().withTrail().with(FireworkEffect.Type.BALL_LARGE).withColor(new Color[] { Color.RED, Color.RED }).with(FireworkEffect.Type.BURST).build();
+		if (!GameUtils.getGameWorlds().contains(damager.getWorld())) {
+			return;
+		}
 		
-		fwMeta.addEffect(effect);
-		fw.setFireworkMeta(fwMeta);
-		fw.detonate();
+		final Firework firework = (Firework) damager.getPassenger();
+		final FireworkMeta fireworkMeta = firework.getFireworkMeta();
+		
+		final Builder builder = FireworkEffect.builder();
+
+		builder.with(Type.BALL_LARGE);
+		builder.with(Type.BURST);
+		builder.withColor(Color.RED, Color.RED);
+		builder.withTrail();
+		
+		final FireworkEffect effect = builder.build();
+		
+		fireworkMeta.addEffect(effect);
+		firework.setFireworkMeta(fireworkMeta);
+		firework.detonate();
 		
 		event.setDamage(6.0D);
 	}
 
 	@EventHandler
-	public void onEntityExplode(EntityExplodeEvent event) {
+	public void on(EntityExplodeEvent event) {
 		if (!State.isState(State.INGAME)) {
 			return;
 		}
 		
-		Entity entity = event.getEntity();
+		final Entity entity = event.getEntity();
 		
 		if (!entity.hasMetadata("meteor")) {
 			return;
 		}
 		
+		if (!GameUtils.getGameWorlds().contains(entity.getWorld())) {
+			return;
+		}
+		
 		final Location loc = event.getLocation();
+		final Random rand = new Random();
 
-		new BukkitRunnable() {
-			public void run() {
-				for (Entity near : PlayerUtils.getNearby(loc, 10)) {
-					if (near.getType() != EntityType.DROPPED_ITEM) {
-						continue;
-					}
-					
-					Item item = (Item) near;
-					Material type = item.getItemStack().getType();
-					
-					switch (type) {
-					case COBBLESTONE:
-					case SAND:
-					case DIRT:
-					case SANDSTONE:
-					case GRAVEL:
-					case SAPLING:
-					case LOG:
-					case LOG_2:
-					case SEEDS:
-						near.remove();
-					default:
-						break;
-					}
-				}
-				
-				Random r = new Random();
-				
-				if (r.nextBoolean()) {
-					return;
-				}
-				
-				float fr = r.nextFloat() * 100.0F;
-				
-				if (fr < chancePerDiamond) {
-					BlockUtils.dropItem(loc, new ItemStack(Material.DIAMOND, 1));
-					return;
-				} 
-				
-				if (fr < chancePerDiamond + chancePerGold) {
-					BlockUtils.dropItem(loc, new ItemStack(Material.GOLD_ORE, 2));
-					return;
-				} 
-				
-				if (fr < chancePerDiamond + chancePerGold + chancePerIron) {
-					BlockUtils.dropItem(loc, new ItemStack(Material.IRON_ORE, 2));
-				}
-			}
-		}.runTaskLater(Main.plugin, 2L);
+		final List<Block> blocks = event.blockList();
+		
+		for (Block block : blocks) {
+			block.setType(Material.AIR);
+		}
+		
+		blocks.clear();
+		
+		if (rand.nextBoolean()) {
+			return;
+		}
+		
+		final double chance = rand.nextDouble();
+		
+		if (chance < CHANCE_PER_DIAMOND) {
+			BlockUtils.dropItem(loc, new ItemStack(Material.DIAMOND, 1));
+			return;
+		} 
+		
+		if (chance < CHANCE_PER_DIAMOND + CHANCE_PER_GOLD) {
+			BlockUtils.dropItem(loc, new ItemStack(Material.GOLD_ORE, 2));
+			return;
+		} 
+		
+		if (chance < CHANCE_PER_DIAMOND + CHANCE_PER_GOLD + CHANCE_PER_IRON) {
+			BlockUtils.dropItem(loc, new ItemStack(Material.IRON_ORE, 2));
+		}
 	}
 
 	@EventHandler
-	public void onCreatureSpawn(CreatureSpawnEvent event) {
+	public void on(CreatureSpawnEvent event) {
 		if (!State.isState(State.INGAME)) {
 			return;
 		}
 		
-		Entity entity = event.getEntity();
+		final Entity entity = event.getEntity();
+		
+		if (!GameUtils.getGameWorlds().contains(entity.getWorld())) {
+			return;
+		}
 		
 		if (entity instanceof Monster && Timers.time < 10 && entity.getLocation().getY() > 60) {
 			event.setCancelled(true);
-			return;
 		}
 		
 		if (!(entity instanceof Creeper)) {
@@ -262,91 +270,103 @@ public class Astrophobia extends Scenario implements Listener {
 			return;
 		}
 		
-		Creeper creep = (Creeper) event.getEntity();
+		final Creeper creep = (Creeper) entity;
+		event.setCancelled(false);
 		
 		creep.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 10, 10), true);
 		creep.setPowered(true);
 
 		creep.getEquipment().setItemInHand(new ItemStack(Material.MONSTER_EGG, 1, (short) 50));
-		creep.setCanPickupItems(false);
 		creep.getEquipment().setItemInHandDropChance(100.0F);
+		creep.setCanPickupItems(false);
 	}
 
+	/**
+	 * Drop an astrophobia item.
+	 */
 	private void dropAstroItem() {
-		Chunk[] chunks = game.getWorld().getLoadedChunks();
-
-		Random r = new Random();
+		if (game.getWorld() == null) {
+			return;
+		}
+		
+		final Chunk[] chunks = game.getWorld().getLoadedChunks();
+		final Random rand = new Random();
 		
 		if (chunks.length == 0) {
 			return;
 		}
 			
-		Chunk dropChunk = chunks[r.nextInt(chunks.length)];
-		Location dropFrom = new Location(dropChunk.getWorld(), dropChunk.getX() * 16 + r.nextInt(16), 255.0D, dropChunk.getZ() * 16 + r.nextInt(15));
+		final Chunk chunk = chunks[rand.nextInt(chunks.length)];
+		final Location loc = new Location(game.getWorld(), chunk.getX() * 16 + rand.nextInt(16), 255.0D, chunk.getZ() * 16 + rand.nextInt(15));
 
-		int chance = r.nextInt(100);
+		final int chance = rand.nextInt(100);
+		
+		final Vector vec = new Vector(rand.nextFloat() * 10.0F - 5.0F, -1.0F * rand.nextFloat(), rand.nextFloat() * 10.0F - 5.0F);
 
 		if (chance < 70) {
-			TNTPrimed tnt = (TNTPrimed) game.getWorld().spawn(dropFrom, TNTPrimed.class);
+			final TNTPrimed meteor = (TNTPrimed) game.getWorld().spawn(loc, TNTPrimed.class);
 			
-			tnt.setVelocity(new Vector((r.nextFloat() * 10.0F - 5.0F) / 2.0F, -1.0F * r.nextFloat(), (r.nextFloat() * 10.0F - 5.0F) / 2.0F));
-			tnt.setFallDistance(150.0F);
-			tnt.setFuseTicks(fuse);
+			meteor.setVelocity(new Vector((rand.nextFloat() * 10.0F - 5.0F) / 2.0F, -1.0F * rand.nextFloat(), (rand.nextFloat() * 10.0F - 5.0F) / 2.0F));
+			meteor.setFallDistance(150.0F);
+			meteor.setFuseTicks(FUSE);
 			
-			tnt.setMetadata("meteor", new FixedMetadataValue(Main.plugin, "isMeteor"));
+			meteor.setMetadata("meteor", new FixedMetadataValue(Main.plugin, "isMeteor"));
 			
-			tnt.setIsIncendiary(r.nextBoolean());
-			tnt.setYield(Math.max((float) ((1000.0D - dropFrom.distance(new Location(game.getWorld(), 0.0D, 63.0D, 0.0D))) / 100.0D), 7.0F));
+			meteor.setIsIncendiary(rand.nextBoolean());
+			meteor.setYield(Math.max((float) ((1000.0D - loc.distance(new Location(game.getWorld(), 0.0D, 63.0D, 0.0D))) / 100.0D), 7.0F));
 			return;
 		} 
 		
 		if (chance < 85) {
-			Creeper creep = (Creeper) game.getWorld().spawnEntity(dropFrom.subtract(0.0D, 155.0D, 0.0D), EntityType.CREEPER);
+			final Creeper creep = game.getWorld().spawn(loc.subtract(0.0D, 155.0D, 0.0D), Creeper.class);
+			
 			creep.setPowered(true);
+			
 			creep.setFallDistance(-500.0F);
 			creep.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 10, 10), true);
-			creep.setVelocity(new Vector(r.nextFloat() * 10.0F - 5.0F, -1.0F * r.nextFloat(), r.nextFloat() * 10.0F - 5.0F));
+			creep.setVelocity(vec);
 
 			creep.getEquipment().setItemInHand(new ItemStack(Material.MONSTER_EGG, 1, (short) 50));
-			creep.setCanPickupItems(false);
 			creep.getEquipment().setItemInHandDropChance(100.0F);
+			creep.setCanPickupItems(false);
 			return;
 		} 
 		
 		if (chance < 100) {
-			Vector cel = new Vector(r.nextFloat() * 10.0F - 5.0F, -1.0F * r.nextFloat(), r.nextFloat() * 10.0F - 5.0F);
-			
 			for (int i = 2; i < 8; i++) {
-				Skeleton creep = (Skeleton) game.getWorld().spawnEntity(dropFrom.subtract(0.0D, 155.0D, 0.0D), EntityType.SKELETON);
-				creep.setFallDistance(-500.0F);
-				creep.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 9999, 10), true);
-				creep.setCanPickupItems(false);
-
-				ItemStack entitychest = new ItemStack(Material.LEATHER_CHESTPLATE);
-				ItemStack entityleg = new ItemStack(Material.LEATHER_LEGGINGS);
-				ItemStack entityboots = new ItemStack(Material.LEATHER_BOOTS);
-
-				ItemStack[] items = { entitychest, entityleg, entityboots };
-				String[] names = { "Space Suit", "Space Pants", "Space Boots" };
-				Color[] colours = { Color.GRAY, Color.GRAY, Color.GRAY };
+				final Skeleton skele = game.getWorld().spawn(loc.subtract(0.0D, 155.0D, 0.0D), Skeleton.class);
 				
-				int ind = 0;
+				skele.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 9999, 10), true);
+				skele.setCanPickupItems(false);
+				skele.setFallDistance(-500.0F);
+
+				final ItemStack entitychest = new ItemStack(Material.LEATHER_CHESTPLATE);
+				final ItemStack entityleg = new ItemStack(Material.LEATHER_LEGGINGS);
+				final ItemStack entityboots = new ItemStack(Material.LEATHER_BOOTS);
+
+				final ItemStack[] items = { entitychest, entityleg, entityboots };
+				final String[] names = { "Space Suit", "Space Pants", "Space Boots" };
+				final Color[] colours = { Color.GRAY, Color.GRAY, Color.GRAY };
+				
+				int index = 0;
 				
 				for (ItemStack item : items) {
-					LeatherArmorMeta lma = (LeatherArmorMeta) item.getItemMeta();
-					lma.setColor(colours[ind]);
-					lma.setDisplayName(names[ind]);
-					item.setItemMeta(lma);
-					ind++;
+					final LeatherArmorMeta meta = (LeatherArmorMeta) item.getItemMeta();
+					
+					meta.setColor(colours[index]);
+					meta.setDisplayName(names[index]);
+					item.setItemMeta(meta);
+					
+					index++;
 				}
 				
-				creep.getEquipment().setArmorContents(items);
+				skele.getEquipment().setArmorContents(items);
 
-				ItemStack entityhelm = new ItemStack(Material.GLASS);
-				creep.getEquipment().setHelmet(entityhelm);
-				creep.setVelocity(cel);
+				final ItemStack entityhelm = new ItemStack(Material.GLASS);
+				skele.getEquipment().setHelmet(entityhelm);
+				skele.setVelocity(vec);
 
-				creep.getEquipment().setItemInHand(new ItemStack(Material.BOW));
+				skele.getEquipment().setItemInHand(new ItemStack(Material.BOW));
 			}
 		}
 	}
