b67ef67df6665ef9fd2f709795f15df955d1c4bc
LeonTG77 <leontg77@gmail.com>
2016-01-26 00:19:06 +0100

    Disabled shift crafting of crafting tables

--- src/com/leontg77/ultrahardcore/scenario/scenarios/BenchBlitz.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/BenchBlitz.java
@@ -1,7 +1,8 @@
 package com.leontg77.ultrahardcore.scenario.scenarios;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -13,6 +14,7 @@ import org.bukkit.inventory.ItemStack;
 
 import com.leontg77.ultrahardcore.State;
 import com.leontg77.ultrahardcore.scenario.Scenario;
+import com.leontg77.ultrahardcore.utils.GameUtils;
 
 /**
  * BenchBlitz scenario class
@@ -20,7 +22,8 @@ import com.leontg77.ultrahardcore.scenario.Scenario;
  * @author LeonTG77
  */
 public class BenchBlitz extends Scenario implements Listener {
-	private List<String> hasCrafted = new ArrayList<String>();
+	private static final String PREFIX = "§6§lBenchBlitz §8» §7";
+	private final Set<UUID> hasCrafted = new HashSet<UUID>();
 	
 	public BenchBlitz() {
 		super("BenchBlitz", "You can only craft one crafting table.");
@@ -33,7 +36,7 @@ public class BenchBlitz extends Scenario implements Listener {
 
 	@Override
 	public void onEnable() {
-		hasCrafted.clear();
+		onDisable();
 	}
 	
 	@EventHandler
@@ -43,38 +46,30 @@ public class BenchBlitz extends Scenario implements Listener {
 		}
 		
 		// this cast is safe, fyi.
-		Player player = (Player) event.getWhoClicked();
-		ItemStack item = event.getCurrentItem();
+		final Player player = (Player) event.getWhoClicked();
+		final ItemStack item = event.getCurrentItem();
+		
+		if (!GameUtils.getGamePlayers().contains(player)) {
+			return;
+		}
 		
 		if (item.getType() != Material.WORKBENCH) {
 			return;
 		}
 		
-		if (hasCrafted.contains(player.getName())) {
-			player.sendMessage(ChatColor.RED + "You are too weak to arrange wood in a square!");
+		if (event.isShiftClick()) {
+			player.sendMessage(ChatColor.RED + "You cannot shift click to craft crafting tables.");
+			event.setCancelled(true);
+			return;
+		}
+		
+		if (hasCrafted.contains(player.getUniqueId())) {
+			player.sendMessage(PREFIX + "You already crafted your only crafting table.");
 			event.setCancelled(true);
 			return;
 		}
 
-		player.sendMessage(ChatColor.GOLD + "That's your only bench! Don't lose it!");
-		hasCrafted.add(player.getName());
-		event.setCancelled(true);
-		
-		// the rest of the code is to make sure the only get 1.
-		if (item != null) {
-			item.setType(Material.AIR);
-		}
-		
-		for (ItemStack content : player.getInventory().getContents()) {
-			if (content == null) {
-				continue;
-			}
-			
-			if (content.getType() == Material.WORKBENCH) {
-				content.setType(Material.AIR);
-			}
-		}
-		
-		player.getInventory().addItem(new ItemStack(Material.WORKBENCH));
+		player.sendMessage(PREFIX + "That's your only crafting table, Don't lose it!");
+		hasCrafted.add(player.getUniqueId());
 	}
 }
