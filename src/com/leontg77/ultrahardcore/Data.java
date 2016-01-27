132f343c7e5739a89cc3536f5a63d1403cdca53e
LeonTG77 <leontg77@gmail.com>
2016-01-27 08:21:33 +0100

    Added a data storage class

---
+++ src/com/leontg77/ultrahardcore/Data.java
@@ -0,0 +1,16 @@
+package com.leontg77.ultrahardcore;
+
+public class Data {
+
+	public Data() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void store() {
+		
+	}
+	
+	public void restore() {
+		
+	}
+}
