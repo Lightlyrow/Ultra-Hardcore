e076014af926af211d142fb8a298a144cdcb76f2
LeonTG77 <leontg77@gmail.com>
2016-01-29 13:49:08 +0100

    Updated imports

--- src/com/leontg77/ultrahardcore/scenario/scenarios/Armageddon.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/Armageddon.java
@@ -18,7 +18,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import com.leontg77.ultrahardcore.Main;
-import com.leontg77.ultrahardcore.events.GameStartEvent;
+import com.leontg77.ultrahardcore.events.uhc.GameStartEvent;
 import com.leontg77.ultrahardcore.scenario.Scenario;
 import com.leontg77.ultrahardcore.utils.BlockUtils;
 
