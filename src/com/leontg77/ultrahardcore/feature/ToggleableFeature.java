b78c304f382302a348064f0e6f037b52b851a466
LeonTG77 <leontg77@gmail.com>
2016-02-06 03:03:33 +0100

    Finished the class

--- src/com/leontg77/ultrahardcore/feature/ToggleableFeature.java
+++ src/com/leontg77/ultrahardcore/feature/ToggleableFeature.java
@@ -1,35 +1,38 @@
 package com.leontg77.ultrahardcore.feature;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import com.leontg77.ultrahardcore.inventory.InvGUI;
+
+/**
+ * A toggleable feature is a feature you can disable and enable.
+ * 
+ * @author LeonTG77
+ */
 public abstract class ToggleableFeature extends Feature {
 	private boolean enabled = false;
 	
-	private ItemStack toggleItem;
-	private int invSlot;
+	protected ItemStack icon = new ItemStack(Material.BARRIER);
+	protected int slot = 0;
 	
-	public ToggleableFeature(String name, String description, ItemStack toggleItem, int invSlot) {
+	/**
+	 * ToggleableFeature constructor.
+	 * 
+	 * @param name The name of the feature.
+	 * @param description A short description of the feature.
+	 * @param toggleItem The item to be used as a toggle item.
+	 * @param invSlot The slot for the toggle item.
+	 */
+	public ToggleableFeature(String name, String description) {
 		super(name, description);
 		
-		this.toggleItem = toggleItem;
-		this.invSlot = invSlot;
-	}
-	
-	public ItemStack getToggleItem() {
-		final ItemStack item = toggleItem;
-		final ItemMeta meta = item.getItemMeta();
-		meta.setDisplayName((isEnabled() ? ChatColor.GREEN : ChatColor.RED) + getName());
-		meta.setLore(Arrays.asList(" ", "§8» §7" + getDescription(), " "));
-		item.setItemMeta(meta);
-		return item;
-	}
-	
-	public int getInventorySlot() {
-		return invSlot;
+		this.enabled = settings.getConfig().getBoolean("feature." + getName().toLowerCase() + ".enabled", false);
 	}
 
 	/**
@@ -55,8 +58,10 @@ public abstract class ToggleableFeature extends Feature {
 		settings.getConfig().set("feature." + getName().toLowerCase() + ".enabled", true);
 		settings.saveConfig();
 		
-		enabled = true;
+		this.enabled = true;
 		onEnable();
+		
+		InvGUI.getGameInfo().update();
 		return true;
 	}
 	
@@ -73,8 +78,10 @@ public abstract class ToggleableFeature extends Feature {
 		settings.getConfig().set("feature." + getName().toLowerCase() + ".enabled", false);
 		settings.saveConfig();
 		
-		enabled = false;
+		this.enabled = false;
 		onDisable();
+		
+		InvGUI.getGameInfo().update();
 		return true;
 	}
 	
@@ -88,11 +95,63 @@ public abstract class ToggleableFeature extends Feature {
 	}
 	
 	/**
-	 * Check if the scenario is enabled
+	 * Check if the feature is enabled
 	 * 
 	 * @return True if enabled, false otherwise.
 	 */
 	public boolean isEnabled() {
 		return enabled;
+	}
+	
+	/**
+	 * Get the item to be used for the config inventory for this option.
+	 * 
+	 * @return The item to use.
+	 */
+	public ItemStack getToggleItem() {
+		final ItemMeta meta = icon.getItemMeta();
+		
+		final String currentDesc = getDescription();
+		String newDesc = "";
+		
+		int i = 0;
+		
+		for (String split : currentDesc.split(" ")) {
+			if (i >= 3 && split.length() > 3) {
+				newDesc += split + ";";
+				i = 0;
+			} else {
+				newDesc += split + " ";
+				i++;
+			}
+		}
+		
+		final List<String> lore = new ArrayList<String>();
+
+		lore.add(" ");
+		lore.add("§8» §7Currently: " + (isEnabled() ? ChatColor.GREEN + "Enabled" : ChatColor.RED + "Disabled"));
+		lore.add(" ");
+		lore.add("§8» §cDescription: ");
+		
+		for (String split : newDesc.split(";")) {
+			lore.add("§8» §7" + split);
+		}
+		
+		lore.add(" ");
+		
+		meta.setDisplayName("§8» §6" + getName() + " §8«");
+		meta.setLore(lore);
+		icon.setItemMeta(meta);
+		
+		return icon;
+	}
+	
+	/**
+	 * Get the slot for the toggle item.
+	 * 
+	 * @return The slot for the item to be placed in.
+	 */
+	public int getInventorySlot() {
+		return slot;
 	}
 }
