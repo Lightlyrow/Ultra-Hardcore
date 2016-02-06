293048df8990c529d04a5056dcd5db01084625c1
LeonTG77 <leontg77@gmail.com>
2016-02-06 02:56:14 +0100

    Code improvement

--- src/com/leontg77/ultrahardcore/feature/Feature.java
+++ src/com/leontg77/ultrahardcore/feature/Feature.java
@@ -3,6 +3,11 @@ package com.leontg77.ultrahardcore.feature;
 import com.leontg77.ultrahardcore.Game;
 import com.leontg77.ultrahardcore.Settings;
 
+/**
+ * A feature of the plugin.
+ * 
+ * @author LeonTG77
+ */
 public abstract class Feature {
 	protected final Settings settings = Settings.getInstance();
 	protected final Game game = Game.getInstance();
@@ -11,12 +16,12 @@ public abstract class Feature {
 	private String name;
 	
 	/**
-	 * The feature class constructor
+	 * Feature constructor
 	 * 
 	 * @param name The name of the feature.
-	 * @param description The description of the feature.
+	 * @param description A short description of the feature.
 	 */
-	protected Feature(String name, String description) {
+	public Feature(String name, String description) {
 		this.description = description;
 		this.name = name;
 	}
