f57870af6f7b6d4aa5c0784e143e67a2d90444f5
LeonTG77 <leontg77@gmail.com>
2016-02-10 08:24:37 +0100

    Added the bow damage health feature

---
+++ src/com/leontg77/ultrahardcore/feature/pvp/ShootHealthFeature.java
@@ -0,0 +1,64 @@
+package com.leontg77.ultrahardcore.feature.pvp;
+
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.leontg77.ultrahardcore.Main;
+import com.leontg77.ultrahardcore.feature.Feature;
+import com.leontg77.ultrahardcore.scenario.ScenarioManager;
+import com.leontg77.ultrahardcore.scenario.scenarios.Paranoia;
+import com.leontg77.ultrahardcore.scenario.scenarios.TeamHealth;
+import com.leontg77.ultrahardcore.utils.NumberUtils;
+
+/**
+ * Shoot Health feature class.
+ * 
+ * @author LeonTG77
+ */
+public class ShootHealthFeature extends Feature implements Listener {
+
+	public ShootHealthFeature() {
+		super("Shoot Health", "Displays the shot persons health to the shooter.");
+	}
+	
+	@EventHandler(ignoreCancelled = true)
+	public void on(EntityDamageByEntityEvent event) {
+		final Entity attacked = event.getEntity();
+		final Entity attacker = event.getDamager();
+		
+		final ScenarioManager scen = ScenarioManager.getInstance();
+		
+    	if (game.isRecordedRound() || scen.getScenario(TeamHealth.class).isEnabled() || scen.getScenario(Paranoia.class).isEnabled()) {
+			return;
+		}
+    	
+    	if (!(attacked instanceof Player) || !(attacker instanceof Arrow)) {
+			return;
+		}
+    	
+    	final Player player = (Player) attacked;
+		final Arrow arrow = (Arrow) attacker;
+		
+		if (!(arrow.getShooter() instanceof Player)) {
+			return;
+		}
+		
+		new BukkitRunnable() {
+			public void run() {
+				final Player killer = (Player) arrow.getShooter();
+				
+				final double health = player.getHealth();
+				final String percent = NumberUtils.makePercent(health);
+				
+				if (health > 0.0000) {
+					killer.sendMessage(Main.PREFIX + "§6" + player.getName() + " §7is now at §a" + percent + "%");
+				}
+			}
+		}.runTaskLater(Main.plugin, 1);
+	}
+}
