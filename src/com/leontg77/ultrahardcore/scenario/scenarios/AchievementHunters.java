0eba699de621e992c10432cef86e72b3a6016439
LeonTG77 <leontg77@gmail.com>
2016-03-01 23:20:25 +0100

    Added the new scenario

---
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/AchievementHunters.java
@@ -0,0 +1,44 @@
+package com.leontg77.ultrahardcore.scenario.scenarios;
+
+import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.leontg77.ultrahardcore.scenario.Scenario;
+
+/**
+ * Cloud9 scenario class
+ * 
+ * @author LeonTG77
+ */
+public class AchievementHunters extends Scenario implements Listener {
+	private BukkitRunnable task;
+	
+	public AchievementHunters() {
+		super("Cloud9", "After 45 minutes, any player below y: 200 will begin to take half a heart of damage every 30 seconds, there are no starter items.");
+	}
+
+	@Override
+	public void onDisable() {
+		task.cancel();
+	}
+
+	@Override
+	public void onEnable() {}
+	
+//	@EventHandler
+//	public void on(PvPEnableEvent event) {
+//		task = new BukkitRunnable() {
+//			public void run() {
+//				for (Player online : game.getPlayers()) {
+//					if (online.getLocation().getY() > 200) {
+//						continue;
+//					}
+//
+//					PlayerUtils.damage(online, 1);
+//				}
+//			}
+//		};
+//		
+//		task.runTaskTimer(Main.plugin, 600, 600);
+//	}
+}
