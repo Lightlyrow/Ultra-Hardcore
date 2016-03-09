f87c8a58732d634f7c49fb58fdc6db5b942bb7fe
LeonTG77 <leontg77@gmail.com>
2016-03-09 08:37:33 +0100

    Changed pve death colors and made it use new instances

--- src/com/leontg77/ultrahardcore/feature/scoreboard/KillBoardFeature.java
+++ src/com/leontg77/ultrahardcore/feature/scoreboard/KillBoardFeature.java
@@ -1,8 +1,5 @@
 package com.leontg77.ultrahardcore.feature.scoreboard;
 
-import java.util.List;
-
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,6 +8,7 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.scoreboard.Team;
 
 import com.leontg77.ultrahardcore.Arena;
+import com.leontg77.ultrahardcore.Game;
 import com.leontg77.ultrahardcore.State;
 import com.leontg77.ultrahardcore.feature.Feature;
 import com.leontg77.ultrahardcore.managers.BoardManager;
@@ -22,42 +20,44 @@ import com.leontg77.ultrahardcore.managers.TeamManager;
  * @author LeonTG77
  */
 public class KillBoardFeature extends Feature implements Listener {
+	private final Arena arena;
+	private final Game game;
+	
+	private final BoardManager board;
+	private final TeamManager teams;
 
-	public KillBoardFeature() {
+	public KillBoardFeature(Arena arena, Game game, BoardManager board, TeamManager teams) {
 		super("Kill Board", "Adds your kill to the sidebar when you get one.");
+		
+		this.arena = arena;
+		this.game = game;
+		
+		this.board = board;
+		this.teams = teams;
 	}
 
 	@EventHandler(priority = EventPriority.LOW)
 	public void on(final PlayerDeathEvent event) {
-		if (game.isRecordedRound()) {
-			return;
-		}
-		
-		final Player player = event.getEntity();
-		final Arena arena = Arena.getInstance();
+		Player player = event.getEntity();
 		
 		// the arena has it's own way of doing deaths.
 		if (arena.isEnabled() && arena.hasPlayer(player)) {
 			return;
 		} 
-
-		final List<World> worlds = game.getWorlds();
 	    
-	    // I don't care about the rest if it hasn't started or they're not in a game world.
-	    if (!State.isState(State.INGAME) || !worlds.contains(player.getWorld())) {
+	    if (!State.isState(State.INGAME) || !game.getWorlds().contains(player.getWorld())) {
 	    	return;
 	    }
 
-		final BoardManager board = BoardManager.getInstance();
 		final Player killer = player.getKiller();
 
 		if (killer == null) {
-			board.setScore("§8» §a§lPvE", board.getScore("§8» §a§lPvE") + 1);
+			board.setScore("§8» §c§oPvE", board.getScore("§8» §c§oPvE") + 1);
 			return;
 		}
 		
-		final Team pteam = TeamManager.getInstance().getTeam(player);
-		final Team team = TeamManager.getInstance().getTeam(killer);
+		Team pteam = teams.getTeam(player);
+		Team team = teams.getTeam(killer);
 		
 		if (pteam != null && pteam.equals(team)) {
 			return;
