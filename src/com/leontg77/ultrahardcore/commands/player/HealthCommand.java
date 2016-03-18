de04fb5b7cc0c2bb7aaed685434e9c476f02416b
LeonTG77 <leontg77@gmail.com>
2016-03-18 16:11:39 +0100

    Made it say their health as well as their max health and absorption hearts

--- src/com/leontg77/ultrahardcore/commands/player/HealthCommand.java
+++ src/com/leontg77/ultrahardcore/commands/player/HealthCommand.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import com.leontg77.ultrahardcore.Main;
@@ -21,43 +22,47 @@ import com.leontg77.ultrahardcore.utils.NumberUtils;
  * @author LeonTG77
  */
 public class HealthCommand extends UHCCommand {
+	private final ScenarioManager scen;
 
-	public HealthCommand() {
+	public HealthCommand(ScenarioManager scen) {
 		super("health", "[player]");
+		
+		this.scen = scen;
 	}
 
 	@Override
 	public boolean execute(CommandSender sender, String[] args) throws CommandException {
-		ScenarioManager scen = ScenarioManager.getInstance();
-		
 		if (scen.getScenario(Paranoia.class).isEnabled() || scen.getScenario(TeamHealth.class).isEnabled()) {
-			throw new CommandException("/h is disabled in this scenario.");
+			throw new CommandException("/h is disabled in this gamemode.");
 		}
+		
+		Player target;
 		
 		if (args.length == 0) {
 			if (!(sender instanceof Player)) {
 				throw new CommandException("Only players can view their own health.");
 			}
 			
-			Player player = (Player) sender;
+			target = (Player) sender;
+		} else {
+			target = Bukkit.getPlayer(args[0]);
 			
-			double health = player.getHealth();
-			double maxhealth = player.getMaxHealth();
+			if (target == null) {
+				throw new CommandException("'" + args[0] + "' is not online.");
+			}
+		}
 
-			player.sendMessage(Main.PREFIX + "§7You are at §6" + NumberUtils.makePercent(health) + "%" + (maxhealth == 20 ? "" : " §7out of maximum §6" + NumberUtils.makePercent(maxhealth) + "%"));
-			return true;
-		}
-		
-		Player target = Bukkit.getPlayer(args[0]);
-		
-		if (target == null) {
-			throw new CommandException("'" + args[0] + "' is not online.");
-		}
+		CraftPlayer craft = (CraftPlayer) target;
 		
 		double health = target.getHealth();
 		double maxhealth = target.getMaxHealth();
+		
+		double absHearts = craft.getHandle().getAbsorptionHearts();
 
-		sender.sendMessage(Main.PREFIX + "§a" + target.getName() + " §7is at §6" + NumberUtils.makePercent(health) + "%" + (maxhealth == 20 ? "" : " §7out of maximum §6" + NumberUtils.makePercent(maxhealth) + "%"));
+		sender.sendMessage(Main.PREFIX + (target == sender ? "Your health:" : "§a" + target.getName() + "'s §7health:"));
+		sender.sendMessage(Main.ARROW + "Health: §a" + NumberUtils.makePercent(health) + "%");
+		sender.sendMessage(Main.ARROW + "Max Health: §a" + NumberUtils.makePercent(maxhealth).substring(2) + "%");
+		sender.sendMessage(Main.ARROW + "Absorption hearts: §e" + NumberUtils.makePercent(absHearts).substring(2) + "%");
 		return true;
 	}
 
