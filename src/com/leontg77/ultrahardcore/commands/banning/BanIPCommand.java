81f235d025ee3e4dbda46fde41540450cd8c6097
Leon Vaktskjold <Leon Vaktskjold>
2016-03-22 13:20:24 +0100

    Allowed the command to take player names, if player isn't found it treats the arg as an ip

--- src/com/leontg77/ultrahardcore/commands/banning/BanIPCommand.java
+++ src/com/leontg77/ultrahardcore/commands/banning/BanIPCommand.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.bukkit.BanEntry;
 import org.bukkit.BanList;
 import org.bukkit.BanList.Type;
 import org.bukkit.Bukkit;
@@ -29,39 +28,48 @@ public class BanIPCommand extends UHCCommand {
 	private final BoardManager board;
 
 	public BanIPCommand(BoardManager board) {
-		super("banip", "<ip> <reason>");
+		super("banip", "<ip|name> <reason>");
 		
 		this.board = board;
 	}
+	
+	private static final Type BANLIST_TYPE = Type.NAME;
 
 	@Override
-	public boolean execute(final CommandSender sender, final String[] args) throws CommandException {
+	public boolean execute(CommandSender sender, String[] args) throws CommandException {
 		if (args.length < 3) {
 			return false;
 		}
 		
-    	final BanList list = Bukkit.getBanList(Type.IP);
+		BanList list = Bukkit.getBanList(BANLIST_TYPE);
+		Player target = Bukkit.getPlayer(args[0]);
+		
+		String IP = args[0];
+		
+		if (target != null) {
+			IP = target.getAddress().getAddress().getHostAddress();
+		}
 
-		final String message = Joiner.on(' ').join(Arrays.copyOfRange(args, 1, args.length));
+		String message = Joiner.on(' ').join(Arrays.copyOfRange(args, 1, args.length));
 
 		PlayerUtils.broadcast(Main.PREFIX + "An IP has been banned for §a" + message);
-		final BanEntry ban = list.addBan(args[0], message, null, sender.getName());
+		list.addBan(IP, message, null, sender.getName());
 		
     	for (Player online : Bukkit.getOnlinePlayers()) {
-    		if (!online.getAddress().getAddress().getHostAddress().equals(args[0])) {
+    		if (!online.getAddress().getAddress().getHostAddress().equals(IP)) {
     			continue;
     		}
     		
 			board.resetScore(online.getName());
 	    	
-			final PlayerDeathEvent event = new PlayerDeathEvent(online, new ArrayList<ItemStack>(), 0, null);
-			Bukkit.getServer().getPluginManager().callEvent(event);
+			PlayerDeathEvent event = new PlayerDeathEvent(online, new ArrayList<ItemStack>(), 0, null);
+			Bukkit.getPluginManager().callEvent(event);
 			
 			online.kickPlayer(
 	    	"§8» §7You have been §4IP banned §7from §6Arctic UHC §8«" +
 	    	"\n" + 
-	    	"\n§cReason §8» §7" + ban.getReason() +
-	    	"\n§cBanned by §8» §7" + ban.getSource() +
+	    	"\n§cReason §8» §7" + message +
+	    	"\n§cBanned by §8» §7" + sender.getName() +
  			"\n" +
 	   		"\n§8» §7If you would like to appeal, DM our twitter §a@ArcticUHC §8«"
 	    	);
@@ -70,7 +78,15 @@ public class BanIPCommand extends UHCCommand {
 	}
 
 	@Override
-	public List<String> tabComplete(final CommandSender sender, final String[] args) {
-		return new ArrayList<String>();
+	public List<String> tabComplete(CommandSender sender, String[] args) {
+		List<String> toReturn = new ArrayList<String>();
+		
+		if (args.length == 1) {
+			for (Player online : Bukkit.getOnlinePlayers()) {
+				toReturn.add(online.getName());
+			}
+		}
+		
+		return toReturn;
 	}
 }
