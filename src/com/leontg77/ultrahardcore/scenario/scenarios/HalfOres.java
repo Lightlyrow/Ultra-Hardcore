beddfacb86ee0634dac20c6d07f2d602feef6aef
Leon Vaktskjold <Leon Vaktskjold>
2016-03-22 18:40:15 +0100

    Finished the scenario code

--- src/com/leontg77/ultrahardcore/scenario/scenarios/HalfOres.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/HalfOres.java
@@ -1,50 +1,60 @@
 package com.leontg77.ultrahardcore.scenario.scenarios;
 
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+import com.google.common.collect.ImmutableSet;
 import com.leontg77.ultrahardcore.scenario.Scenario;
+import com.leontg77.ultrahardcore.utils.BlockUtils;
 
 /**
- * Depths scenario class
+ * 1/2 Ores scenario class.
  * 
  * @author LeonTG77
  */
 public class HalfOres extends Scenario implements Listener {
+	private final Set<UUID> hasMined = new HashSet<UUID>();
 	
 	public HalfOres() {
-		super("Depths", "The lower you go, the more mobs damage you.");
+		super("1/2Ores", "For every 2 ores you mine, 1 drops.");
 	}
-
-	@Override
-	public void onDisable() {}
-
-	@Override
-	public void onEnable() {}
 	
-	@EventHandler
-	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-		if (event.getDamager() instanceof Player || (event.getDamager() instanceof Projectile && ((Projectile) event.getDamager()).getShooter() instanceof Player)) {
+	/**
+	 * A list of all ores in minecraft.
+	 */
+	private static final Set<Material> ORES = ImmutableSet.of(
+			Material.COAL_ORE, Material.IRON_ORE, Material.REDSTONE_ORE, Material.GLOWING_REDSTONE_ORE, Material.EMERALD_ORE,
+			Material.DIAMOND_ORE, Material.GOLD_ORE, Material.LAPIS_ORE
+	);
+	
+	@EventHandler(priority = EventPriority.LOW)
+	public void on(BlockBreakEvent event) {
+		Player player = event.getPlayer();
+		Block block = event.getBlock();
+		
+		if (!ORES.contains(block.getType())) {
 			return;
 		}
 		
-		Entity entity = event.getEntity();
-		
-		if (entity.getLocation().getBlockY() <= 15) {
-			event.setDamage(event.getDamage() * 5);
+		if (hasMined.contains(player.getUniqueId())) {
+			hasMined.remove(player.getUniqueId());
+			
+			event.setCancelled(true);
+			block.setType(Material.AIR);
+			
+			BlockUtils.blockBreak(player, block);
+			BlockUtils.degradeDurabiliy(player);
+		} else {
+			hasMined.add(player.getUniqueId());
 		}
-		else if (entity.getLocation().getBlockY() <= 30) {
-			event.setDamage(event.getDamage() * 3);
-		}
-		else if (entity.getLocation().getBlockY() <= 45) {
-			event.setDamage(event.getDamage() * 2);
-		}
-		else if (entity.getLocation().getBlockY() <= 60) {
-			event.setDamage(event.getDamage() * 1.5);
-		} 
 	}
 }
