25e2e5f506d47c87e239924e7a50090f6f49e0d6
LeonTG77 <leontg77@gmail.com>
2016-03-22 18:52:38 +0100

    New instances and user getting and muted status checker

--- src/com/leontg77/ultrahardcore/commands/msg/MsgCommand.java
+++ src/com/leontg77/ultrahardcore/commands/msg/MsgCommand.java
@@ -1,11 +1,9 @@
 package com.leontg77.ultrahardcore.commands.msg;
 
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TimeZone;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -25,11 +23,15 @@ import com.leontg77.ultrahardcore.utils.DateUtils;
  * @author LeonTG77
  */
 public class MsgCommand extends UHCCommand {
-	public static Map<String, String> msg = new HashMap<String, String>();
+	private final Main plugin;
 	
-	public MsgCommand() {
+	public MsgCommand(Main plugin) {
 		super("msg", "<player> <message>");
+		
+		this.plugin = plugin;
 	}
+	
+	public static Map<String, String> msg = new HashMap<String, String>();
 
 	@Override
 	public boolean execute(CommandSender sender, String[] args) throws CommandException {
@@ -39,14 +41,13 @@ public class MsgCommand extends UHCCommand {
 		}
 		
 		Player player = (Player) sender;
+		User user = plugin.getUser(player);
 		
     	if (args.length < 2) {
         	return false;
         }
 		
-    	if (isMuted(player)) {
-    		User user = User.get(player);
-    		
+    	if (user.isMuted()) {
     		player.sendMessage(Main.PREFIX + "You have been muted for: §a" + user.getMutedReason());
 			
 			if (user.getMuteExpiration() == null) {
@@ -63,13 +64,13 @@ public class MsgCommand extends UHCCommand {
         	throw new CommandException("'" + args[0] + "' is not online.");
         }
         
-		User user = User.get(target);
+		User tUser = plugin.getUser(target);
 		
-		if (user.isIgnoring(player)) {
+		if (tUser.isIgnoring(player)) {
 			throw new CommandException("'" + player.getName() + "' have you ignored.");
 		}
 		
-    	if (isMuted(target)) {
+    	if (tUser.isMuted()) {
     		player.sendMessage(ChatColor.RED + "'" + target.getName() + "' is muted and won't be able to respond.");
     	}
         
@@ -86,32 +87,5 @@ public class MsgCommand extends UHCCommand {
 	@Override
 	public List<String> tabComplete(CommandSender sender, String[] args) {
 		return null; // returning null actually returns a list of all online player names.
-	}
-	
-	/**
-	 * Check if the given sender is muted.
-	 * <p>
-	 * This will only return true if its a player, they're muted and their mute hasn't experied.
-	 * 
-	 * @param sender The sender checking for.
-	 * @return True if they are, false otherwise.
-	 */
-	protected static boolean isMuted(Player player) {
-		User user = User.get(player);
-    	
-		if (!user.isMuted()) {
-			return false;
-		}
-
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-		Date date = new Date();
-		
-		// if the mute isnt permanent (perm == -1) and their mute time experied, return false and unmute.
-		if (user.getMuteExpiration() != null && user.getMuteExpiration().getTime() < date.getTime()) {
-			user.unmute();
-			return false;
-		} 
-		
-		return true;
 	}
 }
