61bff2f3ccb0971f975a22040bedd4cb41755753
LeonTG77 <leontg77@gmail.com>
2016-03-22 18:50:46 +0100

    New instances

--- src/com/leontg77/ultrahardcore/commands/game/MatchpostCommand.java
+++ src/com/leontg77/ultrahardcore/commands/game/MatchpostCommand.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.bukkit.command.CommandSender;
 
+import com.leontg77.ultrahardcore.Game;
 import com.leontg77.ultrahardcore.Main;
 import com.leontg77.ultrahardcore.commands.CommandException;
 import com.leontg77.ultrahardcore.commands.UHCCommand;
@@ -15,14 +16,17 @@ import com.leontg77.ultrahardcore.commands.UHCCommand;
  * @author LeonTG77
  */
 public class MatchpostCommand extends UHCCommand {	
+	private final Game game;
 
-	public MatchpostCommand() {
+	public MatchpostCommand(Game game) {
 		super("matchpost", "");
+		
+		this.game = game;
 	}
 
 	@Override
-	public boolean execute(final CommandSender sender, final String[] args) throws CommandException {
-		final String teamSize = game.getAdvancedTeamSize(false, false).toLowerCase();
+	public boolean execute(CommandSender sender, String[] args) throws CommandException {
+		final String teamSize = game.getTeamSize().toLowerCase();
 		
 		if (teamSize.startsWith("no") || teamSize.startsWith("open")) {
 			throw new CommandException("There are no matches running.");
@@ -33,7 +37,7 @@ public class MatchpostCommand extends UHCCommand {
 	}
 
 	@Override
-	public List<String> tabComplete(final CommandSender sender, final String[] args) {
+	public List<String> tabComplete(CommandSender sender, String[] args) {
 		return new ArrayList<String>();
 	}
 }
