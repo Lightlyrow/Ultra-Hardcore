92e0338faf33837741a7e8d9871e08acd36eae02
LeonTG77 <leontg77@gmail.com>
2016-03-27 16:03:21 +0200

    Changed some colors

--- src/com/leontg77/ultrahardcore/Announcer.java
+++ src/com/leontg77/ultrahardcore/Announcer.java
@@ -34,21 +34,21 @@ public class Announcer {
 	 * List of announcements.
 	 */
 	private static final List<String> ANNOUNCEMENTS = ImmutableList.of(
-			"Remember to use §a/uhc §7for all game information.",
-			"You can look at the hall of fame with §a/hof§7.",
-			"If you have any questions, ask in §a/helpop§7.",
-			"You can find the match post by doing §a/post§7.",
-			"The server runs off a custom UHC plugin by §6LeonTG77§7.",
-			"Wonder if you are lagging? Use §a/ms.",
+			"Remember to use §e/uhc §7for all game information.",
+			"You can look at the hall of fame with §e/hof§7.",
+			"If you have any questions, ask in §e/helpop§7.",
+			"You can find the match post by doing §e/post§7.",
+			"The server runs off a custom UHC plugin by §eLeonTG77§7.",
+			"Wonder if you are lagging? Use §e/ms.",
 			"Follow our twitter for games and updates, §atwitter.com/ArcticUHC§7!",
 			"You can apply for a rank at §6redd.it/45gxj0",
 			"You can report staff at §6redd.it/45gxj0",
 			"You can give us suggestions at §6redd.it/45gxj0",
-			"You can view your stats by using §a/stats§7.",
-			"You can view the top 10 stats with §a/top§7.",
-			"View the border size with §a/border§7.",
-			"Wondering how long it is until PvP/Meetup? Use §a/timeleft§7.",
-			"View the enabled scenario(s) by doing §a/scenario§7."
+			"You can view your stats by using §e/stats§7.",
+			"You can view the top 10 stats with §e/top§7.",
+			"View the border size with §e/border§7.",
+			"Wondering how long it is until PvP/Meetup? Use §e/timeleft§7.",
+			"View the enabled scenario(s) by doing §e/scen§7."
 	);
 	
 	private final Random rand = new Random();
