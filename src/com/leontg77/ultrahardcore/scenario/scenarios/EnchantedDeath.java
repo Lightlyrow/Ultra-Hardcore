c7dab5a4ce5898e15d7e6ff9a997eb339b13ad45
LeonTG77 <leontg77@gmail.com>
2016-03-27 15:57:17 +0200

    Small fixes and description changes

--- src/com/leontg77/ultrahardcore/scenario/scenarios/EnchantedDeath.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/EnchantedDeath.java
@@ -7,44 +7,48 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
 
 import com.leontg77.ultrahardcore.scenario.Scenario;
 
 /**
- * EnchantedDeath scenario class
+ * EnchantedDeath scenario class.
  * 
  * @author LeonTG77
  */
 public class EnchantedDeath extends Scenario implements Listener {
 	
+	/**
+	 * EnchantedDeath scenario class constructor.
+	 */
 	public EnchantedDeath() {
-		super("EnchantedDeath", "You cannot craft an enchantment table, only way of getting it is killing a player.");
+		super("EnchantedDeath", "You cannot craft enchantment tables, however if a player dies to PvP they will drop an enchantment table.");
 	}
 
-	@Override
-	public void onDisable() {}
-
-	@Override
-	public void onEnable() {}
-
-	@EventHandler(priority = EventPriority.LOW)
-	public void onPlayerDeath(PlayerDeathEvent event) {
+	@EventHandler(priority = EventPriority.LOWEST) // lowest priority incase any other things need the drops, like timebomb
+	public void on(PlayerDeathEvent event) {
 		Player player = event.getEntity();
+		Player killer = player.getKiller();
 		
-		if (player.getKiller() == null) {
+		if (killer == null) {
 			return;
 		}
 		
-		event.getDrops().add(new ItemStack (Material.ENCHANTMENT_TABLE));
+		event.getDrops().add(new ItemStack(Material.ENCHANTMENT_TABLE));
 	}
 	
 	@EventHandler
-	public void onPrepareItemCraft(PrepareItemCraftEvent event) {
+	public void on(PrepareItemCraftEvent event) {
+		CraftingInventory inv = event.getInventory();
 		ItemStack item = event.getRecipe().getResult();
 		
+		if (item == null) {
+			return;
+		}
+		
 		if (item.getType() == Material.ENCHANTMENT_TABLE) {
-			event.getInventory().setResult(new ItemStack(Material.AIR));
+			inv.setResult(new ItemStack(Material.AIR));
 		}
 	}
 }
