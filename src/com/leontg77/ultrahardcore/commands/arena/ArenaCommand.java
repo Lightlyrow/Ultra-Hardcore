3480da117e50e16ba41f28a37ef70cf79cdc8cbf
LeonTG77 <leontg77@gmail.com>
2016-04-02 02:24:13 +0200

    Code cleanup and disabled use if parkouring or spectating

--- src/com/leontg77/ultrahardcore/commands/arena/ArenaCommand.java
+++ src/com/leontg77/ultrahardcore/commands/arena/ArenaCommand.java
@@ -9,9 +9,11 @@ import org.bukkit.scoreboard.DisplaySlot;
 
 import com.leontg77.ultrahardcore.Arena;
 import com.leontg77.ultrahardcore.Game;
+import com.leontg77.ultrahardcore.Parkour;
 import com.leontg77.ultrahardcore.commands.CommandException;
 import com.leontg77.ultrahardcore.commands.UHCCommand;
 import com.leontg77.ultrahardcore.managers.BoardManager;
+import com.leontg77.ultrahardcore.managers.SpecManager;
 import com.leontg77.ultrahardcore.utils.PlayerUtils;
 
 /**
@@ -20,30 +22,40 @@ import com.leontg77.ultrahardcore.utils.PlayerUtils;
  * @author LeonTG77
  */
 public class ArenaCommand extends UHCCommand {	
-	private final BoardManager board;
-	
-	private final Arena arena;
 	private final Game game;
+	
+	private final Parkour parkour;
+	private final Arena arena;
+	
+	private final BoardManager board;
+	private final SpecManager spec;
 
-	public ArenaCommand(Arena arena, Game game, BoardManager board) {
+	public ArenaCommand(Arena arena, Game game, Parkour parkour, SpecManager spec, BoardManager board) {
 		super("arena", "[enable|disable|reset|board|leave]");
+
+		this.game = game;
+
+		this.parkour = parkour;
+		this.arena = arena;
 		
 		this.board = board;
-		
-		this.arena = arena;
-		this.game = game;
+		this.spec = spec;
 	}
 
+	private static final String ADMIN_PERMISSION = "uhc.arena.admin";
+	
 	@Override
-	public boolean execute(final CommandSender sender, final String[] args) throws CommandException {
-		if (sender.hasPermission("uhc.arena.admin")) {
-			if (args.length > 0) {
+	public boolean execute(CommandSender sender, String[] args) throws CommandException {
+		if (args.length > 0) {
+			if (sender.hasPermission(ADMIN_PERMISSION)) {
 				if (args[0].equalsIgnoreCase("enable")) {
 					if (arena.isEnabled()) {
 						throw new CommandException("The arena is already enabled.");
 					}
 					
-					PlayerUtils.broadcast(Arena.PREFIX + "The arena has been enabled, use §a/a §7to join it.");
+					PlayerUtils.broadcast(Arena.PREFIX + "The arena has been enabled.");
+					PlayerUtils.broadcast(Arena.PREFIX + "You can use §a/a §7to join it.");
+					
 					arena.enable();
 					return true;
 				} 
@@ -69,7 +81,7 @@ public class ArenaCommand extends UHCCommand {
 					}
 					
 					if (game.arenaBoard()) {
-						for (String entry : arena.sb.getEntries()) {
+						for (String entry : arena.getBoard().getEntries()) {
 							arena.resetScore(entry);
 						}
 						
@@ -79,16 +91,32 @@ public class ArenaCommand extends UHCCommand {
 						board.getKillsObjective().setDisplaySlot(DisplaySlot.SIDEBAR);
 					} else {
 						PlayerUtils.broadcast(Arena.PREFIX + "The arena board has been enabled.");
-						arena.killboard.setDisplaySlot(DisplaySlot.SIDEBAR);
+						arena.getKillBoard().setDisplaySlot(DisplaySlot.SIDEBAR);
 						
 						game.setPregameBoard(false);
 						game.setArenaBoard(true);
-
-						arena.setScore("§8» §c§oPvE", 1);
-						arena.setScore("§8» §c§oPvE", 0);
 					}
 					return true;
 				}
+			}
+			
+			if (args[0].equalsIgnoreCase("leave")) {
+				if (!(sender instanceof Player)) {
+					throw new CommandException("Only players can use the arena.");
+				}
+				
+				Player player = (Player) sender;
+				
+				if (!arena.isEnabled()) {
+					throw new CommandException("The arena is currently disabled.");
+				}
+				
+				if (!arena.hasPlayer(player)) {
+					throw new CommandException("You are not in the arena.");
+				}
+				
+				arena.removePlayer(player, false);;
+				return true;
 			}
 		}
 		
@@ -98,7 +126,7 @@ public class ArenaCommand extends UHCCommand {
 		
 		Player player = (Player) sender;
 		
-		if (args.length > 0 && args[0].equalsIgnoreCase("leave")) {
+		if (args[0].equalsIgnoreCase("leave")) {
 			if (!arena.isEnabled()) {
 				throw new CommandException("The arena is currently disabled.");
 			}
@@ -118,19 +146,27 @@ public class ArenaCommand extends UHCCommand {
 		if (arena.hasPlayer(player)) {
 			throw new CommandException("You are already in the arena.");
 		}
+
+		if (parkour.isParkouring(player)) {
+			throw new CommandException("You cannot join the arena while parkouring\n§cTo leave the parkour use /parkour leave!");
+		}
+
+		if (spec.isSpectating(player)) {
+			throw new CommandException("You cannot join the arena as a spectator.");
+		}
 		
 		arena.addPlayer(player);
 		return true;
 	}
 
 	@Override
-	public List<String> tabComplete(final CommandSender sender, final String[] args) {
+	public List<String> tabComplete(CommandSender sender, String[] args) {
 		List<String> toReturn = new ArrayList<String>();
 		
 		if (args.length == 1) {
 			toReturn.add("leave");
 			
-			if (sender.hasPermission("uhc.arena.admin")) {
+			if (sender.hasPermission(ADMIN_PERMISSION)) {
 				toReturn.add("enable");
 				toReturn.add("disable");
 				toReturn.add("reset");
