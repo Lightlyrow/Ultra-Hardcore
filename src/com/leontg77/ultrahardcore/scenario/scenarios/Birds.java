0eb74e3612c5895fcfa997ea7019cef5424b89d4
LeonTG77 <leontg77@gmail.com>
2016-04-11 00:04:42 +0200

    Made it only work while a game is running and added a height limit on how high you can fly

--- src/com/leontg77/ultrahardcore/scenario/scenarios/Birds.java
+++ src/com/leontg77/ultrahardcore/scenario/scenarios/Birds.java
@@ -5,19 +5,33 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
+import com.leontg77.ultrahardcore.State;
+import com.leontg77.ultrahardcore.Timer;
+import com.leontg77.ultrahardcore.events.FinalHealEvent;
 import com.leontg77.ultrahardcore.scenario.Scenario;
 
 /**
- * Birds scenario class
+ * Birds scenario class.
  * 
  * @author LeonTG77
  */
 public class Birds extends Scenario implements Listener {
+	private final Timer timer;
 	
-	public Birds() {
+	/**
+	 * Birds class constructor.
+	 * 
+	 * @param timer The timer class.
+	 */
+	public Birds(Timer timer) {
 		super("Birds", "All players can fly!");
+		
+		this.timer = timer;
 	}
+	
+	public static final String PREFIX = "§aBirds §8» §7";
 	
 	@Override
 	public void onDisable() {
@@ -29,6 +43,15 @@ public class Birds extends Scenario implements Listener {
 
 	@Override
 	public void onEnable() {
+		if (!State.isState(State.INGAME) || timer.getTimeSinceStartInSeconds() < 20) {
+			return;
+		}
+		
+		on(new FinalHealEvent());
+	}
+	
+	@EventHandler
+	public void on(FinalHealEvent event) {
 		for (Player online : Bukkit.getOnlinePlayers()) {
 			online.setAllowFlight(true);
 		}
@@ -36,8 +59,27 @@ public class Birds extends Scenario implements Listener {
 	
 	@EventHandler
 	public void on(PlayerJoinEvent event) {
-		final Player player = event.getPlayer();
+		if (!State.isState(State.INGAME) || timer.getTimeSinceStartInSeconds() < 20) {
+			return;
+		}
+		
+		Player player = event.getPlayer();
 		
 		player.setAllowFlight(true);
+		player.setFlying(true);
+	}
+	
+	@EventHandler
+	public void on(PlayerMoveEvent event) {
+		if (!State.isState(State.INGAME) || timer.getTimeSinceStartInSeconds() < 20) {
+			return;
+		}
+		
+		Player player = event.getPlayer();
+		
+		if (player.isFlying() && event.getTo().getY() > 255) {
+			player.sendMessage(PREFIX + "You cannot fly above the height limit.");
+			event.setCancelled(true);
+		}
 	}
 }
